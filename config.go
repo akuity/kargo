a4ec10acaa19c9028f5f89c30fe11dd4a34d060e
Kent <kent.rancourt@gmail.com>
2022-08-23 16:49:34 -0400

    remove namespace from k8sta config object
    Signed-off-by: Kent <kent.rancourt@gmail.com>

--- config.go
+++ config.go
@@ -9,10 +9,6 @@ import (
 func k8staConfig() (config.Config, error) {
 	config := config.Config{}
 	var err error
-	if config.LogLevel, err =
-		log.ParseLevel(os.GetEnvVar("LOG_LEVEL", "INFO")); err != nil {
-		return config, err
-	}
-	config.Namespace, err = os.GetRequiredEnvVar("NAMESPACE")
+	config.LogLevel, err = log.ParseLevel(os.GetEnvVar("LOG_LEVEL", "INFO"))
 	return config, err
 }
