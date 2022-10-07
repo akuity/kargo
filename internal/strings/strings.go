6a5a868365affd5a1de813dea486279863ce2c4c
Kent <kent.rancourt@gmail.com>
2022-10-07 15:57:17 -0400

    add string utility for splitting on LAST occurence of a separator
    Signed-off-by: Kent <kent.rancourt@gmail.com>

---
+++ internal/strings/strings.go
@@ -0,0 +1,27 @@
+package strings
+
+import (
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+// SplitLast splits a specified string on the last occurrence of the specified
+// separator and returns two strings -- the first containing everything that
+// preceded the separator and the second containing everything that followed the
+// separator. If the specified string contains no occurrences of the specified
+// separator, an error is returned.
+func SplitLast(s, sep string) (string, string, error) {
+	if sep == "" {
+		return "", "", errors.New("no separator was specified")
+	}
+	i := strings.LastIndex(s, sep)
+	if i < 0 {
+		return "", "", errors.Errorf(
+			"string %q contains no occurrences of separator %q",
+			s,
+			sep,
+		)
+	}
+	return s[:i], s[i+1:], nil
+}
