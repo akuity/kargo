0d4498b363816a3aedf3b152d5e20bbdeda34217
Kent Rancourt <kent.rancourt@gmail.com>
2023-01-25 00:26:22 -0500

    add helm-based promotion mechanism (#184)
    Signed-off-by: Kent <kent.rancourt@gmail.com>

--- internal/controller/promotions.go
+++ internal/controller/promotions.go
@@ -12,6 +12,7 @@ import (
 	api "github.com/akuityio/kargo/api/v1alpha1"
 	"github.com/akuityio/kargo/internal/git"
 	"github.com/akuityio/kargo/internal/kustomize"
+	"github.com/akuityio/kargo/internal/yaml"
 )
 
 func (e *environmentReconciler) promote(
@@ -209,7 +210,7 @@ func (e *environmentReconciler) promoteWithKustomize(
 	} else if !hasDiffs {
 		// We only want health checks to factor in a specific commit if we subscribe
 		// to the Git repo. If we don't subscribe to the Git repo, we're probably in
-		// a case where the associated Application resources track the head of a
+		// a case where the associated Application resources tracks the head of a
 		// branch and we don't want to count Applications as unhealthy just on
 		// account of (with no Kargo involvement) having moved on to a newer commit
 		// at the head of that branch.
@@ -253,12 +254,175 @@ func (e *environmentReconciler) promoteWithKustomize(
 	return newState, errors.Wrap(err, "error getting last commit ID")
 }
 
+// TODO: Add some logging to this function
+// nolint: gocyclo
 func (e *environmentReconciler) promoteWithHelm(
 	ctx context.Context,
 	env *api.Environment,
 	newState api.EnvironmentState,
 ) (api.EnvironmentState, error) {
-	return newState, errors.New("promotion via Helm is not yet supported")
+	if env == nil ||
+		env.Spec.PromotionMechanisms == nil ||
+		env.Spec.PromotionMechanisms.ConfigManagement == nil ||
+		env.Spec.PromotionMechanisms.ConfigManagement.Helm == nil ||
+		len(env.Spec.PromotionMechanisms.ConfigManagement.Helm.Images) == 0 ||
+		len(newState.Images) == 0 {
+		return newState, nil
+	}
+
+	if env.Spec.GitRepo == nil || env.Spec.GitRepo.URL == "" {
+		return newState, errors.New(
+			"cannot promote images via Helm because spec does not contain " +
+				"git repo details",
+		)
+	}
+
+	repoURL := env.Spec.GitRepo.URL
+
+	creds, err := e.getGitRepoCredentialsFn(ctx, repoURL)
+	if err != nil {
+		return newState, errors.Wrapf(
+			err,
+			"error obtaining credentials for git repo %q",
+			repoURL,
+		)
+	}
+
+	repo, err := e.gitCloneFn(
+		ctx,
+		env.Spec.GitRepo.URL,
+		git.RepoCredentials{
+			Username: creds.Username,
+			Password: creds.Password,
+		},
+	)
+	if err != nil {
+		return newState, errors.Wrapf(err, "error cloning git repo %q", repoURL)
+	}
+	if repo != nil { // This could be nil during a test
+		defer repo.Close()
+	}
+	logger := e.logger.WithFields(log.Fields{
+		"environment": env.Name,
+		"namespace":   env.Namespace,
+		"repoURL":     repoURL,
+	})
+	logger.Debug("cloned git repo")
+
+	branch := env.Spec.GitRepo.Branch
+
+	if branch != "" {
+		if err = e.checkoutBranchFn(repo, branch); err != nil {
+			return newState, errors.Wrapf(
+				err,
+				"error checking out branch %q from git repo",
+				repoURL,
+			)
+		}
+	}
+	logger = logger.WithField("branch", branch)
+	logger.Debug("checked out branch")
+
+	imgUpdates := env.Spec.PromotionMechanisms.ConfigManagement.Helm.Images
+	changesByFile := buildChangeMapsByFile(newState.Images, imgUpdates)
+	for file, changes := range changesByFile {
+		if err = yaml.SetStringsInFile(
+			filepath.Join(repo.WorkingDir(), file),
+			changes,
+		); err != nil {
+			return newState, errors.Wrapf(
+				err,
+				"error updating values in file %q",
+				file,
+			)
+		}
+	}
+
+	var hasDiffs bool
+	if hasDiffs, err = repo.HasDiffs(); err != nil {
+		return newState, errors.Wrap(err, "error checking for diffs")
+	} else if !hasDiffs {
+		// We only want health checks to factor in a specific commit if we subscribe
+		// to the Git repo. If we don't subscribe to the Git repo, we're probably in
+		// a case where the associated Application resources tracks the head of a
+		// branch and we don't want to count Applications as unhealthy just on
+		// account of (with no Kargo involvement) having moved on to a newer commit
+		// at the head of that branch.
+		//
+		// TODO: This seems correct for zero environment, but it might not hold up
+		// for non-zero environments.
+		if env.Spec.Subscriptions != nil &&
+			env.Spec.Subscriptions.Repos != nil &&
+			env.Spec.Subscriptions.Repos.Git {
+			newState.HealthCheckCommit, err = repo.LastCommitID()
+		}
+		return newState, errors.Wrap(err, "error getting last commit ID")
+	}
+
+	if err = repo.AddAllAndCommit("updating images"); err != nil {
+		return newState,
+			errors.Wrap(err, "error committing updated images to git repo")
+	}
+
+	if err = repo.Push(); err != nil {
+		return newState, errors.Wrap(
+			err,
+			"error pushing commit containing updated images to git repo",
+		)
+	}
+
+	// We only want health checks to factor in a specific commit if we subscribe
+	// to the Git repo. If we don't subscribe to the Git repo, we're probably in
+	// a case where the associated Application resources track the head of a
+	// branch and we don't want to count Applications as unhealthy just on
+	// account of (with no Kargo involvement) having moved on to a newer commit
+	// at the head of that branch.
+	//
+	// TODO: This seems correct for zero environment, but it might not hold up
+	// for non-zero environments.
+	if env.Spec.Subscriptions != nil &&
+		env.Spec.Subscriptions.Repos != nil &&
+		env.Spec.Subscriptions.Repos.Git {
+		newState.HealthCheckCommit, err = repo.LastCommitID()
+	}
+	return newState, errors.Wrap(err, "error getting last commit ID")
+}
+
+// buildChangeMapsByFile takes a list of images and a list of instructions about
+// changes that should be made to various YAML files and distills them to a
+// map of maps that indexes new values for each YAML file by file name and key.
+func buildChangeMapsByFile(
+	images []api.Image,
+	imageUpdates []api.HelmImageUpdate,
+) map[string]map[string]string {
+	tagsByImage := map[string]string{}
+	for _, image := range images {
+		tagsByImage[image.RepoURL] = image.Tag
+	}
+
+	changesByFile := map[string]map[string]string{}
+	for _, imageUpdate := range imageUpdates {
+		if imageUpdate.Value != "Image" && imageUpdate.Value != "Tag" {
+			// This really shouldn't happen, so we'll ignore it.
+			continue
+		}
+		tag, found := tagsByImage[imageUpdate.Image]
+		if !found {
+			// There's no change to make in this case.
+			continue
+		}
+		if _, found = changesByFile[imageUpdate.ValuesFilePath]; !found {
+			changesByFile[imageUpdate.ValuesFilePath] = map[string]string{}
+		}
+		if imageUpdate.Value == "Image" {
+			changesByFile[imageUpdate.ValuesFilePath][imageUpdate.Key] =
+				fmt.Sprintf("%s:%s", imageUpdate.Image, tag)
+		} else {
+			changesByFile[imageUpdate.ValuesFilePath][imageUpdate.Key] = tag
+		}
+	}
+
+	return changesByFile
 }
 
 func (e *environmentReconciler) promoteWithArgoCD(
--- internal/controller/promotions_test.go
+++ internal/controller/promotions_test.go
@@ -497,6 +497,324 @@ func TestPromoteWithKustomize(t *testing.T) {
 	}
 }
 
+func TestPromoteWithHelm(t *testing.T) {
+	testCases := []struct {
+		name        string
+		env         *api.Environment
+		newState    api.EnvironmentState
+		repoCredsFn func(context.Context, string) (git.RepoCredentials, error)
+		cloneFn     func(
+			context.Context,
+			string,
+			git.RepoCredentials,
+		) (git.Repo, error)
+		checkoutFn func(repo git.Repo, branch string) error
+		assertions func(inState, outState api.EnvironmentState, err error)
+	}{
+		{
+			name: "environment is nil",
+			assertions: func(inState, outState api.EnvironmentState, err error) {
+				require.NoError(t, err)
+				require.Equal(t, inState, outState)
+			},
+		},
+		{
+			name: "PromotionMechanisms is nil",
+			env: &api.Environment{
+				Spec: api.EnvironmentSpec{},
+			},
+			assertions: func(inState, outState api.EnvironmentState, err error) {
+				require.NoError(t, err)
+				require.Equal(t, inState, outState)
+			},
+		},
+		{
+			name: "ConfigManagement is nil",
+			env: &api.Environment{
+				Spec: api.EnvironmentSpec{
+					PromotionMechanisms: &api.PromotionMechanisms{},
+				},
+			},
+			assertions: func(inState, outState api.EnvironmentState, err error) {
+				require.NoError(t, err)
+				require.Equal(t, inState, outState)
+			},
+		},
+		{
+			name: "Helm is nil",
+			env: &api.Environment{
+				Spec: api.EnvironmentSpec{
+					PromotionMechanisms: &api.PromotionMechanisms{
+						ConfigManagement: &api.ConfigManagementPromotionMechanism{},
+					},
+				},
+			},
+			assertions: func(inState, outState api.EnvironmentState, err error) {
+				require.NoError(t, err)
+				require.Equal(t, inState, outState)
+			},
+		},
+		{
+			name: "Helm promotion mechanism has len(Images) == 0",
+			env: &api.Environment{
+				Spec: api.EnvironmentSpec{
+					PromotionMechanisms: &api.PromotionMechanisms{
+						ConfigManagement: &api.ConfigManagementPromotionMechanism{
+							Helm: &api.HelmPromotionMechanism{},
+						},
+					},
+				},
+			},
+			assertions: func(inState, outState api.EnvironmentState, err error) {
+				require.NoError(t, err)
+				require.Equal(t, inState, outState)
+			},
+		},
+		{
+			name: "new Environment state has has len(Images) == 0",
+			env: &api.Environment{
+				Spec: api.EnvironmentSpec{
+					PromotionMechanisms: &api.PromotionMechanisms{
+						ConfigManagement: &api.ConfigManagementPromotionMechanism{
+							Helm: &api.HelmPromotionMechanism{
+								Images: []api.HelmImageUpdate{
+									{},
+								},
+							},
+						},
+					},
+				},
+			},
+			assertions: func(inState, outState api.EnvironmentState, err error) {
+				require.NoError(t, err)
+				require.Equal(t, inState, outState)
+			},
+		},
+		{
+			name: "Environment spec is missing Git repo details",
+			env: &api.Environment{
+				Spec: api.EnvironmentSpec{
+					PromotionMechanisms: &api.PromotionMechanisms{
+						ConfigManagement: &api.ConfigManagementPromotionMechanism{
+							Helm: &api.HelmPromotionMechanism{
+								Images: []api.HelmImageUpdate{
+									{},
+								},
+							},
+						},
+					},
+				},
+			},
+			newState: api.EnvironmentState{
+				Images: []api.Image{
+					{},
+				},
+			},
+			assertions: func(inState, outState api.EnvironmentState, err error) {
+				require.Error(t, err)
+				require.Equal(
+					t,
+					"cannot promote images via Helm because spec does not contain "+
+						"git repo details",
+					err.Error(),
+				)
+				require.Equal(t, inState, outState)
+			},
+		},
+		{
+			name: "error getting Git repo credentials",
+			env: &api.Environment{
+				Spec: api.EnvironmentSpec{
+					GitRepo: &api.GitRepo{
+						URL: "fake-url",
+					},
+					PromotionMechanisms: &api.PromotionMechanisms{
+						ConfigManagement: &api.ConfigManagementPromotionMechanism{
+							Helm: &api.HelmPromotionMechanism{
+								Images: []api.HelmImageUpdate{
+									{},
+								},
+							},
+						},
+					},
+				},
+			},
+			newState: api.EnvironmentState{
+				Images: []api.Image{
+					{},
+				},
+			},
+			repoCredsFn: func(context.Context, string) (git.RepoCredentials, error) {
+				return git.RepoCredentials{}, errors.New("something went wrong")
+			},
+			assertions: func(inState, outState api.EnvironmentState, err error) {
+				require.Error(t, err)
+				require.Contains(
+					t,
+					err.Error(),
+					"error obtaining credentials for git repo",
+				)
+				require.Contains(t, err.Error(), "something went wrong")
+				require.Equal(t, inState, outState)
+			},
+		},
+		{
+			name: "error cloning Git repo",
+			env: &api.Environment{
+				Spec: api.EnvironmentSpec{
+					GitRepo: &api.GitRepo{
+						URL: "fake-url",
+					},
+					PromotionMechanisms: &api.PromotionMechanisms{
+						ConfigManagement: &api.ConfigManagementPromotionMechanism{
+							Helm: &api.HelmPromotionMechanism{
+								Images: []api.HelmImageUpdate{
+									{},
+								},
+							},
+						},
+					},
+				},
+			},
+			newState: api.EnvironmentState{
+				Images: []api.Image{
+					{},
+				},
+			},
+			repoCredsFn: func(context.Context, string) (git.RepoCredentials, error) {
+				return git.RepoCredentials{}, nil
+			},
+			cloneFn: func(
+				context.Context,
+				string,
+				git.RepoCredentials,
+			) (git.Repo, error) {
+				return nil, errors.New("something went wrong")
+			},
+			assertions: func(inState, outState api.EnvironmentState, err error) {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), "error cloning git repo")
+				require.Contains(t, err.Error(), "something went wrong")
+				require.Equal(t, inState, outState)
+			},
+		},
+		{
+			name: "error checking out branch",
+			env: &api.Environment{
+				Spec: api.EnvironmentSpec{
+					GitRepo: &api.GitRepo{
+						URL:    "fake-url",
+						Branch: "fake-branch",
+					},
+					PromotionMechanisms: &api.PromotionMechanisms{
+						ConfigManagement: &api.ConfigManagementPromotionMechanism{
+							Helm: &api.HelmPromotionMechanism{
+								Images: []api.HelmImageUpdate{
+									{},
+								},
+							},
+						},
+					},
+				},
+			},
+			newState: api.EnvironmentState{
+				Images: []api.Image{
+					{},
+				},
+			},
+			repoCredsFn: func(context.Context, string) (git.RepoCredentials, error) {
+				return git.RepoCredentials{}, nil
+			},
+			cloneFn: func(
+				context.Context,
+				string,
+				git.RepoCredentials,
+			) (git.Repo, error) {
+				return nil, nil
+			},
+			checkoutFn: func(git.Repo, string) error {
+				return errors.New("something went wrong")
+			},
+			assertions: func(inState, outState api.EnvironmentState, err error) {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), "error checking out branch")
+				require.Contains(t, err.Error(), "something went wrong")
+				require.Equal(t, inState, outState)
+			},
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			reconciler := environmentReconciler{
+				logger:                  log.New(),
+				getGitRepoCredentialsFn: testCase.repoCredsFn,
+				gitCloneFn:              testCase.cloneFn,
+				checkoutBranchFn:        testCase.checkoutFn,
+			}
+			reconciler.logger.SetLevel(log.ErrorLevel)
+			newState, err := reconciler.promoteWithHelm(
+				context.Background(),
+				testCase.env,
+				testCase.newState,
+			)
+			testCase.assertions(testCase.newState, newState, err)
+		})
+	}
+}
+
+func TestBuildChangeMapsByFile(t *testing.T) {
+	images := []api.Image{
+		{
+			RepoURL: "fake-url",
+			Tag:     "fake-tag",
+		},
+		{
+			RepoURL: "another-fake-url",
+			Tag:     "another-fake-tag",
+		},
+	}
+	imageUpdates := []api.HelmImageUpdate{
+		{
+			ValuesFilePath: "fake-values.yaml",
+			Image:          "fake-url",
+			Key:            "fake-key",
+			Value:          "Image",
+		},
+		{
+			ValuesFilePath: "fake-values.yaml",
+			Image:          "another-fake-url",
+			Key:            "another-fake-key",
+			Value:          "Image",
+		},
+		{
+			ValuesFilePath: "another-fake-values.yaml",
+			Image:          "fake-url",
+			Key:            "fake-key",
+			Value:          "Tag",
+		},
+		{
+			ValuesFilePath: "yet-another-fake-values.yaml",
+			Image:          "image-that-is-not-in-list",
+			Key:            "fake-key",
+			Value:          "Tag",
+		},
+	}
+	result := buildChangeMapsByFile(images, imageUpdates)
+	require.Equal(
+		t,
+		map[string]map[string]string{
+			"fake-values.yaml": {
+				"fake-key":         "fake-url:fake-tag",
+				"another-fake-key": "another-fake-url:another-fake-tag",
+			},
+			"another-fake-values.yaml": {
+				"fake-key": "fake-tag",
+			},
+		},
+		result,
+	)
+}
+
 func TestPromoteWithArgoCD(t *testing.T) {
 	testCases := []struct {
 		name      string
