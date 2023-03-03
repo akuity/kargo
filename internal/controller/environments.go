b5366d990f1d1bab95b6af99b5048e7ee0603ea1
Kent Rancourt <kent.rancourt@gmail.com>
2023-03-03 16:26:51 -0500

    add scaffolding for promotion type and reconciler (#227)
    Signed-off-by: Kent <kent.rancourt@gmail.com>

--- internal/controller/environments.go
+++ internal/controller/environments.go
@@ -525,6 +525,44 @@ func (e *environmentReconciler) getAvailableStatesFromUpstreamEnvs(
 	return availableStates, nil
 }
 
+// TODO: This function could use some tests
+func (e *environmentReconciler) promote(
+	ctx context.Context,
+	promoMechanisms api.PromotionMechanisms,
+	newState api.EnvironmentState,
+) (api.EnvironmentState, error) {
+	var err error
+	for _, gitRepoUpdate := range promoMechanisms.GitRepoUpdates {
+		if gitRepoUpdate.Bookkeeper != nil {
+			if newState, err =
+				e.applyBookkeeperUpdate(ctx, newState, gitRepoUpdate); err != nil {
+				return newState, errors.Wrap(err, "error promoting via Git")
+			}
+		} else {
+			if newState, err =
+				e.applyGitRepoUpdate(ctx, newState, gitRepoUpdate); err != nil {
+				return newState, errors.Wrap(err, "error promoting via Git")
+			}
+		}
+	}
+
+	for _, argoCDAppUpdate := range promoMechanisms.ArgoCDAppUpdates {
+		if err =
+			e.applyArgoCDAppUpdate(ctx, newState, argoCDAppUpdate); err != nil {
+			return newState, errors.Wrap(err, "error promoting via Argo CD")
+		}
+	}
+
+	newState.Health = &api.Health{
+		Status:       api.HealthStateUnknown,
+		StatusReason: "Health has not yet been assessed",
+	}
+
+	e.logger.Debug("completed promotion")
+
+	return newState, nil
+}
+
 // getEnv returns a pointer to the Environment resource specified by the
 // namespacedName argument. If no such resource is found, nil is returned
 // instead.
