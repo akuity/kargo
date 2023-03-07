9ea159f8f131d797d13ffe9755b329b5753ff6ae
Kent <kent.rancourt@gmail.com>
2023-03-07 15:01:39 -0500

    minor cleanup of git utility package to promote consistency with similar utility packages
    Signed-off-by: Kent <kent.rancourt@gmail.com>

--- internal/git/creds.go
+++ internal/git/creds.go
@@ -1,17 +1,15 @@
 package git
 
-// RepoCredentials represents the credentials for connecting to a private git
+// Credentials represents the credentials for connecting to a private Git
 // repository.
-type RepoCredentials struct {
-	// SSHPrivateKey is a private key that can be used for both reading from and
-	// writing to some remote repository.
-	SSHPrivateKey string `json:"sshPrivateKey,omitempty"`
+type Credentials struct {
 	// Username identifies a principal, which combined with the value of the
-	// Password field, can be used for both reading from and writing to some
-	// remote repository.
-	Username string `json:"username,omitempty"`
+	// Password field, can be used for accessing some Git repository.
+	Username string
 	// Password, when combined with the principal identified by the Username
-	// field, can be used for both reading from and writing to some remote
+	// field, can be used for accessing some Git repository.
+	Password string
+	// SSHPrivateKey is a private key that can be used for accessing some Git
 	// repository.
-	Password string `json:"password,omitempty"`
+	SSHPrivateKey string
 }
