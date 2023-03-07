696cf53a44fe9014034036663f469d630a0b0021
Kent <kent.rancourt@gmail.com>
2023-03-07 14:49:24 -0500

    implement credentials DB
    Signed-off-by: Kent <kent.rancourt@gmail.com>

---
+++ internal/controller/credentials.go
@@ -0,0 +1,284 @@
+package controller
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/argoproj/argo-cd/v2/applicationset/utils"
+	"github.com/argoproj/argo-cd/v2/common"
+	"github.com/argoproj/argo-cd/v2/util/git"
+	"github.com/pkg/errors"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/labels"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+)
+
+// credentialsType is a string type used to represent a type of credentials.
+type credentialsType string
+
+const (
+	// credentialsTypeGit represents credentials for a Git repository.
+	credentialsTypeGit credentialsType = "git"
+	// credentialsTypeHelm represents credentials for a Helm chart repository.
+	credentialsTypeHelm credentialsType = "helm"
+	// credentialsTypeImage represents credentials for an image repository.
+	credentialsTypeImage credentialsType = "image"
+
+	// secretsByRepo is the name of the index that credentialsDB uses for indexing
+	// credentials stored in Kubernetes Secrets by repository type + URL.
+	secretsByRepo = "repo"
+
+	kargoSecretTypeLabel = "kargo.akuity.io/secret-type"
+)
+
+// credentials generically represents any type of repository credential.
+type credentials struct {
+	// Username identifies a principal, which combined with the value of the
+	// Password field, can be used for access to some repository.
+	Username string
+	// Password, when combined with the principal identified by the Username
+	// field, can be used for access to some repository.
+	Password string
+	// SSHPrivateKey is a private key that can be used for access to some remote
+	// repository. This is primarily applicable for Git repositories.
+	SSHPrivateKey string
+}
+
+// credentialsDBIface is an interface for a credentials store.
+type credentialsDBIface interface {
+	get(
+		ctx context.Context,
+		namespace string,
+		credType credentialsType,
+		repo string,
+	) (credentials, bool, error)
+}
+
+// credentialsDB is an implementation of the credentialsDBIface interface that
+// utilizes a Kubernetes controller runtime client to index and retrieve
+// credentials stored in Kubernetes Secrets.
+type credentialsDB struct {
+	argoCDNamespace string
+	client          client.Client
+}
+
+// newCredentialsDB initializes a new instance of credentialsDB. This function
+// carries out the important task of indexing credentials stored in Kubernetes
+// Secrets by repository type + URL.
+func newCredentialsDB(
+	ctx context.Context,
+	argoCDNamespace string,
+	mgr manager.Manager,
+) (credentialsDBIface, error) {
+	c := &credentialsDB{
+		argoCDNamespace: argoCDNamespace,
+		client:          mgr.GetClient(),
+	}
+	err := mgr.GetFieldIndexer().IndexField(
+		ctx,
+		&corev1.Secret{},
+		secretsByRepo,
+		c.index,
+	)
+	return c, errors.Wrap(err, "error indexing Secrets by repo")
+}
+
+func (c *credentialsDB) index(obj client.Object) []string {
+	secret := obj.(*corev1.Secret) // nolint: forcetypeassert
+	// Refuse to index this secret if it has no labels or data.
+	if secret.Labels == nil || secret.Data == nil {
+		return nil
+	}
+	if secret.Namespace == c.argoCDNamespace {
+		// If the Secret is in Argo CD's namespace, expect that it should be
+		// labeled like an Argo CD Secret. If it isn't refuse to index this
+		// Secret. We're also not interested in indexing Secrets that represent
+		// credential templates, because we will always have to iterate over
+		// such secrets to find what we're looking for anyway.
+		if secType, ok :=
+			secret.Labels[utils.ArgoCDSecretTypeLabel]; !ok ||
+			secType != common.LabelValueSecretTypeRepository {
+			return nil
+		}
+	} else {
+		// If the Secret is in any other namespace, expect that it should be
+		// labeled like a Kargo Secret. If it isn't refuse to index this Secret.
+		// We're also not interested in indexing Secrets that represent
+		// credential templates, because we will always have to iterate over
+		// such secrets to find what we're looking for anyway.
+		if secType, ok := secret.Labels[kargoSecretTypeLabel]; !ok ||
+			secType != common.LabelValueSecretTypeRepository {
+			return nil
+		}
+	}
+	var credsType credentialsType
+	if credsTypeBytes, ok := secret.Data["type"]; ok {
+		credsType = credentialsType(credsTypeBytes)
+	} else {
+		// If not specified, assume these credentials are for a Git repo.
+		credsType = credentialsTypeGit
+	}
+	// Refuse to index this Secret if we don't recognize what type of
+	// repository these credentials are supposed to be for.
+	switch credsType {
+	case credentialsTypeGit, credentialsTypeHelm, credentialsTypeImage:
+	default:
+		return nil
+	}
+	var repoURL string
+	if repoURLBytes, ok := secret.Data["url"]; ok {
+		repoURL = string(repoURLBytes)
+		if credsType == credentialsTypeGit {
+			// This is important. We don't want the presence or absence of ".git"
+			// at the end of the URL to affect credential lookups.
+			repoURL = git.NormalizeGitURL(string(repoURLBytes))
+		}
+	} else {
+		// No URL. Refuse to index this Secret.
+		return nil
+	}
+	return []string{credsSecretIndexVal(credsType, repoURL)}
+}
+
+func (c *credentialsDB) get(
+	ctx context.Context,
+	namespace string,
+	credType credentialsType,
+	repoURL string,
+) (credentials, bool, error) {
+	if credType == credentialsTypeGit {
+		// This is important. We don't want the presence or absence of ".git" at the
+		// end of the URL to affect credential lookups.
+		repoURL = git.NormalizeGitURL(repoURL)
+	}
+	creds := credentials{}
+
+	var secret *corev1.Secret
+	var err error
+
+	// Check namespace for credentials
+	if secret, err = c.getCredentialsSecret(
+		ctx,
+		namespace,
+		labels.Set(map[string]string{
+			kargoSecretTypeLabel: common.LabelValueSecretTypeRepository,
+		}).AsSelector(),
+		fields.Set(map[string]string{
+			secretsByRepo: credsSecretIndexVal(credType, repoURL),
+		}).AsSelector(),
+	); err != nil {
+		return creds, false, err
+	}
+
+	if secret == nil {
+		// Check namespace for credentials template
+		if secret, err = c.getCredentialsTemplateSecret(
+			ctx,
+			namespace,
+			labels.Set(map[string]string{
+				kargoSecretTypeLabel: common.LabelValueSecretTypeRepoCreds,
+			}).AsSelector(),
+			repoURL,
+		); err != nil {
+			return creds, false, err
+		}
+	}
+
+	if secret == nil {
+		// Check Argo CD's namespace for credentials
+		if secret, err = c.getCredentialsSecret(
+			ctx,
+			c.argoCDNamespace,
+			labels.Set(map[string]string{
+				utils.ArgoCDSecretTypeLabel: common.LabelValueSecretTypeRepository,
+			}).AsSelector(),
+			fields.Set(map[string]string{
+				secretsByRepo: credsSecretIndexVal(credType, repoURL),
+			}).AsSelector(),
+		); err != nil {
+			return creds, false, err
+		}
+	}
+
+	if secret == nil {
+		// Check Argo CD's namespace for credentials template
+		if secret, err = c.getCredentialsTemplateSecret(
+			ctx,
+			c.argoCDNamespace,
+			labels.Set(map[string]string{
+				utils.ArgoCDSecretTypeLabel: common.LabelValueSecretTypeRepoCreds,
+			}).AsSelector(),
+			repoURL,
+		); err != nil || secret == nil {
+			return creds, false, err
+		}
+	}
+
+	creds.Username = string(secret.Data["username"])
+	creds.Password = string(secret.Data["password"])
+	creds.SSHPrivateKey = string(secret.Data["sshPrivateKey"])
+	return creds, true, nil
+}
+
+func (c *credentialsDB) getCredentialsSecret(
+	ctx context.Context,
+	namespace string,
+	labelSelector labels.Selector,
+	fieldSelector fields.Selector,
+) (*corev1.Secret, error) {
+	secrets := corev1.SecretList{}
+	if err := c.client.List(
+		ctx,
+		&secrets,
+		&client.ListOptions{
+			Namespace:     namespace,
+			LabelSelector: labelSelector,
+			FieldSelector: fieldSelector,
+		},
+	); err != nil {
+		return nil, err
+	}
+	if len(secrets.Items) == 0 {
+		return nil, nil
+	}
+	// We know any secret we find has properly formatted data because that was
+	// a condition for indexing it.
+	return &(secrets.Items[0]), nil
+}
+
+func (c *credentialsDB) getCredentialsTemplateSecret(
+	ctx context.Context,
+	namespace string,
+	labelSelector labels.Selector,
+	repoURL string,
+) (*corev1.Secret, error) {
+	secrets := corev1.SecretList{}
+	if err := c.client.List(
+		ctx,
+		&secrets,
+		&client.ListOptions{
+			Namespace:     namespace,
+			LabelSelector: labelSelector,
+		},
+	); err != nil {
+		return nil, err
+	}
+	// Scan for the credentials we're looking for
+	for _, secret := range secrets.Items {
+		if secret.Data == nil {
+			continue
+		}
+		if url, ok := secret.Data["url"]; ok &&
+			strings.HasPrefix(repoURL, string(url)) {
+			return &secret, nil
+		}
+	}
+	return nil, nil
+}
+
+func credsSecretIndexVal(credsType credentialsType, repoURL string) string {
+	return fmt.Sprintf("%s:%s", credsType, repoURL)
+}
---
+++ internal/controller/credentials_test.go
@@ -0,0 +1,293 @@
+package controller
+
+import (
+	"context"
+	"testing"
+
+	"github.com/argoproj/argo-cd/v2/applicationset/utils"
+	"github.com/argoproj/argo-cd/v2/common"
+	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/labels"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+)
+
+func TestIndex(t *testing.T) {
+	testCases := []struct {
+		name           string
+		secret         *corev1.Secret
+		expectedResult []string
+	}{
+		{
+			name: "no labels",
+			secret: &corev1.Secret{
+				Data: make(map[string][]byte),
+			},
+		},
+
+		{
+			name: "no data",
+			secret: &corev1.Secret{
+				ObjectMeta: v1.ObjectMeta{
+					Labels: map[string]string{},
+				},
+			},
+		},
+
+		{
+			name: "secret in Argo CD namespace not labeled",
+			secret: &corev1.Secret{
+				ObjectMeta: v1.ObjectMeta{
+					Namespace: "argo-cd",
+					Labels:    map[string]string{},
+				},
+				Data: map[string][]byte{},
+			},
+		},
+
+		{
+			name: "secret in Argo CD namespace not labeled as a repo",
+			secret: &corev1.Secret{
+				ObjectMeta: v1.ObjectMeta{
+					Namespace: "argo-cd",
+					Labels: map[string]string{
+						utils.ArgoCDSecretTypeLabel: "bogus",
+					},
+				},
+				Data: map[string][]byte{},
+			},
+		},
+
+		{
+			name: "secret in other namespace not labeled",
+			secret: &corev1.Secret{
+				ObjectMeta: v1.ObjectMeta{
+					Namespace: "fake-namespace",
+					Labels:    map[string]string{},
+				},
+				Data: map[string][]byte{},
+			},
+		},
+
+		{
+			name: "secret in other namespace not labeled as a repo",
+			secret: &corev1.Secret{
+				ObjectMeta: v1.ObjectMeta{
+					Namespace: "fake-namespace",
+					Labels: map[string]string{
+						kargoSecretTypeLabel: "bogus",
+					},
+				},
+				Data: map[string][]byte{},
+			},
+		},
+
+		{
+			name: "credentials type is invalid",
+			secret: &corev1.Secret{
+				ObjectMeta: v1.ObjectMeta{
+					Namespace: "fake-namespace",
+					Labels: map[string]string{
+						kargoSecretTypeLabel: common.LabelValueSecretTypeRepository,
+					},
+				},
+				Data: map[string][]byte{
+					"type": []byte("bogus"),
+				},
+			},
+		},
+
+		{
+			name: "URL is missing",
+			secret: &corev1.Secret{
+				ObjectMeta: v1.ObjectMeta{
+					Namespace: "fake-namespace",
+					Labels: map[string]string{
+						kargoSecretTypeLabel: common.LabelValueSecretTypeRepository,
+					},
+				},
+				Data: map[string][]byte{},
+			},
+		},
+
+		{
+			name: "URL is missing",
+			secret: &corev1.Secret{
+				ObjectMeta: v1.ObjectMeta{
+					Namespace: "fake-namespace",
+					Labels: map[string]string{
+						kargoSecretTypeLabel: common.LabelValueSecretTypeRepository,
+					},
+				},
+				Data: map[string][]byte{},
+			},
+			expectedResult: nil,
+		},
+
+		{
+			name: "success",
+			secret: &corev1.Secret{
+				ObjectMeta: v1.ObjectMeta{
+					Namespace: "fake-namespace",
+					Labels: map[string]string{
+						kargoSecretTypeLabel: common.LabelValueSecretTypeRepository,
+					},
+				},
+				Data: map[string][]byte{
+					"url": []byte("fake-url"),
+				},
+			},
+			expectedResult: []string{"git:fake-url"},
+		},
+	}
+	credsDB := &credentialsDB{
+		argoCDNamespace: "argo-cd",
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			require.Equal(t, testCase.expectedResult, credsDB.index(testCase.secret))
+		})
+	}
+}
+
+func TestGetCredentialsSecret(t *testing.T) {
+	testCases := []struct {
+		name          string
+		clientBuilder *fake.ClientBuilder
+		assertions    func(*corev1.Secret, error)
+	}{
+		{
+			name:          "no secrets found",
+			clientBuilder: fake.NewClientBuilder(),
+			assertions: func(secret *corev1.Secret, err error) {
+				require.NoError(t, err)
+				require.Nil(t, secret)
+			},
+		},
+
+		{
+			name: "success",
+			clientBuilder: fake.NewClientBuilder().WithObjects(
+				&corev1.Secret{
+					ObjectMeta: v1.ObjectMeta{
+						Name:      "fake-creds",
+						Namespace: "fake-namespace",
+					},
+				},
+			),
+			assertions: func(secret *corev1.Secret, err error) {
+				require.NoError(t, err)
+				require.NotNil(t, secret)
+				require.Equal(t, "fake-creds", secret.Name)
+			},
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			credsDB := &credentialsDB{
+				client: testCase.clientBuilder.Build(),
+			}
+			testCase.assertions(
+				credsDB.getCredentialsSecret(
+					context.Background(),
+					"fake-namespace",
+					labels.Everything(),
+					fields.Everything(),
+				),
+			)
+		})
+	}
+}
+
+func TestGetCredentialsTemplateSecret(t *testing.T) {
+	testCases := []struct {
+		name          string
+		clientBuilder *fake.ClientBuilder
+		assertions    func(*corev1.Secret, error)
+	}{
+		{
+			name:          "no secrets found",
+			clientBuilder: fake.NewClientBuilder(),
+			assertions: func(secret *corev1.Secret, err error) {
+				require.NoError(t, err)
+				require.Nil(t, secret)
+			},
+		},
+
+		{
+			name: "success",
+			clientBuilder: fake.NewClientBuilder().WithObjects(
+				&corev1.Secret{
+					ObjectMeta: v1.ObjectMeta{
+						Name:      "wrong-creds-template",
+						Namespace: "fake-namespace",
+					},
+					Data: nil, // No data
+				},
+				&corev1.Secret{
+					ObjectMeta: v1.ObjectMeta{
+						Name:      "another-wrong-creds-template",
+						Namespace: "fake-namespace",
+					},
+					Data: map[string][]byte{
+						"url": []byte("not-a-match"),
+					},
+				},
+				&corev1.Secret{
+					ObjectMeta: v1.ObjectMeta{
+						Name:      "fake-creds-template",
+						Namespace: "fake-namespace",
+					},
+					Data: map[string][]byte{
+						"url": []byte("fake"),
+					},
+				},
+			),
+			assertions: func(secret *corev1.Secret, err error) {
+				require.NoError(t, err)
+				require.NotNil(t, secret)
+				require.Equal(t, "fake-creds-template", secret.Name)
+			},
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			credsDB := &credentialsDB{
+				client: testCase.clientBuilder.Build(),
+			}
+			testCase.assertions(
+				credsDB.getCredentialsTemplateSecret(
+					context.Background(),
+					"fake-namespace",
+					labels.Everything(),
+					"fake-url",
+				),
+			)
+		})
+	}
+}
+
+// fakeCredentialsDB is a mock implementation of the credentialsDBIface
+// interface that is used to facilitate unit testing.
+type fakeCredentialsDB struct {
+	getFn func(
+		ctx context.Context,
+		namespace string,
+		credType credentialsType,
+		repo string,
+	) (credentials, bool, error)
+}
+
+func (f *fakeCredentialsDB) get(
+	ctx context.Context,
+	namespace string,
+	credType credentialsType,
+	repo string,
+) (credentials, bool, error) {
+	if f.getFn == nil {
+		return credentials{}, false, nil
+	}
+	return f.getFn(ctx, namespace, credType, repo)
+}
