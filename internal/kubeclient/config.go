4e40d18d36652d7d1af64f0d97c8c85c986a6010
Sunghoon Kang <hoon@akuity.io>
2023-05-08 13:37:22 +0900

    Remove explicitPath from config
    https://github.com/akuity/kargo/pull/348#discussion_r1186973587
    
    Signed-off-by: Sunghoon Kang <hoon@akuity.io>

--- internal/kubeclient/config.go
+++ internal/kubeclient/config.go
@@ -8,13 +8,8 @@ import (
 
 var overrides = clientcmd.ConfigOverrides{}
 
-var (
-	explicitPath string
-)
-
 func NewClientConfig() clientcmd.ClientConfig {
 	rules := clientcmd.NewDefaultClientConfigLoadingRules()
 	rules.DefaultClientConfig = &clientcmd.DefaultClientConfig
-	rules.ExplicitPath = explicitPath
 	return clientcmd.NewInteractiveDeferredLoadingClientConfig(rules, &overrides, os.Stdin)
 }
