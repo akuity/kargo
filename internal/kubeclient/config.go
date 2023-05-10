f2e0f80f3865011366b246c871b305372110606f
Sunghoon Kang <hoon@akuity.io>
2023-05-10 12:10:15 +0900

    feat: add kubernetes credential hook for CLI (#348)
    Signed-off-by: Sunghoon Kang <hoon@akuity.io>

---
+++ internal/kubeclient/config.go
@@ -0,0 +1,16 @@
+package kubeclient
+
+import (
+	"os"
+
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+var overrides = clientcmd.ConfigOverrides{}
+
+func NewClientConfig() clientcmd.ClientConfig {
+	rules := clientcmd.NewDefaultClientConfigLoadingRules()
+	rules.DefaultClientConfig = &clientcmd.DefaultClientConfig
+	return clientcmd.NewInteractiveDeferredLoadingClientConfig(
+		rules, &overrides, os.Stdin)
+}
