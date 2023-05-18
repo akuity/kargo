6832462fe076fadfe1054b653f41c1811e232308
Sunghoon Kang <hoon@akuity.io>
2023-05-18 10:34:01 +0900

    Fix lint issue
    Signed-off-by: Sunghoon Kang <hoon@akuity.io>

--- internal/config/config.go
+++ internal/config/config.go
@@ -34,7 +34,7 @@ func NewAPIConfig() APIConfig {
 		Host:       os.MustGetEnv("HOST", "0.0.0.0"),
 		Port:       MustAtoi(os.MustGetEnv("PORT", "8080")),
 
-		LocalMode:  MustParseBool(os.MustGetEnv("LOCAL_MODE", "false")),
+		LocalMode: MustParseBool(os.MustGetEnv("LOCAL_MODE", "false")),
 
 		GracefulShutdownTimeout: MustParseDuration(
 			os.MustGetEnv("GRACEFUL_SHUTDOWN_TIMEOUT", "30s"),
