2600dfe09d0e6d5a874111cb07029c232870e657
Kent <kent.rancourt@gmail.com>
2023-06-21 09:54:59 -0400

    add function for retrieving env var as bool
    Signed-off-by: Kent <kent.rancourt@gmail.com>

--- internal/os/env.go
+++ internal/os/env.go
@@ -3,6 +3,7 @@ package os
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 // GetEnv retrieves the value of an environment variable having the specified
@@ -28,4 +29,26 @@ func MustGetEnv(key, defaultValue string) string {
 		}
 	}
 	return value
+}
+
+// MustGetEnvAsBool attempts to parse a bool from a string value retrieved from
+// the specified environment variable. If the environment variable is undefined,
+// the specified default value is returned instead. If the environment variable
+// is defined and its value cannot successfully be parsed as a bool, the
+// function panics.
+func MustGetEnvAsBool(name string, defaultValue bool) bool {
+	valStr := os.Getenv(name)
+	if valStr == "" {
+		return defaultValue
+	}
+	if val, err := strconv.ParseBool(valStr); err == nil {
+		return val
+	}
+	panic(
+		fmt.Sprintf(
+			"value %q for environment variable %s was not parsable as a bool",
+			valStr,
+			name,
+		),
+	)
 }
