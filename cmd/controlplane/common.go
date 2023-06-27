12d2da6a32a2bb75c9d516e70fdd445a1e6306d1
Kent <kent.rancourt@gmail.com>
2023-06-27 15:12:35 -0400

    simplify/improve how rest configs are loaded
    Signed-off-by: Kent <kent.rancourt@gmail.com>

--- cmd/controlplane/common.go
+++ cmd/controlplane/common.go
@@ -1,34 +1,27 @@
 package main
 
 import (
-	"strings"
+	"context"
 
+	"github.com/akuity/kargo/internal/logging"
 	"github.com/pkg/errors"
 	"k8s.io/client-go/rest"
-	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
-func getRestConfig(
-	cfgCtx string,
-	preferInClusterCfg bool,
-) (*rest.Config, error) {
-	var cfg *rest.Config
-	var err error
-	if preferInClusterCfg {
-		if cfg, err = rest.InClusterConfig(); err != nil {
-			return nil, errors.Wrapf(err, "error loading in-cluster rest config")
-		}
-		return cfg, nil
+func getRestConfig(ctx context.Context, path string) (*rest.Config, error) {
+	logger := logging.LoggerFromContext(ctx)
+
+	// clientcmd.BuildConfigFromFlags will fall back on in-cluster config if path
+	// is empty, but will issue a warning that we can suppress by checking for
+	// that condition ourselves and calling rest.InClusterConfig() directly.
+	if path == "" {
+		logger.Debug("loading in-cluster REST config")
+		cfg, err := rest.InClusterConfig()
+		return cfg, errors.Wrap(err, "error loading in-cluster REST config")
 	}
-	if cfg, err = config.GetConfigWithContext(cfgCtx); err != nil {
-		if strings.Contains(err.Error(), "does not exist") {
-			if cfg, err = config.GetConfig(); err != nil {
-				return nil, errors.Wrapf(err, "error loading default rest config")
-			}
-			return cfg, nil
-		}
-		return nil,
-			errors.Wrapf(err, "error loading rest config for context %q", cfgCtx)
-	}
-	return cfg, nil
+
+	logger.WithField("path", path).Debug("loading REST config from path")
+	cfg, err := clientcmd.BuildConfigFromFlags("", path)
+	return cfg, errors.Wrapf(err, "error loading REST config from %q", path)
 }
