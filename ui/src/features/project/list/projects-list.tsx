1895730e8fd11c1573e4721b2af61c104ab637d2
Rafal Pelczar <rafal@akuity.io>
2023-07-13 02:48:08 +0200

    cleanup
    Signed-off-by: Rafal Pelczar <rafal@akuity.io>

--- ui/src/features/project/list/projects-list.tsx
+++ ui/src/features/project/list/projects-list.tsx
@@ -1,17 +1,23 @@
 import { useQuery } from '@tanstack/react-query';
+import { Empty } from 'antd';
 
+import { LoadingState } from '@ui/features/common';
 import { listProjects } from '@ui/gen/service/v1alpha1/service-KargoService_connectquery';
 
 import { ProjectItem } from './project-item/project-item';
 import * as styles from './projects-list.module.less';
 
 export const ProjectsList = () => {
-  const { data } = useQuery(listProjects.useQuery({}));
+  const { data, isLoading } = useQuery(listProjects.useQuery({}));
+
+  if (isLoading) return <LoadingState />;
+
+  if (!data || data.projects.length === 0) return <Empty />;
 
   return (
     <>
       <div className={styles.list}>
-        {data?.projects.map((project) => (
+        {data.projects.map((project) => (
           <ProjectItem key={project.name} name={project.name} />
         ))}
       </div>
--- ui/src/pages/project.tsx
+++ ui/src/pages/project.tsx
@@ -1,81 +1,22 @@
-import { useQuery } from '@tanstack/react-query';
-import { Drawer, Typography } from 'antd';
+import { Typography } from 'antd';
 import React from 'react';
-import { generatePath, useNavigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
-import { paths } from '@ui/config/paths';
-import { HealthStatusIcon } from '@ui/features/common/health-status-icon/health-status-icon';
-import { listStages } from '@ui/gen/service/v1alpha1/service-KargoService_connectquery';
-import { Stage } from '@ui/gen/v1alpha1/generated_pb';
+import { ProjectDetails } from '@ui/features/project/project-details/project-details';
 
 import { StageDetails } from '../features/stage/stage-details';
 
-import * as styles from './project.module.less';
-
 export const Project = () => {
-  const { name, stageName } = useParams();
-  const { data, refetch } = useQuery(listStages.useQuery({ project: name }));
-
-  const stagesByName = (data?.stages || []).reduce((acc, stage) => {
-    if (stage.metadata?.name) {
-      acc[stage.metadata?.name] = stage;
-    }
-    return acc;
-  }, {} as Record<string, Stage>);
-  const [currentStage, setCurrentStage] = React.useState<string | null>(stageName || null);
-
-  const navigate = useNavigate();
-
-  const openStage = (stageName: string) => {
-    setCurrentStage(stageName);
-    navigate(generatePath(paths.stage, { name, stageName }));
-  };
-
-  const closeStage = () => {
-    setCurrentStage(null);
-    navigate(generatePath(paths.project, { name }));
-  };
-
-  React.useEffect(() => {
-    if (stageName) {
-      openStage(stageName);
-    }
-  }, [stageName]);
+  const { name } = useParams();
 
   return (
-    <div>
-      <Drawer
-        open={currentStage !== null}
-        onClose={() => closeStage()}
-        width={'80%'}
-        closable={false}
-      >
-        <StageDetails stage={stagesByName[currentStage || '']} refetch={refetch} />
-      </Drawer>
+    <>
       <Typography.Title level={1}>{name}</Typography.Title>
       <Typography.Title level={3} className='!mt-0 !mb-6'>
         Stages
       </Typography.Title>
-      {(data?.stages || []).map((stage) => (
-        <StageItem
-          key={stage.metadata?.name}
-          stage={stage}
-          onClick={() => stage?.metadata?.name && openStage(stage.metadata.name)}
-        />
-      ))}
-    </div>
-  );
-};
-
-const StageItem = (props: { stage: Stage; onClick: () => void }) => {
-  const { stage } = props;
-  return (
-    <div key={stage.metadata?.name} onClick={props.onClick} className={styles.item}>
-      <HealthStatusIcon
-        health={stage.status?.currentState?.health}
-        style={{ marginRight: '12px' }}
-      />
-      {stage.metadata?.name}
-    </div>
+      <ProjectDetails />
+      <StageDetails />
+    </>
   );
 };
