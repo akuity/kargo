d051a558e33c0c114508bc9d887608a66d7fb7e5
Sunghoon Kang <hoon@akuity.io>
2023-07-14 02:12:12 +0900

    Generate JSON schema from CRD manifests (#464)
    Signed-off-by: Sunghoon Kang <hoon@akuity.io>
    Co-authored-by: Kent Rancourt <kent.rancourt@gmail.com>

---
+++ ui/hack/generate-schema.mjs
@@ -0,0 +1,24 @@
+#!/usr/bin/env zx
+
+/* eslint-disable no-undef */
+
+import { openapiSchemaToJsonSchema } from '@openapi-contrib/openapi-schema-to-json-schema';
+import jsonStringify from 'json-stable-stringify';
+
+const crdDir = path.join(__dirname, '../../charts/kargo-kit/crds');
+const crdFiles = (await $`ls '${crdDir}'`.quiet()).stdout
+  .split('\n')
+  .filter((f) => f.endsWith('.yaml'));
+const outDir = path.join(__dirname, '../src/gen/schema');
+await $`mkdir -p '${outDir}'`.quiet();
+await $`rm -r '${outDir}/'`.quiet();
+
+for (const crdFile of crdFiles) {
+  const crd = YAML.parse(await fs.readFile(path.join(crdDir, crdFile), 'utf8'));
+  const name = crd.metadata.name;
+  for (const version of crd.spec.versions) {
+    const outputPath = path.join(outDir, `${name}_${version.name}.json`);
+    const schema = openapiSchemaToJsonSchema(version.schema);
+    await fs.outputFile(outputPath, jsonStringify(schema, { space: 2 }));
+  }
+}
