d7b174c86c26d0704539323c1d3c4b241c6f1e58
Sunghoon Kang <hoon@akuity.io>
2023-07-14 11:08:02 +0900

    Check if namespace has `kargo.akuity.io/project` label
    https://github.com/akuity/kargo/pull/455#discussion_r1262756737
    
    Signed-off-by: Sunghoon Kang <hoon@akuity.io>

--- internal/api/handler/create_stage_v1alpha1.go
+++ internal/api/handler/create_stage_v1alpha1.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/bufbuild/connect-go"
 	"github.com/pkg/errors"
+	corev1 "k8s.io/api/core/v1"
 	kubeerr "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/yaml"
@@ -49,14 +50,23 @@ func CreateStageV1Alpha1(
 		default:
 			return nil, connect.NewError(connect.CodeInvalidArgument, errors.New("environment should not be empty"))
 		}
-		if err := kc.Create(ctx, &stage); err != nil {
+
+		var ns corev1.Namespace
+		if err := kc.Get(ctx, client.ObjectKey{Name: stage.GetNamespace()}, &ns); err != nil {
 			if kubeerr.IsNotFound(err) {
 				return nil, connect.NewError(connect.CodeNotFound, errors.Errorf("project %q not found", stage.GetNamespace()))
 			}
+			return nil, connect.NewError(connect.CodeInternal, errors.Wrap(err, "failed to get project"))
+		}
+		if ns.GetLabels()["kargo.akuity.io/project"] != "true" {
+			return nil, connect.NewError(connect.CodeFailedPrecondition, errors.Errorf("namespace %q is not a project", stage.GetNamespace()))
+		}
+
+		if err := kc.Create(ctx, &stage); err != nil {
 			if kubeerr.IsAlreadyExists(err) {
 				return nil, connect.NewError(connect.CodeAlreadyExists, err)
 			}
-			return nil, connect.NewError(connect.CodeInternal, err)
+			return nil, connect.NewError(connect.CodeInternal, errors.Wrap(err, "failed to create stage"))
 		}
 		return connect.NewResponse(&svcv1alpha1.CreateStageResponse{
 			Stage: toStageProto(stage),
