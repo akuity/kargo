7e8513d281bb61b1b9f9765c8313ca2758f6960a
Sunghoon Kang <hoon@akuity.io>
2023-08-02 14:13:28 +0900

    Validate project in webhook (#500)
    Signed-off-by: Sunghoon Kang <hoon@akuity.io>

---
+++ internal/api/validation/project.go
@@ -0,0 +1,33 @@
+package validation
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/pkg/errors"
+	corev1 "k8s.io/api/core/v1"
+	kubeerr "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/akuity/kargo/api/v1alpha1"
+)
+
+var (
+	ErrProjectNotFound = errors.New("project not found")
+)
+
+func ValidateProject(ctx context.Context, kc client.Client, project string) error {
+	var ns corev1.Namespace
+	if err := kc.Get(ctx, client.ObjectKey{Name: project}, &ns); err != nil {
+		if kubeerr.IsNotFound(err) {
+			return ErrProjectNotFound
+		}
+		return errors.Wrap(err, "get project")
+	}
+	if ns.GetLabels()[v1alpha1.LabelProjectKey] != v1alpha1.LabelTrueValue {
+		return field.Invalid(field.NewPath("metadata", "namespace"),
+			project, fmt.Sprintf("namespace %q is not a project", project))
+	}
+	return nil
+}
