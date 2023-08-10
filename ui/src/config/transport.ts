32c56fc497c5fe7b53c2e9afb0ae43c1d4db602a
Rafal <rafal@akuity.io>
2023-08-11 00:32:38 +0200

    feat(ui): Add global API requests error handling (#477)
    Signed-off-by: Rafal Pelczar <rafal@akuity.io>

--- ui/src/config/transport.ts
+++ ui/src/config/transport.ts
@@ -1,5 +1,20 @@
+import { ConnectError, Interceptor } from '@bufbuild/connect';
 import { createConnectTransport } from '@bufbuild/connect-web';
+import { notification } from 'antd';
+
+const errorHandler: Interceptor = (next) => async (req) => {
+  try {
+    return await next(req);
+  } catch (err) {
+    if (err instanceof ConnectError) {
+      notification.error({ message: err.rawMessage, placement: 'bottomRight' });
+    }
+
+    throw err;
+  }
+};
 
 export const transport = createConnectTransport({
-  baseUrl: '/api'
+  baseUrl: '/api',
+  interceptors: [errorHandler]
 });
