6abc042cea22cd3291e89be79cbda4502d84715e
Kent Rancourt <kent.rancourt@gmail.com>
2023-08-23 17:26:37 -0400

    add optional field to image subscriptions to link it back to a source CODE (not config) repo  (#583)
    Signed-off-by: Kent <kent.rancourt@gmail.com>

--- internal/controller/stages/images.go
+++ internal/controller/stages/images.go
@@ -2,7 +2,10 @@ package stages
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
+	"github.com/argoproj/argo-cd/v2/util/git"
 	"github.com/pkg/errors"
 
 	api "github.com/akuity/kargo/api/v1alpha1"
@@ -58,11 +61,29 @@ func (r *reconciler) getLatestImages(
 			)
 		}
 		imgs[i] = api.Image{
-			RepoURL: sub.RepoURL,
-			Tag:     tag,
+			RepoURL:    sub.RepoURL,
+			GitRepoURL: r.getImageSourceURL(sub.GitRepoURL, tag),
+			Tag:        tag,
 		}
 		logger.WithField("tag", tag).
 			Debug("found latest suitable image tag")
 	}
 	return imgs, nil
+}
+
+const (
+	githubURLPrefix = "https://github.com"
+)
+
+func (r *reconciler) getImageSourceURL(gitRepoURL, tag string) string {
+	for baseUrl, fn := range r.imageSourceURLFnsByBaseURL {
+		if strings.HasPrefix(gitRepoURL, baseUrl) {
+			return fn(gitRepoURL, tag)
+		}
+	}
+	return ""
+}
+
+func getGithubImageSourceURL(gitRepoURL, tag string) string {
+	return fmt.Sprintf("%s/tree/%s", git.NormalizeGitURL(gitRepoURL), tag)
 }
--- internal/controller/stages/images_test.go
+++ internal/controller/stages/images_test.go
@@ -3,6 +3,7 @@ package stages
 import (
 	"context"
 	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -115,6 +116,71 @@ func TestGetLatestImages(t *testing.T) {
 					"fake-namespace",
 					testSubs,
 				),
+			)
+		})
+	}
+}
+
+func TestFetImageSourceURL(t *testing.T) {
+	const testURLPrefix = "fake-url-prefix"
+	testCases := []struct {
+		name        string
+		reconciler  *reconciler
+		expectedURL string
+	}{
+		{
+			name:        "no image source URL function found",
+			reconciler:  &reconciler{},
+			expectedURL: "",
+		},
+		{
+			name: "image source URL function found",
+			reconciler: &reconciler{
+				imageSourceURLFnsByBaseURL: map[string]func(string, string) string{
+					testURLPrefix: func(string, string) string {
+						return "fake-url"
+					},
+				},
+			},
+			expectedURL: "fake-url",
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			require.Equal(
+				t,
+				testCase.expectedURL,
+				testCase.reconciler.getImageSourceURL(testURLPrefix, "fake-tag"),
+			)
+		})
+	}
+}
+
+func TestGetGithubImageSourceURL(t *testing.T) {
+	const testTag = "fake-tag"
+	testCases := []struct {
+		name    string
+		baseURL string
+	}{
+		{
+			name:    "with .git suffix",
+			baseURL: "https://github.com/akuity/kargo.git",
+		},
+		{
+			name:    "without .git suffix",
+			baseURL: "https://github.com/akuity/kargo",
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			require.Equal(
+				t,
+				fmt.Sprintf(
+					"%s/tree/%s",
+					"https://github.com/akuity/kargo",
+					testTag,
+				),
+				getGithubImageSourceURL(testCase.baseURL, testTag),
 			)
 		})
 	}
