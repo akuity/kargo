665232d5dfa14ce1827a0dcb7e5003372c7bd36a
Kent Rancourt <kent.rancourt@gmail.com>
2023-08-25 10:57:15 -0400

    fix ISS claim on admin tokens (#594)
    Signed-off-by: Kent <kent.rancourt@gmail.com>

--- internal/api/config/config.go
+++ internal/api/config/config.go
@@ -15,11 +15,14 @@ import (
 type AdminConfig struct {
 	// HashedPassword is a bcrypt hash of the password for the admin account.
 	HashedPassword string `envconfig:"ADMIN_ACCOUNT_PASSWORD_HASH" required:"true"`
+	// TokenIssuer is the value to be used in the ISS claim of ID tokens issued for
+	// the admin account.
+	TokenIssuer string `envconfig:"ADMIN_ACCOUNT_TOKEN_ISSUER" required:"true"`
 	// TokenAudience is the value to be used in the AUD claim of ID tokens issued
 	// for the admin account.
 	TokenAudience string `envconfig:"ADMIN_ACCOUNT_TOKEN_AUDIENCE" required:"true"`
 	// TokenSigningKey is the key used to sign ID tokens for the admin account.
-	TokenSigningKey []byte `envconfig:"TOKEN_SIGNING_KEY" required:"true"`
+	TokenSigningKey []byte `envconfig:"ADMIN_ACCOUNT_TOKEN_SIGNING_KEY" required:"true"`
 }
 
 // AdminConfigFromEnv returns an AdminConfig populated from environment
