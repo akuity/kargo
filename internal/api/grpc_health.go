4d9e21b7d403cf7b7931ce6cdb7e26f68a3b653a
Sunghoon Kang <hoon@akuity.io>
2023-09-04 18:47:45 +0900

    Add liveness probe to Kargo API deployment (#632)
    Fixes #550
    
    Signed-off-by: Sunghoon Kang <hoon@akuity.io>

--- internal/api/grpc_health.go
+++ internal/api/grpc_health.go
@@ -1,9 +1,13 @@
 package api
 
 import (
-	grpchealth "connectrpc.com/grpchealth"
+	"connectrpc.com/grpchealth"
+
+	"github.com/akuity/kargo/pkg/api/service/v1alpha1/svcv1alpha1connect"
 )
 
 func NewHealthChecker() grpchealth.Checker {
-	return grpchealth.NewStaticChecker()
+	return grpchealth.NewStaticChecker(
+		svcv1alpha1connect.KargoServiceName,
+	)
 }
