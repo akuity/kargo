bdad0347780a67e71e76f7856f57a2d7ad1d48b4
Sunghoon Kang <hoon@akuity.io>
2023-09-07 14:42:19 +0900

    Add CLI commands for create/apply/delete resources with manifest
    Signed-off-by: Sunghoon Kang <hoon@akuity.io>

--- internal/cli/create/project.go
+++ internal/cli/create/project.go
@@ -1,34 +1,68 @@
 package create
 
 import (
-	"context"
+	"fmt"
+	"strings"
 
 	"connectrpc.com/connect"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/utils/pointer"
 
 	kargoapi "github.com/akuity/kargo/api/v1alpha1"
-	v1alpha1 "github.com/akuity/kargo/pkg/api/service/v1alpha1"
-	"github.com/akuity/kargo/pkg/api/service/v1alpha1/svcv1alpha1connect"
+	"github.com/akuity/kargo/internal/cli/client"
+	"github.com/akuity/kargo/internal/cli/option"
+	kargosvcapi "github.com/akuity/kargo/pkg/api/service/v1alpha1"
 )
 
-func createProject(
-	ctx context.Context,
-	kargoSvcCli svcv1alpha1connect.KargoServiceClient,
-	name string,
-) (runtime.Object, error) {
-	resp, err := kargoSvcCli.CreateProject(ctx, connect.NewRequest(&v1alpha1.CreateProjectRequest{
-		Name: name,
-	}))
-	if err != nil {
-		return nil, err
-	}
+func newProjectCommand(opt *option.Option) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "project (NAME)",
+		Short: "Create a project",
+		Args:  cobra.MinimumNArgs(1),
+		Example: `
+# Create project
+kargo create project my-project
+`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx := cmd.Context()
 
-	var project unstructured.Unstructured
-	project.SetAPIVersion(kargoapi.GroupVersion.String())
-	project.SetKind("Project")
-	project.SetCreationTimestamp(metav1.NewTime(resp.Msg.GetProject().GetCreateTime().AsTime()))
-	project.SetName(resp.Msg.GetProject().GetName())
-	return &project, nil
+			name := strings.TrimSpace(args[0])
+			if name == "" {
+				return errors.New("name is required")
+			}
+
+			kargoSvcCli, err := client.GetClientFromConfig(ctx, opt)
+			if err != nil {
+				return errors.New("get client from config")
+			}
+			resp, err := kargoSvcCli.CreateProject(ctx,
+				connect.NewRequest(&kargosvcapi.CreateProjectRequest{
+					Name: name,
+				}))
+			if err != nil {
+				return errors.Wrap(err, "create project")
+			}
+
+			var project unstructured.Unstructured
+			project.SetAPIVersion(kargoapi.GroupVersion.String())
+			project.SetKind("Project")
+			project.SetCreationTimestamp(metav1.NewTime(resp.Msg.GetProject().GetCreateTime().AsTime()))
+			project.SetName(resp.Msg.GetProject().GetName())
+
+			if pointer.StringDeref(opt.PrintFlags.OutputFormat, "") == "" {
+				_, _ = fmt.Fprintf(opt.IOStreams.Out, "Project Created: %q\n", name)
+				return nil
+			}
+			printer, err := opt.PrintFlags.ToPrinter()
+			if err != nil {
+				return errors.Wrap(err, "new printer")
+			}
+			return printer.PrintObj(&project, opt.IOStreams.Out)
+		},
+	}
+	opt.PrintFlags.AddFlags(cmd)
+	return cmd
 }
--- internal/cli/delete/stage.go
+++ internal/cli/delete/stage.go
@@ -1,23 +1,55 @@
 package delete
 
 import (
-	"context"
+	"errors"
+	"fmt"
 
 	"connectrpc.com/connect"
+	pkgerrors "github.com/pkg/errors"
+	"github.com/spf13/cobra"
+	"golang.org/x/exp/slices"
 
+	"github.com/akuity/kargo/internal/cli/client"
+	"github.com/akuity/kargo/internal/cli/option"
 	v1alpha1 "github.com/akuity/kargo/pkg/api/service/v1alpha1"
-	"github.com/akuity/kargo/pkg/api/service/v1alpha1/svcv1alpha1connect"
 )
 
-func deleteStage(
-	ctx context.Context,
-	kargoSvcCli svcv1alpha1connect.KargoServiceClient,
-	project string,
-	name string,
-) error {
-	_, err := kargoSvcCli.DeleteStage(ctx, connect.NewRequest(&v1alpha1.DeleteStageRequest{
-		Project: project,
-		Name:    name,
-	}))
-	return err
+func newStageCommand(opt *option.Option) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "stage [NAME]...",
+		Short: "Delete stage by name",
+		Args:  cobra.MinimumNArgs(1),
+		Example: `
+# Delete stage
+kargo delete stage --project=my-project my-stage
+`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx := cmd.Context()
+			kargoSvcCli, err := client.GetClientFromConfig(ctx, opt)
+			if err != nil {
+				return pkgerrors.New("get client from config")
+			}
+
+			project := opt.Project.OrElse("")
+			if project == "" {
+				return errors.New("project is required")
+			}
+
+			var resErr error
+			for _, name := range slices.Compact(args) {
+				if _, err := kargoSvcCli.DeleteStage(ctx, connect.NewRequest(&v1alpha1.DeleteStageRequest{
+					Project: project,
+					Name:    name,
+				})); err != nil {
+					resErr = errors.Join(resErr, pkgerrors.Wrap(err, "Error"))
+					continue
+				}
+				_, _ = fmt.Fprintf(opt.IOStreams.Out, "Stage Deleted: %q\n", name)
+			}
+			return resErr
+		},
+	}
+	opt.PrintFlags.AddFlags(cmd)
+	option.OptionalProject(opt.Project)(cmd.Flags())
+	return cmd
 }
