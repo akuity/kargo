a72bd35be4fc9298a8ef0570decd7c2ebedf2cbd
Alexander Matyushentsev <AMatyushentsev@gmail.com>
2023-09-08 16:07:54 -0700

    feat: render stages DAG using React & dagre library
    Signed-off-by: Alexander Matyushentsev <AMatyushentsev@gmail.com>

---
+++ ui/src/features/project/project-details/stage-node.tsx
@@ -0,0 +1,22 @@
+import { HealthStatusIcon } from '@ui/features/common/health-status/health-status-icon';
+import { Stage } from '@ui/gen/v1alpha1/types_pb';
+
+import * as styles from './stage-node.module.less';
+
+export const StageNode = ({ stage, color }: { stage: Stage; color: string }) => (
+  <div className={styles.node} style={{ backgroundColor: color }}>
+    <h3>
+      {stage.metadata?.name}
+      {stage.status?.health && (
+        <HealthStatusIcon
+          health={stage.status?.health}
+          style={{ position: 'absolute', right: '1em' }}
+        />
+      )}
+    </h3>
+    <div className={styles.body}>
+      <h3>Current Freight</h3>
+      <p>{stage.status?.currentFreight?.id?.slice(0, 7) || 'N/A'} </p>
+    </div>
+  </div>
+);
