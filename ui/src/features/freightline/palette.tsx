4b46444a6206b4484c15fa681c2a126c89a547cd
Remington Breeze <remington@breeze.software>
2023-09-09 16:04:57 -0700

    feat: add button to promote subscribers from DAG
    Signed-off-by: Remington Breeze <remington@breeze.software>

--- ui/src/features/freightline/palette.tsx
+++ ui/src/features/freightline/palette.tsx
@@ -3,7 +3,11 @@ import { getBackground } from '@ui/utils/stages';
 export const Palette = () => (
   <div className='flex items-center p-6 bg-zinc-900'>
     {Array.from('x'.repeat(15)).map((_, i) => (
-      <div key={i} className={`h-6 w-6 mr-4 rounded-md ${getBackground(i)}`} />
+      <div
+        key={i}
+        className={`h-6 w-6 mr-4 rounded-md`}
+        style={{ backgroundColor: getBackground(i) }}
+      />
     ))}
   </div>
 );
