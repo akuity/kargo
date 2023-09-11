d52f5fafed1732b6b9b30e6bf222703722e55839
Kent <kent.rancourt@gmail.com>
2023-09-11 18:15:50 -0400

    actual controller changes
    Signed-off-by: Kent <kent.rancourt@gmail.com>

--- internal/controller/stages/argocd.go
+++ internal/controller/stages/argocd.go
@@ -21,75 +21,121 @@ func (r *reconciler) checkHealth(
 
 	health := kargoapi.Health{
 		// We'll start healthy and degrade as we find issues
-		Status: kargoapi.HealthStateHealthy,
-		Issues: []string{},
+		Status:     kargoapi.HealthStateHealthy,
+		ArgoCDApps: make([]kargoapi.ArgoCDAppStatus, len(argoCDAppUpdates)),
+		Issues:     []string{},
 	}
 
-	for _, check := range argoCDAppUpdates {
-		app, err := r.getArgoCDAppFn(ctx, r.argoClient, check.AppNamespaceOrDefault(), check.AppName)
+	for i, updates := range argoCDAppUpdates {
+		health.ArgoCDApps[i] = kargoapi.ArgoCDAppStatus{
+			Namespace: updates.AppNamespaceOrDefault(),
+			Name:      updates.AppName,
+		}
+
+		app, err := r.getArgoCDAppFn(
+			ctx,
+			r.argoClient,
+			updates.AppNamespaceOrDefault(),
+			updates.AppName,
+		)
+
 		if err != nil {
-			if health.Status != kargoapi.HealthStateUnhealthy {
-				health.Status = kargoapi.HealthStateUnknown
+			health.ArgoCDApps[i].HealthStatus = kargoapi.ArgoCDAppHealthStatus{
+				Status: kargoapi.ArgoCDAppHealthStateUnknown,
 			}
+			health.ArgoCDApps[i].SyncStatus = kargoapi.ArgoCDAppSyncStatus{
+				Status: kargoapi.ArgoCDAppSyncStateUnknown,
+			}
+			health.Status = health.Status.Merge(kargoapi.HealthStateUnknown)
 			health.Issues = append(
 				health.Issues,
 				fmt.Sprintf(
 					"error finding Argo CD Application %q in namespace %q: %s",
-					check.AppName,
-					check.AppNamespaceOrDefault(),
+					updates.AppName,
+					updates.AppNamespaceOrDefault(),
 					err,
 				),
 			)
-		} else if app == nil {
-			if health.Status != kargoapi.HealthStateUnhealthy {
-				health.Status = kargoapi.HealthStateUnknown
+			continue
+		}
+
+		if app == nil {
+			health.ArgoCDApps[i].HealthStatus = kargoapi.ArgoCDAppHealthStatus{
+				Status: kargoapi.ArgoCDAppHealthStateUnknown,
 			}
+			health.ArgoCDApps[i].SyncStatus = kargoapi.ArgoCDAppSyncStatus{
+				Status: kargoapi.ArgoCDAppSyncStateUnknown,
+			}
+			health.Status = health.Status.Merge(kargoapi.HealthStateUnknown)
 			health.Issues = append(
 				health.Issues,
 				fmt.Sprintf(
 					"unable to find Argo CD Application %q in namespace %q",
-					check.AppName,
-					check.AppNamespaceOrDefault(),
+					updates.AppName,
+					updates.AppNamespaceOrDefault(),
 				),
 			)
-		} else if len(app.Spec.Sources) > 0 {
-			if health.Status != kargoapi.HealthStateUnhealthy {
-				health.Status = kargoapi.HealthStateUnknown
-			}
+			continue
+		}
+
+		health.ArgoCDApps[i].HealthStatus = kargoapi.ArgoCDAppHealthStatus{
+			Status:  kargoapi.ArgoCDAppHealthState(app.Status.Health.Status),
+			Message: app.Status.Health.Message,
+		}
+		health.ArgoCDApps[i].SyncStatus = kargoapi.ArgoCDAppSyncStatus{
+			Status:    kargoapi.ArgoCDAppSyncState(app.Status.Sync.Status),
+			Revision:  app.Status.Sync.Revision,
+			Revisions: app.Status.Sync.Revisions,
+		}
+
+		// TODO: We should re-evaluate this soon. It may have been fixed in recent
+		// versions.
+		if len(app.Spec.Sources) > 0 {
+			health.Status = health.Status.Merge(kargoapi.HealthStateUnknown)
 			health.Issues = append(
 				health.Issues,
 				fmt.Sprintf(
 					"bugs in Argo CD currently prevent a comprehensive assessment of "+
 						"the health of multi-source Application %q in namespace %q",
-					check.AppName,
-					check.AppNamespaceOrDefault(),
+					updates.AppName,
+					updates.AppNamespaceOrDefault(),
 				),
 			)
-		} else {
-			health = health.Merge(stageHealthForAppHealth(app))
-			if currentFreight != nil {
-				var desiredRevision string
-				for _, commit := range currentFreight.Commits {
-					if commit.RepoURL == app.Spec.Source.RepoURL {
-						if commit.HealthCheckCommit != "" {
-							desiredRevision = commit.HealthCheckCommit
-						} else {
-							desiredRevision = commit.ID
-						}
-					}
-					break
-				}
-				if desiredRevision == "" {
-					for _, chart := range currentFreight.Charts {
-						if chart.RegistryURL == app.Spec.Source.RepoURL &&
-							chart.Name == app.Spec.Source.Chart {
-							desiredRevision = chart.Version
-							break
-						}
+			continue
+		}
+
+		stageHealth, issue := stageHealthForAppHealth(app)
+		health.Status = health.Status.Merge(stageHealth)
+		if issue != "" {
+			health.Issues = append(health.Issues, issue)
+		}
+
+		if currentFreight != nil {
+			var desiredRevision string
+			for _, commit := range currentFreight.Commits {
+				if commit.RepoURL == app.Spec.Source.RepoURL {
+					if commit.HealthCheckCommit != "" {
+						desiredRevision = commit.HealthCheckCommit
+					} else {
+						desiredRevision = commit.ID
 					}
 				}
-				if desiredRevision != "" {
-					health = health.Merge(stageHealthForAppSync(app, desiredRevision))
+				break
+			}
+			if desiredRevision == "" {
+				for _, chart := range currentFreight.Charts {
+					if chart.RegistryURL == app.Spec.Source.RepoURL &&
+						chart.Name == app.Spec.Source.Chart {
+						desiredRevision = chart.Version
+						break
+					}
+				}
+			}
+			if desiredRevision != "" {
+				stageHealth, issue = stageHealthForAppSync(app, desiredRevision)
+				health.Status = health.Status.Merge(stageHealth)
+				if issue != "" {
+					health.Issues = append(health.Issues, issue)
 				}
 			}
 		}
@@ -98,40 +144,49 @@ func (r *reconciler) checkHealth(
 	return &health
 }
 
-func stageHealthForAppHealth(app *argocd.Application) kargoapi.Health {
+func stageHealthForAppHealth(
+	app *argocd.Application,
+) (kargoapi.HealthState, string) {
 	switch app.Status.Health.Status {
 	case argohealth.HealthStatusDegraded, argohealth.HealthStatusUnknown:
-		return kargoapi.Health{
-			Status: kargoapi.HealthStateUnhealthy,
-			Issues: []string{fmt.Sprintf("Argo CD Application %q in namespace %q has health state %q",
-				app.Name, app.Namespace, app.Status.Health.Status)},
-		}
+		return kargoapi.HealthStateUnhealthy,
+			fmt.Sprintf(
+				"Argo CD Application %q in namespace %q has health state %q",
+				app.Name,
+				app.Namespace,
+				app.Status.Health.Status,
+			)
 	case argohealth.HealthStatusProgressing, "":
-		return kargoapi.Health{
-			Status: kargoapi.HealthStateProgressing,
-			Issues: []string{fmt.Sprintf("Argo CD Application %q in namespace %q is progressing",
-				app.Name, app.Namespace)},
-		}
-	default:
-		return kargoapi.Health{Status: kargoapi.HealthStateHealthy}
+		return kargoapi.HealthStateProgressing,
+			fmt.Sprintf(
+				"Argo CD Application %q in namespace %q is progressing",
+				app.Name,
+				app.Namespace,
+			)
 	}
+	return kargoapi.HealthStateHealthy, ""
 }
 
-func stageHealthForAppSync(app *argocd.Application, revision string) kargoapi.Health {
+func stageHealthForAppSync(
+	app *argocd.Application,
+	revision string,
+) (kargoapi.HealthState, string) {
 	if revision != "" && app.Status.Sync.Revision != revision {
-
 		if app.Operation != nil && app.Operation.Sync != nil {
-			return kargoapi.Health{
-				Status: kargoapi.HealthStateProgressing,
-				Issues: []string{fmt.Sprintf("Argo CD Application %q in namespace %q is being synced",
-					app.Name, app.Namespace)},
-			}
+			return kargoapi.HealthStateProgressing,
+				fmt.Sprintf(
+					"Argo CD Application %q in namespace %q is being synced",
+					app.Name,
+					app.Namespace,
+				)
 		}
-		return kargoapi.Health{
-			Status: kargoapi.HealthStateUnhealthy,
-			Issues: []string{fmt.Sprintf("Argo CD Application %q in namespace %q is not synced to revision %q",
-				app.Name, app.Namespace, revision)},
-		}
+		return kargoapi.HealthStateUnhealthy,
+			fmt.Sprintf(
+				"Argo CD Application %q in namespace %q is not synced to revision %q",
+				app.Name,
+				app.Namespace,
+				revision,
+			)
 	}
-	return kargoapi.Health{Status: kargoapi.HealthStateHealthy}
+	return kargoapi.HealthStateHealthy, ""
 }
