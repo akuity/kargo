1b750834113612a6db40b3e65e92e09c1f4968e5
Rafal <rafal@akuity.io>
2023-09-13 03:09:52 +0200

    feat(ui): Helm/Container/Git Repository is shown in DAG (#735)
    Signed-off-by: Rafal Pelczar <rafal@akuity.io>

---
+++ ui/src/features/project/project-details/nodes/repo-node.tsx
@@ -0,0 +1,54 @@
+import { faDocker, faGit } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Tooltip } from 'antd';
+
+import { NodeType, NodesRepoType } from '../types';
+
+import * as styles from './repo-node.module.less';
+
+const MAX_CHARS = 19;
+
+type Props = {
+  nodeData: NodesRepoType;
+  height: number;
+};
+
+const name = {
+  [NodeType.REPO_IMAGE]: 'Image',
+  [NodeType.REPO_GIT]: 'Git',
+  [NodeType.REPO_CHART]: 'Chart'
+};
+
+const ico = {
+  [NodeType.REPO_IMAGE]: faDocker,
+  [NodeType.REPO_GIT]: faGit
+};
+
+export const RepoNode = ({ nodeData, height }: Props) => (
+  <div style={{ height }} className={styles.node}>
+    <h3 className='flex justify-between'>
+      <span>{name[nodeData.type]}</span>
+      {nodeData.type !== NodeType.REPO_CHART && <FontAwesomeIcon icon={ico[nodeData.type]} />}
+    </h3>
+    <div className={styles.body}>
+      {(nodeData.type === NodeType.REPO_IMAGE || nodeData.type === NodeType.REPO_GIT) && (
+        <RepoNodeBody label='Repo URL' value={nodeData.data.repoUrl} />
+      )}
+      {nodeData.type === NodeType.REPO_CHART && (
+        <RepoNodeBody label='Registry URL' value={nodeData.data.registryUrl} />
+      )}
+    </div>
+  </div>
+);
+
+const RepoNodeBody = ({ label, value }: { label: string; value: string }) => (
+  <>
+    <div className={styles.label}>{label}</div>
+    <Tooltip title={value}>
+      <div className={styles.value}>
+        {value.length > MAX_CHARS && '...'}
+        {value.substring(value.length - MAX_CHARS)}
+      </div>
+    </Tooltip>
+  </>
+);
