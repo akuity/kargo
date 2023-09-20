8e99533f8c15cd5bb7e0f8026d5c4e4cb9d8eb01
Kent <kent.rancourt@gmail.com>
2023-09-20 11:36:29 -0400

    script some of the quickstart to make it... quicker
    Signed-off-by: Kent <kent.rancourt@gmail.com>

---
+++ hack/quickstart/kind.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+kind create cluster \
+  --wait 120s \
+  --config - <<EOF
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+name: kargo-quickstart
+nodes:
+- extraPortMappings:
+  - containerPort: 31443 # Argo CD dashboard
+    hostPort: 8443
+  - containerPort: 31444 # Kargo dashboard
+    hostPort: 8444
+  - containerPort: 30081 # test application instance
+    hostPort: 8081
+  - containerPort: 30082 # UAT application instance
+    hostPort: 8082
+  - containerPort: 30083 # prod application instance
+    hostPort: 8083
+  
+EOF
+
+helm install cert-manager cert-manager \
+  --repo https://charts.jetstack.io \
+  --version 1.11.5 \
+  --namespace cert-manager \
+  --create-namespace \
+  --set installCRDs=true \
+  --wait
+
+helm install argocd argo-cd \
+  --repo https://argoproj.github.io/argo-helm \
+  --version 5.46.6 \
+  --namespace argocd \
+  --create-namespace \
+  --set 'configs.secret.argocdServerAdminPassword=$2a$10$5vm8wXaSdbuff0m9l21JdevzXBzJFPCi8sy6OOnpZMAG.fOXL7jvO' \
+  --set dex.enabled=false \
+  --set notifications.enabled=false \
+  --set server.service.type=NodePort \
+  --set server.service.nodePortHttp=31443 \
+  --wait
+
+helm install kargo \
+  oci://ghcr.io/akuity/kargo-charts/kargo \
+  --namespace kargo \
+  --create-namespace \
+  --set api.service.type=NodePort \
+  --set api.service.nodePort=31444 \
+  --set 'api.adminAccount.password=admin' \
+  --wait
