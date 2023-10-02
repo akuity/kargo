8e98df6a034974775a456a70c5aafcf95c39e071
Tal Hason <thason@redhat.com>
2023-10-02 15:30:14 +0300

    reorder docker podman
    Signed-off-by: Tal Hason <thason@redhat.com>

--- check_container.sh
+++ check_container.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Check if Podman is available
-if command -v podman &> /dev/null; then
-    echo "podman"
-    exit 0
-fi
-
 # Check if Docker is available
 if command -v docker &> /dev/null; then
     echo "docker"
+    exit 0
+fi
+
+# Check if Podman is available
+if command -v podman &> /dev/null; then
+    echo "podman"
     exit 0
 fi
 
