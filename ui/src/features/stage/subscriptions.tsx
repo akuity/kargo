edcc7885bfbe4b77b565b4dcc3f631f2037a4e73
Rafal <rafal@akuity.io>
2023-11-21 01:53:28 +0100

    fix(ui): Warehouse subscription is not displayed in Stage details (#1137)
    Signed-off-by: Rafal Pelczar <rafal@akuity.io>

--- ui/src/features/stage/subscriptions.tsx
+++ ui/src/features/stage/subscriptions.tsx
@@ -3,7 +3,6 @@ import { Link, generatePath } from 'react-router-dom';
 
 import { paths } from '@ui/config/paths';
 import { Subscriptions as SubscriptionsType } from '@ui/gen/v1alpha1/types_pb';
-import { urlWithProtocol } from '@ui/utils/url';
 
 export const Subscriptions = (props: {
   subscriptions?: SubscriptionsType;
@@ -18,6 +17,12 @@ export const Subscriptions = (props: {
   return (
     <div>
       <Typography.Title level={3}>Subscriptions</Typography.Title>
+
+      {subscriptions.warehouse && (
+        <Descriptions bordered size='small' column={1} style={{ width: '50%' }}>
+          <Descriptions.Item label='Warehouse'>{subscriptions.warehouse}</Descriptions.Item>
+        </Descriptions>
+      )}
 
       {!!subscriptions.upstreamStages.length && (
         <>
@@ -37,90 +42,6 @@ export const Subscriptions = (props: {
                     {stage.name}
                   </Link>
                 </Descriptions.Item>
-              </Descriptions>
-            ))}
-          </Space>
-        </>
-      )}
-
-      {!!subscriptions.repos?.git.length && (
-        <>
-          <Typography.Title level={5} style={{ marginTop: '.8em' }}>
-            Git Repositories
-          </Typography.Title>
-          <Space direction='vertical' style={{ width: '100%' }}>
-            {subscriptions?.repos.git.map((gitRepo) => (
-              <Descriptions bordered size='small' key={gitRepo.repoUrl} column={1}>
-                <Descriptions.Item label='URL'>
-                  <Typography.Link
-                    href={urlWithProtocol(gitRepo.repoUrl)}
-                    target='_blank'
-                    rel='noreferrer'
-                  >
-                    {gitRepo.repoUrl}
-                  </Typography.Link>
-                </Descriptions.Item>
-                <Descriptions.Item label='Branch'>{gitRepo.branch}</Descriptions.Item>
-              </Descriptions>
-            ))}
-          </Space>
-        </>
-      )}
-
-      {!!subscriptions.repos?.images.length && (
-        <>
-          <Typography.Title level={5} style={{ marginTop: '.8em' }}>
-            Container images
-          </Typography.Title>
-          <Space direction='vertical' style={{ width: '100%' }}>
-            {subscriptions?.repos.images.map((image) => (
-              <Descriptions bordered size='small' key={image.repoUrl}>
-                <Descriptions.Item label='URL'>
-                  <Typography.Link
-                    href={urlWithProtocol(image.repoUrl)}
-                    target='_blank'
-                    rel='noreferrer'
-                  >
-                    {image.repoUrl}
-                  </Typography.Link>
-                </Descriptions.Item>
-                {image.semverConstraint && (
-                  <Descriptions.Item label='Semver Constraint'>
-                    {image.semverConstraint}
-                  </Descriptions.Item>
-                )}
-                <Descriptions.Item label='Update Strategy'>
-                  {image.updateStrategy}
-                </Descriptions.Item>
-              </Descriptions>
-            ))}
-          </Space>
-        </>
-      )}
-
-      {!!subscriptions.repos?.charts.length && (
-        <>
-          <Typography.Title level={5} style={{ marginTop: '.8em' }}>
-            Helm charts
-          </Typography.Title>
-          <Space direction='vertical' style={{ width: '100%' }}>
-            {subscriptions?.repos.charts.map((chart) => (
-              <Descriptions bordered size='small' key={chart.registryUrl}>
-                <Descriptions.Item label='Registry URL'>
-                  <Typography.Link
-                    href={urlWithProtocol(chart.registryUrl)}
-                    target='_blank'
-                    rel='noreferrer'
-                  >
-                    {chart.registryUrl}
-                  </Typography.Link>
-                </Descriptions.Item>
-                {chart.name && <Descriptions.Item label='Name'>{chart.name}</Descriptions.Item>}
-                {chart.semverConstraint && (
-                  <Descriptions.Item label='Semver Constraint'>
-                    {chart.semverConstraint}
-                  </Descriptions.Item>
-                )}
               </Descriptions>
             ))}
           </Space>
