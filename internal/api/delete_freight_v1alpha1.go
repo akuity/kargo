1884d236aaa201a130e8ba8ea3d1fa7c9fb7431d
Sunghoon Kang <hoon@akuity.io>
2023-11-29 12:54:49 +0900

    refactor: remove redundant `get` call
    https://github.com/akuity/kargo/pull/1177#discussion_r1408704682
    
    Signed-off-by: Sunghoon Kang <hoon@akuity.io>

--- internal/api/delete_freight_v1alpha1.go
+++ internal/api/delete_freight_v1alpha1.go
@@ -6,7 +6,7 @@ import (
 	"connectrpc.com/connect"
 	"github.com/pkg/errors"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	"sigs.k8s.io/controller-runtime/pkg/client"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	kargoapi "github.com/akuity/kargo/api/v1alpha1"
 	svcv1alpha1 "github.com/akuity/kargo/pkg/api/service/v1alpha1"
@@ -22,24 +22,18 @@ func (s *server) DeleteFreight(
 	if err := s.validateProject(ctx, req.Msg.GetProject()); err != nil {
 		return nil, err
 	}
-	var freight kargoapi.Freight
-	if err := s.client.Get(
-		ctx,
-		client.ObjectKey{
+	if err := s.client.Delete(ctx, &kargoapi.Freight{
+		ObjectMeta: metav1.ObjectMeta{
 			Namespace: req.Msg.GetProject(),
 			Name:      req.Msg.GetName(),
 		},
-		&freight,
-	); err != nil {
+	}); err != nil {
 		if apierrors.IsNotFound(err) {
 			return nil, connect.NewError(
 				connect.CodeNotFound,
 				errors.Errorf("freight %q not found", req.Msg.GetName()),
 			)
 		}
-		return nil, errors.Wrap(err, "get freight")
-	}
-	if err := s.client.Delete(ctx, &freight); err != nil {
 		return nil, errors.Wrap(err, "delete freight")
 	}
 	return connect.NewResponse(&svcv1alpha1.DeleteFreightResponse{}), nil
