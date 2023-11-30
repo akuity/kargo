f2ed203519183f9c0d69882ec534f8e7fe1f6063
Remington Breeze <remington@breeze.software>
2023-11-30 13:51:37 -0800

    fix(ui): display proper available freight for promote-subscribers
    Signed-off-by: Remington Breeze <remington@breeze.software>

--- ui/src/features/common/freight-label.tsx
+++ ui/src/features/common/freight-label.tsx
@@ -3,9 +3,9 @@ import { Freight } from '@ui/gen/v1alpha1/types_pb';
 const ALIAS_LABEL_KEY = 'kargo.akuity.com/alias';
 
 export const FreightLabel = ({ freight }: { freight?: Freight }) => (
-  <>
+  <span className='truncate'>
     {freight?.metadata?.labels[ALIAS_LABEL_KEY]
       ? freight?.metadata?.labels[ALIAS_LABEL_KEY]
       : freight?.metadata?.name?.substring(0, 7) || 'N/A'}
-  </>
+  </span>
 );
