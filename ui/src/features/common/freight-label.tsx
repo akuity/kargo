725d57aaeb68cc51ebefc5f66efd447023b7b2f3
Remington Breeze <remington@breeze.software>
2023-11-30 09:31:04 -0800

    feat(ui): add freight ID tooltip to freight alias
    Signed-off-by: Remington Breeze <remington@breeze.software>

--- ui/src/features/common/freight-label.tsx
+++ ui/src/features/common/freight-label.tsx
@@ -1,11 +1,18 @@
+import { Tooltip } from 'antd';
+import { useEffect, useState } from 'react';
+
 import { Freight } from '@ui/gen/v1alpha1/types_pb';
 
 const ALIAS_LABEL_KEY = 'kargo.akuity.com/alias';
 
-export const FreightLabel = ({ freight }: { freight?: Freight }) => (
-  <>
-    {freight?.metadata?.labels[ALIAS_LABEL_KEY]
-      ? freight?.metadata?.labels[ALIAS_LABEL_KEY]
-      : freight?.metadata?.name?.substring(0, 7) || 'N/A'}
-  </>
-);
+export const FreightLabel = ({ freight }: { freight?: Freight }) => {
+  const [id, setId] = useState<string>('');
+  const [alias, setAlias] = useState<string | undefined>();
+
+  useEffect(() => {
+    setAlias(freight?.metadata?.labels[ALIAS_LABEL_KEY]);
+    setId(freight?.metadata?.name?.substring(0, 7) || 'N/A');
+  }, [freight]);
+
+  return <>{alias ? <Tooltip title={id}>{alias}</Tooltip> : <>{id}</>}</>;
+};
