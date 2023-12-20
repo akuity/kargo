e6d646ef97c1a77c041bfbcf05d3d8b51ecb81ea
Kent Rancourt <kent.rancourt@gmail.com>
2023-12-20 14:43:34 -0500

    update annotations used for user -> sa mappings
    Signed-off-by: Kent Rancourt <kent.rancourt@gmail.com>

--- api/v1alpha1/annotations.go
+++ api/v1alpha1/annotations.go
@@ -3,7 +3,7 @@ package v1alpha1
 const (
 	AnnotationKeyRefresh = "kargo.akuity.io/refresh"
 
-	AnnotationKeyOIDCEmails   = "rbac.kargo.akuity.io/email"
+	AnnotationKeyOIDCEmails   = "rbac.kargo.akuity.io/emails"
 	AnnotationKeyOIDCGroups   = "rbac.kargo.akuity.io/groups"
-	AnnotationKeyOIDCSubjects = "rbac.kargo.akuity.io/sub"
+	AnnotationKeyOIDCSubjects = "rbac.kargo.akuity.io/subjects"
 )
