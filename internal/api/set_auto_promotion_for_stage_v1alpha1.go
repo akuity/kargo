178b3a319af138df0eefd3876951e25fc6726c75
Kent Rancourt <kent.rancourt@gmail.com>
2024-01-22 13:26:23 -0500

    corresponding api server changes
    Signed-off-by: Kent Rancourt <kent.rancourt@gmail.com>

--- internal/api/set_auto_promotion_for_stage_v1alpha1.go
+++ internal/api/set_auto_promotion_for_stage_v1alpha1.go
@@ -5,12 +5,8 @@ import (
 
 	"connectrpc.com/connect"
 	"github.com/pkg/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	kargoapi "github.com/akuity/kargo/api/v1alpha1"
-	typesv1alpha1 "github.com/akuity/kargo/internal/api/types/v1alpha1"
-	"github.com/akuity/kargo/internal/kubeclient"
 	svcv1alpha1 "github.com/akuity/kargo/pkg/api/service/v1alpha1"
 )
 
@@ -27,37 +23,32 @@ func (s *server) SetAutoPromotionForStage(
 	if err := s.validateProject(ctx, req.Msg.GetProject()); err != nil {
 		return nil, err
 	}
-
-	var policyList kargoapi.PromotionPolicyList
-	if err := s.client.List(ctx, &policyList, client.InNamespace(req.Msg.GetProject()), client.MatchingFields{
-		kubeclient.PromotionPoliciesByStageIndexField: req.Msg.GetStage(),
-	}); err != nil {
-		return nil, errors.Wrap(err, "list promotion policies")
+	project, err := kargoapi.GetProject(ctx, s.client, req.Msg.GetProject())
+	if err != nil {
+		return nil, errors.Wrap(err, "get project")
 	}
-
-	// Since only one PromotionPolicy is allowed per stage,
-	// create if not exists and update if exists.
-	var policy kargoapi.PromotionPolicy
-	if len(policyList.Items) > 0 {
-		policy = policyList.Items[0]
-		policy.EnableAutoPromotion = req.Msg.GetEnable()
-		if err := s.client.Update(ctx, &policy); err != nil {
-			return nil, errors.Wrap(err, "update promotion policy")
+	if project.Spec.PromotionPolicies == nil {
+		project.Spec.PromotionPolicies = []kargoapi.PromotionPolicy{}
+	}
+	var set bool
+	for i, policy := range project.Spec.PromotionPolicies {
+		if policy.Stage == req.Msg.GetStage() {
+			project.Spec.PromotionPolicies[i].EnableAutoPromotion = req.Msg.GetEnable()
+			set = true
+			break
 		}
-	} else {
-		policy = kargoapi.PromotionPolicy{
-			ObjectMeta: metav1.ObjectMeta{
-				Namespace: req.Msg.GetProject(),
-				Name:      req.Msg.GetStage(),
+	}
+	if !set {
+		project.Spec.PromotionPolicies = append(
+			project.Spec.PromotionPolicies,
+			kargoapi.PromotionPolicy{
+				Stage:               req.Msg.GetStage(),
+				EnableAutoPromotion: req.Msg.GetEnable(),
 			},
-			Stage:               req.Msg.GetStage(),
-			EnableAutoPromotion: req.Msg.GetEnable(),
-		}
-		if err := s.client.Create(ctx, &policy); err != nil {
-			return nil, errors.Wrap(err, "create promotion policy")
-		}
+		)
 	}
-	return connect.NewResponse(&svcv1alpha1.SetAutoPromotionForStageResponse{
-		PromotionPolicy: typesv1alpha1.ToPromotionPolicyProto(policy),
-	}), nil
+	if err := s.client.Update(ctx, project); err != nil {
+		return nil, errors.Wrap(err, "update project")
+	}
+	return connect.NewResponse(&svcv1alpha1.SetAutoPromotionForStageResponse{}), nil
 }
