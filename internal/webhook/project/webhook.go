3b03d70034dbe0e339b4e42e6375e28dea767a7d
Kent Rancourt <kent.rancourt@gmail.com>
2024-01-26 19:35:01 -0500

    fix lint error
    Signed-off-by: Kent Rancourt <kent.rancourt@gmail.com>

--- internal/webhook/project/webhook.go
+++ internal/webhook/project/webhook.go
@@ -3,8 +3,8 @@ package project
 import (
 	"context"
 
-	log "github.com/sirupsen/logrus"
 	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
