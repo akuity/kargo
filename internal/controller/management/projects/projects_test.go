1f3430368271e6a6985e18356519f6d0493cf8ce
Kent Rancourt <kent.rancourt@gmail.com>
2024-02-16 10:23:57 -0500

    make management controller able to create project namespaces again
    Signed-off-by: Kent Rancourt <kent.rancourt@gmail.com>

--- internal/controller/management/projects/projects_test.go
+++ internal/controller/management/projects/projects_test.go
@@ -6,7 +6,11 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
@@ -20,6 +24,9 @@ func TestNewReconciler(t *testing.T) {
 	require.NotNil(t, r.getProjectFn)
 	require.NotNil(t, r.syncProjectFn)
 	require.NotNil(t, r.patchProjectStatusFn)
+	require.NotNil(t, r.getNamespaceFn)
+	require.NotNil(t, r.createNamespaceFn)
+	require.NotNil(t, r.updateNamespaceFn)
 }
 
 func TestReconcile(t *testing.T) {
@@ -173,13 +180,203 @@ func TestSyncProject(t *testing.T) {
 		)
 	}{
 		{
-			name: "success",
+			name: "error getting namespace",
 			project: &kargoapi.Project{
 				Status: kargoapi.ProjectStatus{
 					Phase: kargoapi.ProjectPhaseInitializing,
 				},
 			},
-			reconciler: &reconciler{},
+			reconciler: &reconciler{
+				getNamespaceFn: func(
+					context.Context,
+					types.NamespacedName,
+					client.Object,
+					...client.GetOption,
+				) error {
+					return errors.New("something went wrong")
+				},
+			},
+			assertions: func(initialStatus, newStatus kargoapi.ProjectStatus, err error) {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), "something went wrong")
+				require.Contains(t, err.Error(), "error getting namespace")
+				// Status is unchanged
+				require.Equal(t, initialStatus, newStatus)
+			},
+		},
+		{
+			name: "namespace exists, is not owned by project, but is labeled " +
+				"as a project; error updating namespace",
+			project: &kargoapi.Project{},
+			reconciler: &reconciler{
+				getNamespaceFn: func(
+					_ context.Context,
+					_ types.NamespacedName,
+					obj client.Object,
+					_ ...client.GetOption,
+				) error {
+					ns, ok := obj.(*corev1.Namespace)
+					require.True(t, ok)
+					ns.Labels = map[string]string{
+						kargoapi.ProjectLabelKey: kargoapi.LabelTrueValue,
+					}
+					return nil
+				},
+				updateNamespaceFn: func(
+					context.Context,
+					client.Object,
+					...client.UpdateOption,
+				) error {
+					return errors.New("something went wrong")
+				},
+			},
+			assertions: func(initialStatus, newStatus kargoapi.ProjectStatus, err error) {
+				require.Error(t, err)
+				// Status should reflect the failure
+				require.Contains(t, err.Error(), "error updating namespace")
+				require.Contains(t, err.Error(), "something went wrong")
+				// And is otherwise unchanged
+				newStatus.Message = initialStatus.Message
+				require.Equal(t, initialStatus, newStatus)
+			},
+		},
+		{
+			name: "namespace exists, is not owned by project, but is labeled " +
+				"as a project; success updating namespace",
+			project: &kargoapi.Project{},
+			reconciler: &reconciler{
+				getNamespaceFn: func(
+					_ context.Context,
+					_ types.NamespacedName,
+					obj client.Object,
+					_ ...client.GetOption,
+				) error {
+					ns, ok := obj.(*corev1.Namespace)
+					require.True(t, ok)
+					ns.Labels = map[string]string{
+						kargoapi.ProjectLabelKey: kargoapi.LabelTrueValue,
+					}
+					return nil
+				},
+				updateNamespaceFn: func(
+					context.Context,
+					client.Object,
+					...client.UpdateOption,
+				) error {
+					return nil
+				},
+			},
+			assertions: func(initialStatus, newStatus kargoapi.ProjectStatus, err error) {
+				require.NoError(t, err)
+				require.Equal(t, newStatus.Phase, kargoapi.ProjectPhaseReady)
+				require.Empty(t, newStatus.Message)
+			},
+		},
+		{
+			name:    "namespace exists, is not owned by project, and is not labeled as a project",
+			project: &kargoapi.Project{},
+			reconciler: &reconciler{
+				getNamespaceFn: func(
+					context.Context,
+					types.NamespacedName,
+					client.Object,
+					...client.GetOption,
+				) error {
+					return nil
+				},
+			},
+			assertions: func(initialStatus, newStatus kargoapi.ProjectStatus, err error) {
+				require.Error(t, err)
+				// Status should reflect the unrecoverable failure
+				require.Equal(t, newStatus.Phase, kargoapi.ProjectPhaseInitializationFailed)
+				require.Contains(t, err.Error(), "failed to initialize Project")
+			},
+		},
+		{
+			name: "namespace exists and is owned by project",
+			project: &kargoapi.Project{
+				ObjectMeta: metav1.ObjectMeta{
+					UID: types.UID("fake-uid"),
+				},
+			},
+			reconciler: &reconciler{
+				getNamespaceFn: func(
+					_ context.Context,
+					_ types.NamespacedName,
+					obj client.Object,
+					_ ...client.GetOption,
+				) error {
+					ns := obj.(*corev1.Namespace) // nolint: forcetypeassert
+					ns.OwnerReferences = []metav1.OwnerReference{
+						{
+							UID: types.UID("fake-uid"),
+						},
+					}
+					return nil
+				},
+			},
+			assertions: func(_, newStatus kargoapi.ProjectStatus, err error) {
+				require.NoError(t, err)
+				// Status should reflect that the Project is in a ready state
+				require.Equal(t, newStatus.Phase, kargoapi.ProjectPhaseReady)
+			},
+		},
+		{
+			name: "namespace does not exist; error creating it",
+			project: &kargoapi.Project{
+				Status: kargoapi.ProjectStatus{
+					Phase: kargoapi.ProjectPhaseInitializing,
+				},
+			},
+			reconciler: &reconciler{
+				getNamespaceFn: func(
+					context.Context,
+					types.NamespacedName,
+					client.Object,
+					...client.GetOption,
+				) error {
+					return apierrors.NewNotFound(schema.GroupResource{}, "")
+				},
+				createNamespaceFn: func(
+					context.Context,
+					client.Object,
+					...client.CreateOption,
+				) error {
+					return errors.New("something went wrong")
+				},
+			},
+			assertions: func(initialStatus, newStatus kargoapi.ProjectStatus, err error) {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), "something went wrong")
+				require.Contains(t, err.Error(), "error creating namespace")
+				// Status is unchanged
+				require.Equal(t, initialStatus, newStatus)
+			},
+		},
+		{
+			name: "namespace does not exist; success creating it",
+			project: &kargoapi.Project{
+				Status: kargoapi.ProjectStatus{
+					Phase: kargoapi.ProjectPhaseInitializing,
+				},
+			},
+			reconciler: &reconciler{
+				getNamespaceFn: func(
+					context.Context,
+					types.NamespacedName,
+					client.Object,
+					...client.GetOption,
+				) error {
+					return apierrors.NewNotFound(schema.GroupResource{}, "")
+				},
+				createNamespaceFn: func(
+					context.Context,
+					client.Object,
+					...client.CreateOption,
+				) error {
+					return nil
+				},
+			},
 			assertions: func(_, newStatus kargoapi.ProjectStatus, err error) {
 				require.NoError(t, err)
 				// Status should reflect that the Project is in a ready state
