bc55b39b01434807d0b60b9a2ce0668b4d30ce40
Remington Breeze <remington@breeze.software>
2024-02-21 15:09:16 -0800

    fix(ui): long freight tags made promotion impossible  (#1510)
    Signed-off-by: Remington Breeze <remington@breeze.software>

--- ui/src/features/freightline/freight-contents.tsx
+++ ui/src/features/freightline/freight-contents.tsx
@@ -20,7 +20,7 @@ export const FreightContents = (props: {
     } & React.PropsWithChildren
   ) => (
     <Tooltip
-      className={`flex items-center my-1 flex-col bg-neutral-800 rounded p-1 w-full ${
+      className={`flex items-center my-1 flex-col bg-neutral-800 rounded p-1 w-20 max-w-20 overflow-x-hidden ${
         promoting && highlighted ? 'bg-transparent' : ''
       }`}
       overlay={props.overlay}
@@ -33,7 +33,7 @@ export const FreightContents = (props: {
 
   return (
     <div
-      className={`hover:text-white flex flex-col justify-start items-center font-mono text-xs flex-shrink min-w-min w-full overflow-y-auto max-h-full ${
+      className={`hover:text-white flex flex-col justify-start items-center font-mono text-xs flex-shrink min-w-min w-20 overflow-y-auto max-h-full ${
         highlighted ? 'text-white' : 'text-gray-500'
       }`}
     >
