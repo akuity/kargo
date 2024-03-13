9f09705ce3410bdad570c53eef00835534c8615c
Kent Rancourt <kent.rancourt@gmail.com>
2024-03-13 10:56:40 -0400

    feat(gc): garbage collect old freight (#1600)
    Signed-off-by: Kent Rancourt <kent.rancourt@gmail.com>

--- cmd/controlplane/garbage_collector.go
+++ cmd/controlplane/garbage_collector.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/metrics/server"
 
 	kargoapi "github.com/akuity/kargo/api/v1alpha1"
 	"github.com/akuity/kargo/internal/api/kubernetes"
 	"github.com/akuity/kargo/internal/garbage"
+	"github.com/akuity/kargo/internal/kubeclient"
 	"github.com/akuity/kargo/internal/os"
 	versionpkg "github.com/akuity/kargo/internal/version"
 )
@@ -33,7 +38,7 @@ func newGarbageCollectorCommand() *cobra.Command {
 
 			cfg := garbage.CollectorConfigFromEnv()
 
-			var kubeClient client.Client
+			var mgr manager.Manager
 			{
 				restCfg, err :=
 					kubernetes.GetRestConfig(ctx, os.GetEnv("KUBECONFIG", ""))
@@ -47,17 +52,41 @@ func newGarbageCollectorCommand() *cobra.Command {
 				if err = kargoapi.AddToScheme(scheme); err != nil {
 					return fmt.Errorf("error adding Kargo API to scheme: %w", err)
 				}
-				if kubeClient, err = client.New(
+				if mgr, err = ctrl.NewManager(
 					restCfg,
-					client.Options{
+					ctrl.Options{
 						Scheme: scheme,
+						Metrics: server.Options{
+							BindAddress: "0",
+						},
 					},
 				); err != nil {
-					return fmt.Errorf("err: %w", err)
+					return fmt.Errorf("error initializing controller manager: %w", err)
+				}
+				// Index Freight by Warehouse
+				if err = kubeclient.IndexFreightByWarehouse(ctx, mgr); err != nil {
+					return fmt.Errorf("error indexing Freight by Warehouse: %w", err)
+				}
+				// Index Stages by Freight
+				if err = kubeclient.IndexStagesByFreight(ctx, mgr); err != nil {
+					return fmt.Errorf("error indexing Stages by Freight: %w", err)
 				}
 			}
 
-			return garbage.NewCollector(kubeClient, cfg).Run(ctx)
+			ctx, cancel := context.WithCancel(ctx)
+			defer cancel()
+
+			go func() {
+				if err := mgr.Start(ctx); err != nil {
+					panic(fmt.Errorf("start manager: %w", err))
+				}
+			}()
+
+			if !mgr.GetCache().WaitForCacheSync(ctx) {
+				return errors.New("error waiting for cache sync")
+			}
+
+			return garbage.NewCollector(mgr.GetClient(), cfg).Run(ctx)
 		},
 	}
 }
