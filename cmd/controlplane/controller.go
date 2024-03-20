8ab332b52ec96f1330e51323d975d7c452d10660
Hidde Beydals <hidde@hhh.computer>
2024-03-20 22:58:59 +0100

    refactor(controller): untangle controller setup
    Signed-off-by: Hidde Beydals <hidde@hhh.computer>

--- cmd/controlplane/controller.go
+++ cmd/controlplane/controller.go
@@ -33,12 +33,19 @@ import (
 	_ "github.com/akuity/kargo/internal/gitprovider/github"
 )
 
-type controllerOpts struct {
+type controllerOptions struct {
+	ShardName  string
+	KubeConfig string
+
+	ArgoCDEnabled       bool
+	ArgoCDKubeConfig    string
+	ArgoCDNamespaceOnly bool
+
 	Logger *log.Logger
 }
 
 func newControllerCommand() *cobra.Command {
-	cmdOpts := &controllerOpts{
+	cmdOpts := &controllerOptions{
 		Logger: log.StandardLogger(),
 	}
 
@@ -48,6 +55,8 @@ func newControllerCommand() *cobra.Command {
 		SilenceErrors:     true,
 		SilenceUsage:      true,
 		RunE: func(cmd *cobra.Command, _ []string) error {
+			cmdOpts.complete()
+
 			return cmdOpts.run(cmd.Context())
 		},
 	}
@@ -55,162 +64,36 @@ func newControllerCommand() *cobra.Command {
 	return cmd
 }
 
-func (o *controllerOpts) run(ctx context.Context) error {
+func (o *controllerOptions) complete() {
+	o.ShardName = os.GetEnv("SHARD_NAME", "")
+	o.KubeConfig = os.GetEnv("KUBECONFIG", "")
+	o.ArgoCDEnabled = types.MustParseBool(os.GetEnv("ARGOCD_INTEGRATION_ENABLED", "true"))
+	o.ArgoCDKubeConfig = os.GetEnv("ARGOCD_KUBECONFIG", "")
+	o.ArgoCDNamespaceOnly = types.MustParseBool(os.GetEnv("ARGOCD_WATCH_ARGOCD_NAMESPACE_ONLY", "false"))
+}
+
+func (o *controllerOptions) run(ctx context.Context) error {
 	version := versionpkg.GetVersion()
-	shardName := os.GetEnv("SHARD_NAME", "")
 
 	startupLogEntry := o.Logger.WithFields(log.Fields{
 		"version": version.Version,
 		"commit":  version.GitCommit,
 	})
-	if shardName != "" {
-		startupLogEntry = startupLogEntry.WithField("shard", shardName)
+	if o.ShardName != "" {
+		startupLogEntry = startupLogEntry.WithField("shard", o.ShardName)
 	}
 	startupLogEntry.Info("Starting Kargo Controller")
 
 	stagesReconcilerCfg := stages.ReconcilerConfigFromEnv()
 
-	var kargoMgr manager.Manager
-	{
-		// If the env var is undefined, this will resolve to kubeconfig for the
-		// cluster the controller is running in.
-		//
-		// It is typically defined if this controller is running somewhere other
-		// than where the Kargo resources live. One example of this would be a
-		// sharded topology wherein Kargo controllers run on application
-		// clusters, with Kargo resources hosted in a centralized management
-		// cluster.
-		restCfg, err :=
-			kubernetes.GetRestConfig(ctx, os.GetEnv("KUBECONFIG", ""))
-		if err != nil {
-			return fmt.Errorf("error loading REST config for Kargo controller manager: %w", err)
-		}
-		restCfg.ContentType = runtime.ContentTypeJSON
-
-		scheme := runtime.NewScheme()
-		if err = corev1.AddToScheme(scheme); err != nil {
-			return fmt.Errorf(
-				"error adding Kubernetes core API to Kargo controller manager scheme: %w",
-				err,
-			)
-		}
-		if err = kargoapi.AddToScheme(scheme); err != nil {
-			return fmt.Errorf(
-				"error adding Kargo API to Kargo controller manager scheme: %w",
-				err,
-			)
-		}
-		if stagesReconcilerCfg.RolloutsIntegrationEnabled {
-			if argoRolloutsExists(ctx, restCfg) {
-				log.Info("Argo Rollouts integration is enabled")
-				if err = rollouts.AddToScheme(scheme); err != nil {
-					return fmt.Errorf(
-						"error adding Argo Rollouts API to Kargo controller manager scheme: %w",
-						err,
-					)
-				}
-			} else {
-				log.Warn(
-					"Argo Rollouts integration was enabled, but no Argo Rollouts " +
-						"CRDs were found. Proceeding without Argo Rollouts integration.",
-				)
-			}
-		}
-
-		secretReq, err := controller.GetCredentialsRequirement()
-		if err != nil {
-			return fmt.Errorf("error getting label requirement for credentials Secrets: %w", err)
-		}
-
-		cacheOpts := cache.Options{
-			ByObject: map[client.Object]cache.ByObject{
-				// Only watch Secrets matching the label requirements
-				// for credentials.
-				&corev1.Secret{}: {
-					Label: labels.NewSelector().Add(*secretReq),
-				},
-			},
-		}
-
-		if kargoMgr, err = ctrl.NewManager(
-			restCfg,
-			ctrl.Options{
-				Scheme: scheme,
-				Metrics: server.Options{
-					BindAddress: "0",
-				},
-				Cache: cacheOpts,
-			},
-		); err != nil {
-			return fmt.Errorf("error initializing Kargo controller manager: %w", err)
-		}
+	kargoMgr, err := o.setupKargoManager(ctx, stagesReconcilerCfg)
+	if err != nil {
+		return fmt.Errorf("error initializing Kargo controller manager: %w", err)
 	}
 
-	var argocdMgr manager.Manager
-	if types.MustParseBool(os.GetEnv("ARGOCD_INTEGRATION_ENABLED", "true")) {
-		// If the env var is undefined, this will resolve to kubeconfig for the
-		// cluster the controller is running in.
-		//
-		// It is typically defined if this controller is running somewhere other
-		// than where the Argo CD resources live. Two examples of this would
-		// involve topologies wherein Kargo controllers run EITHER sharded
-		// across application clusters OR in a centralized management cluster,
-		// but with Argo CD deployed to a different management cluster.
-		restCfg, err :=
-			kubernetes.GetRestConfig(ctx, os.GetEnv("ARGOCD_KUBECONFIG", ""))
-		if err != nil {
-			return fmt.Errorf("error loading REST config for Argo CD controller manager: %w", err)
-		}
-		restCfg.ContentType = runtime.ContentTypeJSON
-
-		argocdNamespace := libargocd.Namespace()
-
-		// There's a chance there is only permission to interact with Argo CD
-		// Application resources in a single namespace, so we will use that
-		// namespace when attempting to determine if Argo CD CRDs are installed.
-		if argoCDExists(ctx, restCfg, argocdNamespace) {
-			o.Logger.Info("Argo CD integration is enabled")
-			scheme := runtime.NewScheme()
-			if err = corev1.AddToScheme(scheme); err != nil {
-				return fmt.Errorf(
-					"error adding Kubernetes core API to Argo CD controller manager scheme: %w",
-					err,
-				)
-			}
-			if err = argocd.AddToScheme(scheme); err != nil {
-				return fmt.Errorf(
-					"error adding Argo CD API to Argo CD controller manager scheme: %w",
-					err,
-				)
-			}
-			cacheOpts := cache.Options{} // Watches all namespaces by default
-			if types.MustParseBool(
-				os.GetEnv("ARGOCD_WATCH_ARGOCD_NAMESPACE_ONLY", "false"),
-			) {
-				cacheOpts.DefaultNamespaces = map[string]cache.Config{
-					argocdNamespace: {},
-				}
-			}
-			if argocdMgr, err = ctrl.NewManager(
-				restCfg,
-				ctrl.Options{
-					Scheme: scheme,
-					Metrics: server.Options{
-						BindAddress: "0",
-					},
-					Cache: cacheOpts,
-				},
-			); err != nil {
-				return fmt.Errorf("error initializing Argo CD Application controller manager: %w", err)
-			}
-		} else {
-			o.Logger.Warn(
-				"Argo CD integration was enabled, but no Argo CD CRDs were " +
-					"found. Proceeding without Argo CD integration.",
-			)
-		}
-	} else {
-		o.Logger.Info("Argo CD integration is disabled")
+	argocdMgr, err := o.setupArgoCDManager(ctx)
+	if err != nil {
+		return fmt.Errorf("error initializing Argo CD Application controller manager: %w", err)
 	}
 
 	credentialsDB := credentials.NewKubernetesDatabase(
@@ -218,12 +101,167 @@ func (o *controllerOpts) run(ctx context.Context) error {
 		credentials.KubernetesDatabaseConfigFromEnv(),
 	)
 
+	if err := o.setupReconcilers(ctx, kargoMgr, argocdMgr, credentialsDB, stagesReconcilerCfg); err != nil {
+		return fmt.Errorf("error setting up reconcilers: %w", err)
+	}
+
+	return o.startManagers(ctx, kargoMgr, argocdMgr)
+}
+
+func (o *controllerOptions) setupKargoManager(
+	ctx context.Context,
+	stagesReconcilerCfg stages.ReconcilerConfig,
+) (manager.Manager, error) {
+	// If the env var is undefined, this will resolve to kubeconfig for the
+	// cluster the controller is running in.
+	//
+	// It is typically defined if this controller is running somewhere other
+	// than where the Kargo resources live. One example of this would be a
+	// sharded topology wherein Kargo controllers run on application
+	// clusters, with Kargo resources hosted in a centralized management
+	// cluster.
+	restCfg, err := kubernetes.GetRestConfig(ctx, o.KubeConfig)
+	if err != nil {
+		return nil, fmt.Errorf("error loading REST config for Kargo controller manager: %w", err)
+	}
+	restCfg.ContentType = runtime.ContentTypeJSON
+
+	scheme := runtime.NewScheme()
+	if err = corev1.AddToScheme(scheme); err != nil {
+		return nil, fmt.Errorf(
+			"error adding Kubernetes core API to Kargo controller manager scheme: %w",
+			err,
+		)
+	}
+	if err = kargoapi.AddToScheme(scheme); err != nil {
+		return nil, fmt.Errorf(
+			"error adding Kargo API to Kargo controller manager scheme: %w",
+			err,
+		)
+	}
+	if stagesReconcilerCfg.RolloutsIntegrationEnabled {
+		if argoRolloutsExists(ctx, restCfg) {
+			log.Info("Argo Rollouts integration is enabled")
+			if err = rollouts.AddToScheme(scheme); err != nil {
+				return nil, fmt.Errorf(
+					"error adding Argo Rollouts API to Kargo controller manager scheme: %w",
+					err,
+				)
+			}
+		} else {
+			log.Warn(
+				"Argo Rollouts integration was enabled, but no Argo Rollouts " +
+					"CRDs were found. Proceeding without Argo Rollouts integration.",
+			)
+		}
+	}
+
+	secretReq, err := controller.GetCredentialsRequirement()
+	if err != nil {
+		return nil, fmt.Errorf("error getting label requirement for credentials Secrets: %w", err)
+	}
+
+	cacheOpts := cache.Options{
+		ByObject: map[client.Object]cache.ByObject{
+			// Only watch Secrets matching the label requirements
+			// for credentials.
+			&corev1.Secret{}: {
+				Label: labels.NewSelector().Add(*secretReq),
+			},
+		},
+	}
+
+	return ctrl.NewManager(
+		restCfg,
+		ctrl.Options{
+			Scheme: scheme,
+			Metrics: server.Options{
+				BindAddress: "0",
+			},
+			Cache: cacheOpts,
+		},
+	)
+}
+
+func (o *controllerOptions) setupArgoCDManager(ctx context.Context) (manager.Manager, error) {
+	if !o.ArgoCDEnabled {
+		o.Logger.Info("Argo CD integration is disabled")
+		return nil, nil
+	}
+
+	// If the env var is undefined, this will resolve to kubeconfig for the
+	// cluster the controller is running in.
+	//
+	// It is typically defined if this controller is running somewhere other
+	// than where the Argo CD resources live. Two examples of this would
+	// involve topologies wherein Kargo controllers run EITHER sharded
+	// across application clusters OR in a centralized management cluster,
+	// but with Argo CD deployed to a different management cluster.
+	restCfg, err := kubernetes.GetRestConfig(ctx, o.ArgoCDKubeConfig)
+	if err != nil {
+		return nil, fmt.Errorf("error loading REST config for Argo CD controller manager: %w", err)
+	}
+	restCfg.ContentType = runtime.ContentTypeJSON
+
+	argocdNamespace := libargocd.Namespace()
+
+	// There's a chance there is only permission to interact with Argo CD
+	// Application resources in a single namespace, so we will use that
+	// namespace when attempting to determine if Argo CD CRDs are installed.
+	if !argoCDExists(ctx, restCfg, argocdNamespace) {
+		o.Logger.Warn(
+			"Argo CD integration was enabled, but no Argo CD CRDs were found. " +
+				"Proceeding without Argo CD integration.",
+		)
+		return nil, nil
+	}
+
+	o.Logger.Info("Argo CD integration is enabled")
+
+	scheme := runtime.NewScheme()
+	if err = corev1.AddToScheme(scheme); err != nil {
+		return nil, fmt.Errorf(
+			"error adding Kubernetes core API to Argo CD controller manager scheme: %w",
+			err,
+		)
+	}
+	if err = argocd.AddToScheme(scheme); err != nil {
+		return nil, fmt.Errorf(
+			"error adding Argo CD API to Argo CD controller manager scheme: %w",
+			err,
+		)
+	}
+	cacheOpts := cache.Options{} // Watches all namespaces by default
+	if o.ArgoCDNamespaceOnly {
+		cacheOpts.DefaultNamespaces = map[string]cache.Config{
+			argocdNamespace: {},
+		}
+	}
+
+	return ctrl.NewManager(
+		restCfg,
+		ctrl.Options{
+			Scheme: scheme,
+			Metrics: server.Options{
+				BindAddress: "0",
+			},
+			Cache: cacheOpts,
+		},
+	)
+}
+
+func (o *controllerOptions) setupReconcilers(
+	ctx context.Context,
+	kargoMgr, argocdMgr manager.Manager,
+	credentialsDB credentials.Database,
+	stagesReconcilerCfg stages.ReconcilerConfig,
+) error {
 	if err := promotions.SetupReconcilerWithManager(
 		ctx,
 		kargoMgr,
 		argocdMgr,
 		credentialsDB,
-		shardName,
+		o.ShardName,
 	); err != nil {
 		return fmt.Errorf("error setting up Promotions reconciler: %w", err)
 	}
@@ -240,14 +278,19 @@ func (o *controllerOpts) run(ctx context.Context) error {
 	if err := warehouses.SetupReconcilerWithManager(
 		kargoMgr,
 		credentialsDB,
-		shardName,
+		o.ShardName,
 	); err != nil {
 		return fmt.Errorf("error setting up Warehouses reconciler: %w", err)
 	}
 
-	var errChan = make(chan error)
+	return nil
+}
 
-	wg := sync.WaitGroup{}
+func (o *controllerOptions) startManagers(ctx context.Context, kargoMgr, argocdMgr manager.Manager) error {
+	var (
+		errChan = make(chan error)
+		wg      = sync.WaitGroup{}
+	)
 
 	if argocdMgr != nil {
 		wg.Add(1)
