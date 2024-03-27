4406ce7ecef311a58a920aad73aeffa0cce5499c
Hidde Beydals <hidde@hhh.computer>
2024-03-27 12:17:06 +0100

    feat(api): impl. `GetAnalysisTemplate` endpoint
    Signed-off-by: Hidde Beydals <hidde@hhh.computer>

---
+++ internal/api/get_analysistemplate_v1alpha1.go
@@ -0,0 +1,54 @@
+package api
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"connectrpc.com/connect"
+	"k8s.io/apimachinery/pkg/types"
+
+	svcv1alpha1 "github.com/akuity/kargo/pkg/api/service/v1alpha1"
+)
+
+func (s *server) GetAnalysisTemplate(
+	ctx context.Context,
+	req *connect.Request[svcv1alpha1.GetAnalysisTemplateRequest],
+) (*connect.Response[svcv1alpha1.GetAnalysisTemplateResponse], error) {
+	if !s.cfg.RolloutsIntegrationEnabled {
+		return nil, connect.NewError(
+			connect.CodeUnimplemented,
+			errors.New("Argo Rollouts integration is not enabled"),
+		)
+	}
+
+	project := req.Msg.GetProject()
+	if err := validateFieldNotEmpty("project", project); err != nil {
+		return nil, err
+	}
+
+	name := req.Msg.GetName()
+	if err := validateFieldNotEmpty("name", name); err != nil {
+		return nil, err
+	}
+
+	if err := s.validateProjectExists(ctx, project); err != nil {
+		return nil, err
+	}
+
+	at, err := s.getAnalysisTemplateFn(ctx, s.client, types.NamespacedName{
+		Namespace: project,
+		Name:      name,
+	})
+	if err != nil {
+		return nil, err
+	}
+	if at == nil {
+		err = fmt.Errorf("AnalysisTemplate %q not found in namespace %q", name, project)
+		return nil, connect.NewError(connect.CodeNotFound, err)
+	}
+
+	return connect.NewResponse(&svcv1alpha1.GetAnalysisTemplateResponse{
+		AnalysisTemplate: at,
+	}), nil
+}
