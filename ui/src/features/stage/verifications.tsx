a18fd72439e8f2442228ad5cd660a76d9db3a036
Rafal Pelczar <rafal@akuity.io>
2024-03-27 12:49:37 +0100

    feat(ui): Verifications visibility on stages
    Signed-off-by: Rafal Pelczar <rafal@akuity.io>

---
+++ ui/src/features/stage/verifications.tsx
@@ -0,0 +1,107 @@
+import {
+  faCircleCheck,
+  faCircleExclamation,
+  faCircleNotch,
+  faCircleQuestion,
+  faHourglassStart
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Popover, Table, Tooltip, theme } from 'antd';
+import React from 'react';
+
+import { Stage } from '@ui/gen/v1alpha1/generated_pb';
+
+type Props = {
+  stage: Stage;
+};
+
+export const Verifications = ({ stage }: Props) => {
+  const verifications = React.useMemo(
+    () => [
+      ...(stage.status?.currentFreight?.verificationHistory || []).map((i) => ({
+        freigth: stage.status?.currentFreight?.name,
+        ...i
+      })),
+
+      ...(stage.status?.history || []).flatMap((freigth) =>
+        freigth.verificationHistory.map((verification) => ({
+          freigth: freigth.name,
+          ...verification
+        }))
+      )
+    ],
+    [stage]
+  );
+
+  return (
+    <Table<(typeof verifications)[number]>
+      dataSource={verifications}
+      size='small'
+      pagination={{ hideOnSinglePage: true }}
+      rowKey={(p) => p.id || ''}
+    >
+      <Table.Column<(typeof verifications)[number]>
+        width={28}
+        render={(_, verification) => {
+          switch (verification.phase) {
+            case 'Successful':
+              return (
+                <Popover title={verification.phase} placement='right'>
+                  <FontAwesomeIcon
+                    color={theme.defaultSeed.colorSuccess}
+                    icon={faCircleCheck}
+                    size='lg'
+                  />
+                </Popover>
+              );
+            case 'Failed':
+            case 'Error':
+            case 'Aborted':
+              return (
+                <Popover
+                  content={verification.message}
+                  title={verification.phase}
+                  placement='right'
+                >
+                  <FontAwesomeIcon
+                    color={theme.defaultSeed.colorError}
+                    icon={faCircleExclamation}
+                    size='lg'
+                  />
+                </Popover>
+              );
+            case 'Running':
+              return (
+                <Tooltip title={verification.phase} placement='right'>
+                  <FontAwesomeIcon icon={faCircleNotch} spin size='lg' />
+                </Tooltip>
+              );
+            case 'Pending':
+              return (
+                <Tooltip title={verification.phase} placement='right'>
+                  <FontAwesomeIcon color='#aaa' icon={faHourglassStart} size='lg' />
+                </Tooltip>
+              );
+            default:
+              return (
+                <Popover
+                  title={verification.phase}
+                  content={verification.message}
+                  placement='right'
+                >
+                  <FontAwesomeIcon color='#aaa' icon={faCircleQuestion} size='lg' />
+                </Popover>
+              );
+          }
+        }}
+      />
+      <Table.Column title='ID' dataIndex='id' />
+      <Table.Column
+        title='Freigth'
+        dataIndex='freigth'
+        render={(val) => val?.substring(0, 7)}
+        width={120}
+      />
+    </Table>
+  );
+};
