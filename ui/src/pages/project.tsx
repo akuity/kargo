fbddc833afdfd1505da0cd8f522663f5168550b1
Rafal <rafal@akuity.io>
2024-04-02 21:49:42 +0200

    feat(ui): Move the Create Stage/Warehouse button (#1732)
    Signed-off-by: Rafal Pelczar <rafal@akuity.io>

--- ui/src/pages/project.tsx
+++ ui/src/pages/project.tsx
@@ -1,25 +1,12 @@
-import {
-  faChartBar,
-  faChevronDown,
-  faCircleNodes,
-  faIdBadge,
-  faMasksTheater,
-  faPalette,
-  faWandSparkles,
-  faWarehouse
-} from '@fortawesome/free-solid-svg-icons';
+import { faChartBar, faCircleNodes, faIdBadge } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Button, Dropdown, Space, Tabs, Tooltip } from 'antd';
+import { Tabs } from 'antd';
 import { generatePath, useNavigate, useParams } from 'react-router-dom';
 
 import { paths } from '@ui/config/paths';
-import { useModal } from '@ui/features/common/modal/use-modal';
 import { AnalysisTemplatesList } from '@ui/features/project/analysis-templates/analysis-templates-list';
 import { CredentialsList } from '@ui/features/project/credentials/credentials-list';
-import { CreateStageModal } from '@ui/features/project/project-details/create-stage-modal';
-import { CreateWarehouseModal } from '@ui/features/project/project-details/create-warehouse-modal';
 import { ProjectDetails } from '@ui/features/project/project-details/project-details';
-import { clearColors } from '@ui/features/stage/utils';
 
 const tabs = {
   details: {
@@ -45,14 +32,6 @@ export const Project = ({ tab = 'details' }: { tab?: ProjectTab }) => {
   const { name } = useParams();
   const navigate = useNavigate();
 
-  const { show: showCreateStage } = useModal(
-    name ? (p) => <CreateStageModal {...p} project={name} /> : undefined
-  );
-  const { show: showCreateWarehouse } = useModal(
-    name ? (p) => <CreateWarehouseModal {...p} project={name} /> : undefined
-  );
-
-  // const [activeTab, setActiveTab] = useState<ProjectTab>(tab || 'details');
   // we must render the tab contents outside of the Antd tabs component to prevent layout issues in the ProjectDetails component
   const renderTab = (key: ProjectTab) => {
     switch (key) {
@@ -75,51 +54,6 @@ export const Project = ({ tab = 'details' }: { tab?: ProjectTab }) => {
             <div className='font-semibold mb-1 text-xs text-gray-600'>PROJECT</div>
             <div className='text-2xl font-semibold'>{name}</div>
           </div>
-          <Tooltip title='Reassign Stage Colors'>
-            <Button
-              type='default'
-              className='mr-2'
-              onClick={() => {
-                clearColors(name || '');
-                window.location.reload();
-              }}
-            >
-              <FontAwesomeIcon icon={faPalette} />
-            </Button>
-          </Tooltip>{' '}
-          <Dropdown
-            menu={{
-              items: [
-                {
-                  key: '1',
-                  label: (
-                    <>
-                      <FontAwesomeIcon icon={faMasksTheater} size='xs' className='mr-2' /> Stage
-                    </>
-                  ),
-                  onClick: () => showCreateStage()
-                },
-                {
-                  key: '2',
-                  label: (
-                    <>
-                      <FontAwesomeIcon icon={faWarehouse} size='xs' className='mr-2' /> Warehouse
-                    </>
-                  ),
-                  onClick: () => showCreateWarehouse()
-                }
-              ]
-            }}
-            placement='bottomRight'
-            trigger={['click']}
-          >
-            <Button type='primary' icon={<FontAwesomeIcon icon={faWandSparkles} size='1x' />}>
-              <Space>
-                Create
-                <FontAwesomeIcon icon={faChevronDown} size='xs' />
-              </Space>
-            </Button>
-          </Dropdown>
         </div>
       </div>
       <Tabs
