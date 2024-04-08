2d58c161756bc3f9a79e7c1ce92dacb23eba4326
Sunghoon Kang <hoon@akuity.io>
2024-04-08 10:51:22 -0700

    fix(ui): provide initialization value to `metav1.Time` properties (#1794)
    Signed-off-by: Sunghoon Kang <hoon@akuity.io>

--- ui/tests/api/apimachinery.spec.ts
+++ ui/tests/api/apimachinery.spec.ts
@@ -1,25 +1,34 @@
-import { Timestamp } from '@bufbuild/protobuf';
 import { describe, expect, test } from 'vitest';
 
+import { Time } from '../../src/gen/k8s.io/apimachinery/pkg/apis/meta/v1/generated_pb';
 import { IntOrString } from '../../src/gen/k8s.io/apimachinery/pkg/util/intstr/generated_pb';
 
 describe('metav1.Time', () => {
+  test('fromBinary with empty value', () => {
+    const time = Time.fromBinary(new Uint8Array());
+    expect(time).toBeDefined();
+    expect(time.seconds).toBe(BigInt(0));
+    expect(time.nanos).toBe(0);
+  });
   test('fromJson', () => {
-    const time = Timestamp.fromJson('2023-05-30T00:00:00Z');
+    const time = Time.fromJson('2023-05-30T00:00:00Z');
     expect(time).toBeDefined();
     expect(time.seconds).toBe(BigInt(1685404800));
     expect(time.nanos).toBe(0);
   });
-
   test('toJson', () => {
-    const time = new Timestamp({seconds: BigInt(1685404800), nanos: 0});
+    const time = new Time({seconds: BigInt(1685404800), nanos: 0});
     expect(time.toJson()).toBe('2023-05-30T00:00:00Z');
-    expect(time.toJsonString()).toBe('"2023-05-30T00:00:00Z"');
   });
-
+  // Make sure that the generated code behaves identically to metav1.Time implementation in Go.
+  test('toJson with empty value', () => {
+    const time = new Time({});
+    expect(time.toDate()).toStrictEqual(new Date(0))
+    expect(time.toJson()).toBeNull();
+  });
   test('toDate', () => {
     const date = new Date('2023-05-30T00:00:00Z');
-    const time = new Timestamp({seconds: BigInt(1685404800), nanos: 0});
+    const time = new Time({seconds: BigInt(1685404800), nanos: 0});
     expect(time.toDate().getTime()).toBe(date.getTime());
   });
 });
