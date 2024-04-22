16ea95f84d0fd7680b84d2d42f397d688f63e0a2
Hidde Beydals <hidde@hhh.computer>
2024-04-22 14:06:49 +0200

    chore(api): move event annotations
    Signed-off-by: Hidde Beydals <hidde@hhh.computer>

--- api/v1alpha1/annotations.go
+++ api/v1alpha1/annotations.go
@@ -31,22 +31,6 @@ const (
 	AnnotationKeyOIDCEmails   = "rbac.kargo.akuity.io/email"
 	AnnotationKeyOIDCGroups   = "rbac.kargo.akuity.io/groups"
 	AnnotationKeyOIDCSubjects = "rbac.kargo.akuity.io/sub"
-
-	AnnotationKeyEventActor                  = "event.kargo.akuity.io/actor"
-	AnnotationKeyEventProject                = "event.kargo.akuity.io/project"
-	AnnotationKeyEventPromotionName          = "event.kargo.akuity.io/promotion-name"
-	AnnotationKeyEventPromotionCreateTime    = "event.kargo.akuity.io/promotion-create-time"
-	AnnotationKeyEventFreightAlias           = "event.kargo.akuity.io/freight-alias"
-	AnnotationKeyEventFreightName            = "event.kargo.akuity.io/freight-name"
-	AnnotationKeyEventFreightCreateTime      = "event.kargo.akuity.io/freight-create-time"
-	AnnotationKeyEventFreightCommits         = "event.kargo.akuity.io/freight-commits"
-	AnnotationKeyEventFreightImages          = "event.kargo.akuity.io/freight-images"
-	AnnotationKeyEventFreightCharts          = "event.kargo.akuity.io/freight-charts"
-	AnnotationKeyEventStageName              = "event.kargo.akuity.io/stage-name"
-	AnnotationKeyEventAnalysisRunName        = "event.kargo.akuity.io/analysis-run-name"
-	AnnotationKeyEventVerificationPending    = "event.kargo.akuity.io/verification-pending"
-	AnnotationKeyEventVerificationStartTime  = "event.kargo.akuity.io/verification-start-time"
-	AnnotationKeyEventVerificationFinishTime = "event.kargo.akuity.io/verification-finish-time"
 )
 
 // RefreshAnnotationValue returns the value of the AnnotationKeyRefresh
