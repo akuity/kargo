8d100fd89d3c09633b10ebf54f34c145bbaac336
Remington Breeze <remington@breeze.software>
2024-05-16 10:05:23 -0700

    chore(ui): vertically compress pipeline page layout (slightly) (#2023)
    Signed-off-by: Remington Breeze <remington@breeze.software>

--- ui/src/features/freightline/freightline-header.tsx
+++ ui/src/features/freightline/freightline-header.tsx
@@ -40,44 +40,46 @@ export const FreightlineHeader = ({
   };
 
   return (
-    <div className='w-full p-1 pl-6 h-12 mb-3 flex flex-col justify-end font-semibold text-sm'>
-      <div className='flex items-center'>
+    <div className='w-full pl-6 h-8 mb-3 flex flex-col justify-end font-semibold text-sm'>
+      <div className='flex items-end'>
         {action ? (
           <>
-            <FontAwesomeIcon icon={getIcon(action)} className='mr-2' />
-            {promotingStage && action != 'manualApproval' ? (
-              <>
-                PROMOTING{' '}
-                {action === 'promoteSubscribers'
-                  ? `TO ${(downstreamSubs || []).length} DOWNSTREAM SUBSCRIBERS (${downstreamSubs?.join(', ')}) OF`
-                  : ''}{' '}
-                STAGE :{' '}
-                <div
-                  className='px-2 py-1 rounded text-white ml-2 font-semibold'
-                  style={{
-                    backgroundColor: stageColorMap[promotingStage]
-                  }}
-                >
-                  {promotingStage.toUpperCase()}
-                </div>
-                <Tooltip
-                  title={
-                    <>
-                      Available freight are any which have been verified in{' '}
-                      {action === 'promote' && 'any immediately upstream stage OR approved for'}{' '}
-                      this stage.
-                    </>
-                  }
-                >
-                  <FontAwesomeIcon
-                    icon={faQuestionCircle}
-                    className='cursor-pointer text-zinc-500 ml-2'
-                  />
-                </Tooltip>
-              </>
-            ) : (
-              <>MANUALLY APPROVING FREIGHT</>
-            )}
+            <div className='flex items-center'>
+              <FontAwesomeIcon icon={getIcon(action)} className='mr-2' />
+              {promotingStage && action != 'manualApproval' ? (
+                <>
+                  PROMOTING{' '}
+                  {action === 'promoteSubscribers'
+                    ? `TO ${(downstreamSubs || []).length} DOWNSTREAM SUBSCRIBERS (${downstreamSubs?.join(', ')}) OF`
+                    : ''}{' '}
+                  STAGE :{' '}
+                  <div
+                    className='px-2 rounded text-white ml-2 font-semibold'
+                    style={{
+                      backgroundColor: stageColorMap[promotingStage]
+                    }}
+                  >
+                    {promotingStage.toUpperCase()}
+                  </div>
+                  <Tooltip
+                    title={
+                      <>
+                        Available freight are any which have been verified in{' '}
+                        {action === 'promote' && 'any immediately upstream stage OR approved for'}{' '}
+                        this stage.
+                      </>
+                    }
+                  >
+                    <FontAwesomeIcon
+                      icon={faQuestionCircle}
+                      className='cursor-pointer text-zinc-500 ml-2'
+                    />
+                  </Tooltip>
+                </>
+              ) : (
+                <>MANUALLY APPROVING FREIGHT</>
+              )}
+            </div>
 
             <div
               className='ml-auto mr-4 cursor-pointer px-2 py-1 text-white bg-zinc-700 rounded hover:bg-zinc-600 font-semibold text-sm'
@@ -87,10 +89,10 @@ export const FreightlineHeader = ({
             </div>
           </>
         ) : (
-          <>
+          <div className='flex items-center text-neutral-500 text-xs'>
             <FontAwesomeIcon icon={faTimeline} className='mr-2' />
             FREIGHTLINE
-          </>
+          </div>
         )}
       </div>
     </div>
