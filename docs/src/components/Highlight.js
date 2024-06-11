68047fcc51778dfc2c3f688690206210869db833
Kent Rancourt <kent.rancourt@gmail.com>
2024-06-11 10:24:46 -0400

    fix custom mdx highlight component
    Signed-off-by: Kent Rancourt <kent.rancourt@gmail.com>

--- docs/src/components/Highlight.js
+++ docs/src/components/Highlight.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export default function Highlight({children, color}) {
+export default function Highlight({children}) {
   return (
     <span
       style={{
-        backgroundColor: color,
-        borderRadius: '2px',
+        borderRadius: '4px',
+        backgroundColor: '#3F4C60',
         color: '#fff',
         padding: '0.2rem',
       }}>
