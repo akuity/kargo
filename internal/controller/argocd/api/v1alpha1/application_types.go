f6a32af541ebc3dc001b7d07ca501ca58dc94259
akuitybot <105087302+akuitybot@users.noreply.github.com>
2024-06-14 10:29:44 -0700

    chore(backport release-0.7): fix(promo): notice changes to Argo CD source updates (#2159)
    Co-authored-by: Hidde Beydals <hiddeco@users.noreply.github.com>

--- internal/controller/argocd/api/v1alpha1/application_types.go
+++ internal/controller/argocd/api/v1alpha1/application_types.go
@@ -1,6 +1,8 @@
 package v1alpha1
 
 import (
+	"reflect"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -29,7 +31,33 @@ type ApplicationSource struct {
 	Chart          string                      `json:"chart,omitempty"`
 }
 
+// Equals compares two instances of ApplicationSource and returns true if
+// they are equal.
+func (source *ApplicationSource) Equals(other *ApplicationSource) bool {
+	if source == nil && other == nil {
+		return true
+	}
+	if source == nil || other == nil {
+		return false
+	}
+	return reflect.DeepEqual(source.DeepCopy(), other.DeepCopy())
+}
+
 type ApplicationSources []ApplicationSource
+
+// Equals compares two instances of ApplicationSources and returns true if
+// they are equal.
+func (s ApplicationSources) Equals(other ApplicationSources) bool {
+	if len(s) != len(other) {
+		return false
+	}
+	for i := range s {
+		if !s[i].Equals(&other[i]) {
+			return false
+		}
+	}
+	return true
+}
 
 type RefreshType string
 
@@ -148,4 +176,6 @@ type OperationState struct {
 
 type SyncOperationResult struct {
 	Revision string `json:"revision,omitempty"`
+	Source ApplicationSource `json:"source,omitempty"`
+	Sources ApplicationSources `json:"sources,omitempty"`
 }
