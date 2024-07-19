5574afbb7ba2f504d75517b421850f3ed6833eba
Kent Rancourt <kent.rancourt@gmail.com>
2024-07-19 17:14:09 -0400

    restore main branch (edge) docs and scripts to always point at latest release
    Signed-off-by: Kent Rancourt <kent.rancourt@gmail.com>

--- hack/quickstart/install.sh
+++ hack/quickstart/install.sh
@@ -39,7 +39,6 @@ helm install argo-rollouts argo-rollouts \
 # Password is 'admin'
 helm install kargo \
   oci://ghcr.io/akuity/kargo-charts/kargo \
-  --version 0.8.0-rc.2 \
   --namespace kargo \
   --create-namespace \
   --set api.service.type=NodePort \
