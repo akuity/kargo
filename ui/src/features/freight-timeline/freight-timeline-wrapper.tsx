10e0b436304031404626b528d385754b8d444285
Remington Breeze <remington@breeze.software>
2024-08-28 13:02:37 -0700

    feat(ui): ability to scroll freight timeline with buttons
    Signed-off-by: Remington Breeze <remington@breeze.software>

--- ui/src/features/freight-timeline/freight-timeline-wrapper.tsx
+++ ui/src/features/freight-timeline/freight-timeline-wrapper.tsx
@@ -1,4 +1,30 @@
+import { faCaretLeft, faCaretRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Button, Flex } from 'antd';
+import { useLayoutEffect, useRef, useState } from 'react';
+
+import { headerButtonStyle } from './utils';
+
 export const FreightTimelineWrapper = ({ children }: { children: React.ReactNode }) => {
+  const timeline = useRef<HTMLDivElement>(null);
+  const [showScrollbars, setShowScrollbars] = useState(false);
+
+  useLayoutEffect(() => {
+    const handleResize = () => {
+      if (timeline.current) {
+        const { scrollWidth, clientWidth } = timeline.current;
+        setShowScrollbars(scrollWidth > clientWidth);
+      }
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [timeline]);
+
   return (
     <div className='w-full py-3 flex flex-col overflow-hidden'>
       <div className='flex h-48 w-full items-center px-1'>
@@ -8,9 +34,37 @@ export const FreightTimelineWrapper = ({ children }: { children: React.ReactNode
         >
           NEW
         </div>
-        <div className='flex items-center h-full overflow-x-auto w-full'>{children}</div>
+        <div className='flex items-center h-full overflow-x-auto w-full' ref={timeline}>
+          {children}
+        </div>
         <div className='rotate-90 text-gray-500 text-sm font-semibold ml-auto'>OLD</div>
       </div>
+      {showScrollbars && (
+        <Flex align='center' className='mt-2 pr-2' justify='end'>
+          <Button
+            onClick={() => {
+              timeline.current?.scrollTo({
+                left: timeline.current?.scrollLeft - 200,
+                behavior: 'smooth'
+              });
+            }}
+            size='small'
+            className={headerButtonStyle(false)}
+            icon={<FontAwesomeIcon icon={faCaretLeft} />}
+          />
+          <Button
+            onClick={() => {
+              timeline.current?.scrollTo({
+                left: timeline.current?.scrollLeft + 200,
+                behavior: 'smooth'
+              });
+            }}
+            size='small'
+            className={headerButtonStyle(false)}
+            icon={<FontAwesomeIcon icon={faCaretRight} />}
+          />
+        </Flex>
+      )}
     </div>
   );
 };
