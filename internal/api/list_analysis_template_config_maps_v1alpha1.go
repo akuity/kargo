3d705c1ee3498120992badf93786e5faad73dff8
Sunghoon Kang <hoon@akuity.io>
2024-09-11 12:22:24 +0900

    feat(api): add endpoints to get AnalysisTemplate config resources (#2480)
    Signed-off-by: Sunghoon Kang <hoon@akuity.io>

---
+++ internal/api/list_analysis_template_config_maps_v1alpha1.go
@@ -0,0 +1,61 @@
+package api
+
+import (
+	"context"
+	"fmt"
+	"slices"
+	"strings"
+
+	"connectrpc.com/connect"
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	kargoapi "github.com/akuity/kargo/api/v1alpha1"
+	svcv1alpha1 "github.com/akuity/kargo/pkg/api/service/v1alpha1"
+)
+
+func (s *server) ListAnalysisTemplateConfigMaps(
+	ctx context.Context,
+	req *connect.Request[svcv1alpha1.ListAnalysisTemplateConfigMapsRequest],
+) (*connect.Response[svcv1alpha1.ListAnalysisTemplateConfigMapsResponse], error) {
+	if !s.cfg.RolloutsIntegrationEnabled {
+		return nil, connect.NewError(
+			connect.CodeUnimplemented,
+			fmt.Errorf("Argo Rollouts integration is not enabled"),
+		)
+	}
+
+	project := req.Msg.GetProject()
+	if err := validateFieldNotEmpty("project", project); err != nil {
+		return nil, err
+	}
+
+	if err := s.validateProjectExists(ctx, project); err != nil {
+		return nil, err
+	}
+
+	var list corev1.ConfigMapList
+	opts := []client.ListOption{
+		client.InNamespace(project),
+		client.MatchingLabels{
+			kargoapi.AnalysisRunTemplateLabelKey: kargoapi.AnalysisRunTemplateLabelValueConfig,
+		},
+	}
+	if err := s.client.List(ctx, &list, opts...); err != nil {
+		return nil, fmt.Errorf("list ConfigMaps: %w", err)
+	}
+
+	// Sort ascending by name
+	slices.SortFunc(list.Items, func(lhs, rhs corev1.ConfigMap) int {
+		return strings.Compare(lhs.Name, rhs.Name)
+	})
+
+	cms := make([]*corev1.ConfigMap, len(list.Items))
+	for idx := range list.Items {
+		cms[idx] = &list.Items[idx]
+	}
+
+	return connect.NewResponse(&svcv1alpha1.ListAnalysisTemplateConfigMapsResponse{
+		ConfigMaps: cms,
+	}), nil
+}
