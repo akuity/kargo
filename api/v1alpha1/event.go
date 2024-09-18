f317bb76f19bf73bad62b13c2a242491cb6aa002
Hidde Beydals <hidde@hhh.computer>
2024-09-18 14:16:55 +0200

    fix(api): prevent panic in FormatEventUserActor
    Replace unsafe type assertions on `u.Claims` with safe pattern, to
    avoid a panic on missing (nil) or non-string claims.
    
    Signed-off-by: Hidde Beydals <hidde@hhh.computer>

--- api/v1alpha1/event.go
+++ api/v1alpha1/event.go
@@ -64,8 +64,18 @@ func FormatEventControllerActor(name string) string {
 // 3. If the subject is available, it returns subject in "subject:<subject>" format.
 // 4. Otherwise, it returns EventActorUnknown.
 func FormatEventUserActor(u user.Info) string {
-	email := u.Claims["email"].(string) // nolint: forcetypeassert
-	subject := u.Claims["sub"].(string) // nolint: forcetypeassert
+	var email, subject string
+	if emailClaim, ok := u.Claims["email"]; ok {
+		if emailStr, ok := emailClaim.(string); ok {
+			email = emailStr
+		}
+	}
+	if subClaim, ok := u.Claims["sub"]; ok {
+		if subStr, ok := subClaim.(string); ok {
+			subject = subStr
+		}
+	}
+
 	switch {
 	case u.IsAdmin:
 		return EventActorAdmin
