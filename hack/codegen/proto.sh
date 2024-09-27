513db60249097e35b738075bb1e15a6ef7dc29d8
Hidde Beydals <hidde@hhh.computer>
2024-09-27 19:29:05 +0200

    chore: deal with code-generator breaking changes
    Signed-off-by: Hidde Beydals <hidde@hhh.computer>

--- hack/codegen/proto.sh
+++ hack/codegen/proto.sh
@@ -9,20 +9,27 @@ readonly API_PKGS=(
 )
 
 readonly APIMACHINERY_PKGS=(
-  "k8s.io/apimachinery/pkg/util/intstr"
-  "+k8s.io/apimachinery/pkg/api/resource"
-  "+k8s.io/apimachinery/pkg/runtime/schema"
-  "+k8s.io/apimachinery/pkg/runtime"
-  "k8s.io/apimachinery/pkg/apis/meta/v1"
-  "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
-  "k8s.io/api/core/v1,k8s.io/api/batch/v1,k8s.io/api/rbac/v1"
+  "-k8s.io/api/core/v1"
+  "-k8s.io/api/batch/v1"
+  "-k8s.io/api/rbac/v1"
+  "-k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+  "-k8s.io/apimachinery/pkg/util/intstr"
+  "-k8s.io/apimachinery/pkg/api/resource"
+  "-k8s.io/apimachinery/pkg/runtime/schema"
+  "-k8s.io/apimachinery/pkg/runtime"
+  "-k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+# Change working directory to the root of the repository
 work_dir=$(dirname "${0}")
 work_dir=$(readlink -f "${work_dir}/../..")
 
+# Create a temporary build directory
+build_dir=$(mktemp -d)
+
 function clean() {
   echo "Clean up intermediate resources..."
+  rm -r "${build_dir}" || true
   rm -r "${work_dir}/pkg/api/v1alpha1" || true
   rm -r "${work_dir}/pkg/api/rbac" || true
   rm -r "${work_dir}/vendor" || true
@@ -30,21 +37,56 @@ function clean() {
 trap 'clean' EXIT
 
 function main() {
+  echo "Change working directory to temporary build directory..."
+  cd "${build_dir}"
+
+  echo "Prepare build directory..."
+  # Initialize dummy module inside build directory
+  go mod init github.com/akuity
+  go work init
+
+  # Copy source files to build directory
+  local build_src_dir
+  build_src_dir="${build_dir}/src/github.com/akuity/kargo"
+  mkdir -p "${build_src_dir}"
+
+  # Use find to locate and copy .go files, go.mod, and go.sum
+  # while preserving the directory structure
+  find "$work_dir" \( \
+    -name '*.go' -o \
+    -name 'go.mod' -o \
+    -name 'go.sum' \
+  \) -type f | while read -r file; do
+    rel_path="${file#$work_dir/}"
+    dest_file="$build_src_dir/$rel_path"
+    dest_dir=$(dirname "$dest_file")
+    mkdir -p "$dest_dir"
+    cp "$file" "$dest_file"
+  done
+  go work use ./src/github.com/akuity/kargo
+
   echo "Vendor dependencies for protobuf code generation..."
-  go mod tidy
-  go mod vendor
+  go work sync
+  go work vendor
 
   echo "Generate protobuf code from Kubebuilder structs..."
-    GOPATH=${GOPATH} go-to-protobuf \
-    --go-header-file=./hack/boilerplate.go.txt \
+  go-to-protobuf \
+    --go-header-file="${work_dir}/hack/boilerplate.go.txt" \
     --packages="$(IFS=, ; echo "${API_PKGS[*]}")" \
     --apimachinery-packages="$(IFS=, ; echo "${APIMACHINERY_PKGS[*]}")" \
-    --proto-import "${work_dir}/hack/include" \
-    --proto-import "${work_dir}/vendor"
+    --proto-import="${work_dir}/hack/include" \
+    --proto-import="${build_dir}/vendor" \
+    --output-dir="${build_dir}/src"
 
   echo "Copy generated code to the working directory..."
-  cp -R "${GOPATH}/src/github.com/akuity/kargo/api" "${work_dir}"
-  cp -R "${GOPATH}/src/github.com/akuity/kargo/internal" "${work_dir}"
+  cp -R "${build_src_dir}/api" "${work_dir}"
+  cp -R "${build_src_dir}/internal" "${work_dir}"
+
+  echo "Change working directory to repository directory..."
+  cd "${work_dir}"
+
+  echo "Vendor dependencies for protobuf code generation..."
+  go mod vendor
 
   echo "Generate protobuf code (Go)..."
   buf generate api
@@ -63,7 +105,7 @@ function main() {
 }
 
 (
+  # Include local binaries in the PATH
   export PATH="${work_dir}/hack/bin:${PATH}"
-  cd "${work_dir}"
   main
 )
