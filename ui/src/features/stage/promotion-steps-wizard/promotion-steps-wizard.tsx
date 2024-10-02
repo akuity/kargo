b9a334df5fbb325a04ad4500451031ca0593f1a8
Mayursinh Sarvaiya <marvinduff97@gmail.com>
2024-10-02 11:06:02 -0300

    chore(ui): imr
    Signed-off-by: Mayursinh Sarvaiya <marvinduff97@gmail.com>

--- ui/src/features/stage/promotion-steps-wizard/promotion-steps-wizard.tsx
+++ ui/src/features/stage/promotion-steps-wizard/promotion-steps-wizard.tsx
@@ -1,8 +1,9 @@
 import {
   faAdd,
+  faAngleRight,
   faCaretDown,
   faCaretUp,
-  faPencil,
+  faCog,
   faTrash
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -32,113 +33,120 @@ export const PromotionStepsWizard = (props: PromotionStepsWizardType) => {
   return (
     <div>
       {/* AVAILABLE RUNNERS - TODO(Marvin9) add search functionality */}
-      <div className='flex gap-2 flex-wrap'>
-        {registry.runners
-          .filter((runner) => !selectedRunners.find((r) => r.identifier === runner.identifier))
-          // .slice(0, 5)
-          .map((runner, idx) => (
-            <Tag
-              className='cursor-pointer'
-              color={'cyan'}
-              key={runner.identifier + idx}
-              onClick={() => {
-                setSelectedRunners([...selectedRunners, runner]);
-              }}
-              icon={<FontAwesomeIcon className='mr-2' icon={faAdd} />}
-            >
-              {runner.identifier}
+      <div className='flex gap-2 relative'>
+        <div className='flex overflow-hidden'>
+          {registry.runners
+            .filter((runner) => !selectedRunners.find((r) => r.identifier === runner.identifier))
+            // .slice(0, 5)
+            .map((runner, idx) => (
+              <Tag
+                className='cursor-pointer'
+                key={runner.identifier + idx}
+                onClick={() => {
+                  setSelectedRunners([...selectedRunners, runner]);
+                }}
+                icon={<FontAwesomeIcon className='mr-2' icon={faAdd} />}
+              >
+                {runner.identifier}
 
-              {runner.unstable_icons.length > 0 &&
-                runner.unstable_icons.map((icon) => (
-                  <FontAwesomeIcon className='ml-2' key={icon.iconName} icon={icon} />
-                ))}
-            </Tag>
-          ))}
+                {runner.unstable_icons.length > 0 &&
+                  runner.unstable_icons.map((icon) => (
+                    <FontAwesomeIcon className='ml-2' key={icon.iconName} icon={icon} />
+                  ))}
+              </Tag>
+            ))}
+        </div>
+        <FontAwesomeIcon
+          className='absolute text-lg right-0 bg-white h-full pl-5 cursor-pointer'
+          icon={faAngleRight}
+        />
       </div>
 
       {/* SELECTED RUNNERS */}
       {selectedRunners.length > 0 && (
-        <div className='mt-5 space-y-4'>
-          {selectedRunners.map((runner, order) => (
-            <Card key={runner.identifier}>
-              <div className='flex items-center gap-5 w-full'>
-                <div className='flex flex-col gap-5 text-2xl cursor-pointer'>
-                  {order > 0 && (
-                    <FontAwesomeIcon
-                      icon={faCaretUp}
-                      onClick={() => {
-                        const newOrder = [...selectedRunners];
-                        newOrder[order] = selectedRunners[order - 1];
-                        newOrder[order - 1] = runner;
-                        setSelectedRunners(newOrder);
-                      }}
-                    />
-                  )}
-                  {order < selectedRunners.length - 1 && (
-                    <FontAwesomeIcon
-                      icon={faCaretDown}
-                      onClick={() => {
-                        const newOrder = [...selectedRunners];
-                        newOrder[order] = selectedRunners[order + 1];
-                        newOrder[order + 1] = runner;
-                        setSelectedRunners(newOrder);
-                      }}
-                    />
-                  )}
-                </div>
+        <div className='mt-5 space-y-4 w-6/12'>
+          {selectedRunners.map((runner, order) => {
+            const patchRunner = (nextRunner: RunnerWithConfiguration) => {
+              setSelectedRunners(
+                selectedRunners.map((localRunner, localOrder) => {
+                  if (order === localOrder) {
+                    return {
+                      ...localRunner,
+                      ...nextRunner
+                    };
+                  }
 
-                <span className='font-semibold'>
-                  {order + 1} - {runner.identifier}
-                </span>
+                  return localRunner;
+                })
+              );
+            };
 
-                {runner.unstable_icons.length > 0 && (
-                  <div className='flex ml-auto gap-2'>
-                    {runner.unstable_icons.map((icon) => (
-                      <FontAwesomeIcon key={icon.iconName} icon={icon} />
-                    ))}
+            const onSettingOpen = () => {
+              runnerEdit.show((props) => (
+                <PromotionStepDrawer
+                  {...props}
+                  selectedRunner={runner}
+                  patchSelectedRunner={patchRunner}
+                />
+              ));
+            };
+
+            return (
+              <Card key={runner.identifier} className='cursor-pointer' onClick={onSettingOpen}>
+                <div className='flex items-center gap-5'>
+                  <div className='flex flex-col gap-5 text-2xl cursor-pointer'>
+                    {order > 0 && (
+                      <FontAwesomeIcon
+                        icon={faCaretUp}
+                        onClick={() => {
+                          const newOrder = [...selectedRunners];
+                          newOrder[order] = selectedRunners[order - 1];
+                          newOrder[order - 1] = runner;
+                          setSelectedRunners(newOrder);
+                        }}
+                      />
+                    )}
+                    {order < selectedRunners.length - 1 && (
+                      <FontAwesomeIcon
+                        icon={faCaretDown}
+                        onClick={() => {
+                          const newOrder = [...selectedRunners];
+                          newOrder[order] = selectedRunners[order + 1];
+                          newOrder[order + 1] = runner;
+                          setSelectedRunners(newOrder);
+                        }}
+                      />
+                    )}
                   </div>
-                )}
 
-                <div className='space-x-4'>
-                  <Button
-                    icon={<FontAwesomeIcon icon={faPencil} />}
-                    onClick={() => {
-                      const patchRunner = (nextRunner: RunnerWithConfiguration) => {
+                  <span className='font-semibold'>
+                    {order + 1} - {runner.identifier}
+                  </span>
+
+                  {runner.unstable_icons.length > 0 && (
+                    <div className='flex ml-auto gap-2'>
+                      {runner.unstable_icons.map((icon) => (
+                        <FontAwesomeIcon key={icon.iconName} icon={icon} />
+                      ))}
+                    </div>
+                  )}
+
+                  <div className='space-x-4'>
+                    <Button icon={<FontAwesomeIcon icon={faCog} />} onClick={onSettingOpen} />
+                    <Button
+                      icon={<FontAwesomeIcon icon={faTrash} className='text-red-500' />}
+                      onClick={(e) => {
+                        e.stopPropagation();
                         setSelectedRunners(
-                          selectedRunners.map((localRunner, localOrder) => {
-                            if (order === localOrder) {
-                              return {
-                                ...localRunner,
-                                ...nextRunner
-                              };
-                            }
-
-                            return localRunner;
-                          })
+                          selectedRunners.filter((r) => r.identifier !== runner.identifier)
                         );
-                      };
-
-                      runnerEdit.show((props) => (
-                        <PromotionStepDrawer
-                          {...props}
-                          selectedRunner={runner}
-                          patchSelectedRunner={patchRunner}
-                        />
-                      ));
-                    }}
-                  />
-                  <Button
-                    icon={<FontAwesomeIcon icon={faTrash} className='text-red-500' />}
-                    onClick={() =>
-                      setSelectedRunners(
-                        selectedRunners.filter((r) => r.identifier !== runner.identifier)
-                      )
-                    }
-                  />
+                      }}
+                    />
+                  </div>
                 </div>
-              </div>
-            </Card>
-          ))}
+              </Card>
+            );
+          })}
         </div>
       )}
     </div>
