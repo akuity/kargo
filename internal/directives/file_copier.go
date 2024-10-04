43de1e8396526aa6c25d0d3ea71530c8df9f71a6
akuitybot <105087302+akuitybot@users.noreply.github.com>
2024-10-04 07:33:43 -0700

    chore(backport release-0.9): feat(directives): allow ignore rules for `copy` (#2655)
    Co-authored-by: Hidde Beydals <hiddeco@users.noreply.github.com>

--- internal/directives/file_copier.go
+++ internal/directives/file_copier.go
@@ -1,14 +1,17 @@
 package directives
 
 import (
+	"bufio"
 	"context"
 	"errors"
 	"fmt"
 	"io/fs"
+	"os"
 	"path/filepath"
 	"strings"
 
 	securejoin "github.com/cyphar/filepath-securejoin"
+	"github.com/go-git/go-git/v5/plumbing/format/gitignore"
 	"github.com/otiai10/copy"
 	"github.com/xeipuuv/gojsonschema"
 
@@ -80,11 +83,21 @@ func (f *fileCopier) runPromotionStep(
 			fmt.Errorf("could not secure join outPath %q: %w", cfg.OutPath, err)
 	}
 
+	// Load the ignore rules.
+	matcher, err := f.loadIgnoreRules(inPath, cfg.Ignore)
+	if err != nil {
+		return PromotionStepResult{Status: kargoapi.PromotionPhaseErrored},
+			fmt.Errorf("failed to load ignore rules: %w", err)
+	}
+
 	// Perform the copy operation.
 	opts := copy.Options{
 		OnSymlink: func(src string) copy.SymlinkAction {
 			logging.LoggerFromContext(ctx).Trace("ignoring symlink", "src", src)
 			return copy.Skip
+		},
+		Skip: func(f os.FileInfo, src, _ string) (bool, error) {
+			return matcher.Match(strings.Split(src, string(filepath.Separator)), f.IsDir()), nil
 		},
 		OnError: func(_, _ string, err error) error {
 			return sanitizePathError(err, stepCtx.WorkDir)
@@ -95,6 +108,48 @@ func (f *fileCopier) runPromotionStep(
 			fmt.Errorf("failed to copy %q to %q: %w", cfg.InPath, cfg.OutPath, err)
 	}
 	return PromotionStepResult{Status: kargoapi.PromotionPhaseSucceeded}, nil
+}
+
+// loadIgnoreRules loads the ignore rules from the given string. The rules are
+// separated by newlines, and comments are allowed with the '#' character.
+// It returns a gitignore.Matcher that can be used to match paths against the
+// rules.
+func (f *fileCopier) loadIgnoreRules(inPath, rules string) (gitignore.Matcher, error) {
+	// Determine the domain for the ignore rules. For directories, the domain is
+	// the directory itself. For files, the domain is the parent directory.
+	fi, err := os.Lstat(inPath)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			// Let the error slide if the path does not exist, to allow
+			// the copy operation to fail later. This provides a more
+			// predictable user experience.
+			return gitignore.NewMatcher(nil), nil
+		}
+		return nil, fmt.Errorf("failed to determine domain: %w", err)
+	}
+	var domain []string
+	switch {
+	case fi.IsDir():
+		domain = strings.Split(inPath, string(filepath.Separator))
+	default:
+		domain = strings.Split(filepath.Dir(inPath), string(filepath.Separator))
+	}
+
+	// Default patterns to ignore the .git directory.
+	ps := []gitignore.Pattern{
+		gitignore.ParsePattern(".git", domain),
+	}
+
+	// Parse additional user-provided rules.
+	scanner := bufio.NewScanner(strings.NewReader(rules))
+	for scanner.Scan() {
+		s := scanner.Text()
+		if !strings.HasPrefix(s, "#") && len(strings.TrimSpace(s)) > 0 {
+			ps = append(ps, gitignore.ParsePattern(s, domain))
+		}
+	}
+
+	return gitignore.NewMatcher(ps), nil
 }
 
 // sanitizePathError sanitizes the path in a path error to be relative to the
