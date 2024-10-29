fedf4aa7ca6f612f2e19358c017edaf6dbc8207d
Sergio Teixeira <sergio@triggerise.org>
2024-10-29 13:54:40 +0000

    feat(ui): remember pagination option, project search selection jumps into project (#2821)
    Signed-off-by: Sergio Teixeira <sergio@triggerise.org>

--- ui/src/features/project/list/project-list-filter.tsx
+++ ui/src/features/project/list/project-list-filter.tsx
@@ -3,7 +3,9 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { AutoComplete, Button } from 'antd';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
+import { paths } from '@ui/config/paths';
 import { listProjects } from '@ui/gen/service/v1alpha1/service-KargoService_connectquery';
 
 export const ProjectListFilter = ({
@@ -15,20 +17,33 @@ export const ProjectListFilter = ({
 }) => {
   const { data } = useQuery(listProjects);
   const [filter, setFilter] = useState(init || '');
+  const navigate = useNavigate();
+
+  const filteredProjects = data?.projects.filter((p) =>
+    p.metadata?.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter' && filteredProjects?.length === 1) {
+      const selectedProject = filteredProjects[0].metadata?.name;
+      navigate(paths.project.replace(':name', selectedProject));
+    }
+  };
+
+  const handleSelect = (value: string) => {
+    navigate(paths.project.replace(':name', value));
+  };
 
   return (
     <div className='flex items-center w-2/3'>
       <AutoComplete
         placeholder='Filter...'
-        options={data?.projects.map((p) => ({ value: p.metadata?.name }))}
+        options={filteredProjects?.map((p) => ({ value: p.metadata?.name }))}
         onChange={setFilter}
         className='w-full mr-2'
         value={filter}
-        onKeyDown={(e) => {
-          if (e.key === 'Enter') {
-            onChange(filter);
-          }
-        }}
+        onKeyDown={handleKeyDown}
+        onSelect={handleSelect}
       />
       <Button type='primary' onClick={() => onChange(filter)}>
         <FontAwesomeIcon icon={faMagnifyingGlass} />
