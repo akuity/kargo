e47bd261deab05f11bc0d35c4af06b7a4736ce0d
Kent Rancourt <kent.rancourt@gmail.com>
2024-11-19 16:46:16 -0500

    wip
    Signed-off-by: Kent Rancourt <kent.rancourt@gmail.com>

--- docs/docs/35-references/10-promotion-steps.md
+++ docs/docs/35-references/10-promotion-steps.md
@@ -11,10 +11,10 @@ in a typical promotion process. Similarly, configuration options for each step
 are laid out in order of their applicability to typical use cases.
 
 :::info
-Promotion steps support the use of [expr-lang](https://expr-lang.org/)
-expressions in their configuration. Many examples in this reference document
-will include expressions to demonstrate their use. For more information on
-expressions, refer to our [Expression Language Reference](./20-expression-language.md).
+Promotion steps support the use of [expr-lang] expressions in their
+configuration. Many examples in this reference document will include expressions
+to demonstrate their use. For more information on expressions, refer to our
+[Expression Language Reference](./20-expression-language.md).
 :::
 
 ## `git-clone`
@@ -1156,3 +1156,192 @@ Although the `argocd-update` step is the only promotion step to currently
 utilize this health check framework, we anticipate that future built-in and
 third-party promotion steps will take advantage of it as well.
 :::
+
+## `http`
+
+`http` is a generic step that makes an HTTP/S request to enable basic integration
+with a wide variety of external services.
+
+### `http` Configuration
+
+| Name | Type | Required | Description |
+|------|------|----------|-------------|
+| `method` | `string` | Y | The HTTP method to use. |
+| `url` | `string` | Y | The URL to which the request should be made. |
+| `headers` | `[]object` | N | A list of headers to include in the request. |
+| `headers[].name` | `string` | Y | The name of the header. |
+| `headers[].value` | `string` | Y | The value of the header. |
+| `queryParams` | `[]object` | N | A list of query parameters to include in the request. |
+| `queryParams[].name` | `string` | Y | The name of the query parameter. |
+| `queryParams[].value` | `string` | Y | The value of the query parameter. The provided value will automatically be URL-encoded if necessary. |
+| `body` | `string` | N | The body of the request. |
+| `insecureSkipTLSVerify` | `boolean` | N | Indicates whether to bypass TLS certificate verification when making the request. Setting this to `true` is highly discouraged. |
+| `successExpression` | `string` | N | An [expr-lang] expression that can evaluate the response to determine success. If this is left undefined and `failureExpression` _is_ defined, the default success criteria will be the inverse of the specified failure criteria. If both are left undefined, success is `true` when the HTTP status code is `2xx`. If `successExpression` and `failureExpression` are both defined and both evaluate to `true`, the failure takes precedence. Note that this expression should _not_ be offset by `${{` and `}}`. See examples for more details. |
+| `failureExpression` | `string` | N | An [expr-lang] expression that can evaluate the response to determine failure. If this is left undefined and `successExpression` _is_ defined, the default failure criteria will be the inverse of the specified success criteria. If both are left undefined, failure is `true` when the HTTP status code is _not_ `2xx`. If `successExpression` and `failureExpression` are both defined and both evaluate to `true`, the failure takes precedence. Note that this expression should _not_ be offset by `${{` and `}}`. See examples for more details. |
+| `outputs` | `[]object` | N | A list of rules for extracting outputs from the HTTP response. These are only applied to responses deemed successful. |
+| `outputs[].name` | `string` | Y | The name of the output. |
+| `outputs[].fromExpression` | `string` | Y | An [expr-lang] expression that can extract a value from the HTTP response. Note that this expression should _not_ be offset by `${{` and `}}`. See examples for more details. |
+
+:::note
+An HTTP response that is not conclusively determined to have succeeded or failed
+will result in the step reporting a result of `Running`. Kargo will retry such
+a step on its next attempt at reconciling the `Promotion` resource. This will
+continue until the step succeeds, fails, exhausts the configured maximum number
+of retries, or a configured timeout has elapsed.
+:::
+
+### `http` Expressions
+
+The `successExpression`, `failureExpression`, and `outputs[].fromExpression`
+fields all support [expr-lang] expressions.
+
+:::note
+The expressions included in the `successExpression`, `failureExpression`, and
+`outputs[].fromExpression` fields should _not_ be offset by `${{` and `}}`. This
+is to prevent the expressions from being evaluated by Kargo during
+pre-processing of step configurations. The `http` step itself will evaluate
+these expressions.
+:::
+
+A `response` object (a `map[string]any`) is available to these expressions. It
+is structured as follows:
+
+| Field | Type | Description |
+|-------|------|-------------|
+| `status` | `int` | The HTTP status code of the response. |
+| `headers` | `http.Header` | The headers of the response. See applicable [Go documentation](https://pkg.go.dev/net/http#Header). |
+| `header` | `func(string) string` | `headers` can be inconvenient to work with directly. This function allows you to access a header by name. |
+| `body` | `map[string]any` | The body of the response, if any, unmarshaled into a map. If the response body is empty, this map will also be empty. |
+
+### `http` Examples
+
+<Tabs groupId="http">
+
+<TabItem value="basic" label="Basic Usage" default>
+
+This examples configuration makes a `GET` request to the
+[Cat Facts API.](https://www.catfacts.net/api/) and uses the default
+success/failure criteria.
+
+```yaml
+steps:
+# ...
+- uses: http
+  as: cat-facts
+  config:
+    method: GET
+    url: https://www.catfacts.net/api/
+    outputs:
+    - name: status
+      fromExpression: response.status
+    - name: fact1
+      fromExpression: response.body.facts[0]
+    - name: fact2
+      fromExpression: response.body.facts[1]
+```
+
+Assuming a `200` response with the following JSON body:
+
+```json
+{
+    "facts": [
+        {
+            "fact_number": 1,
+            "fact": "Kittens have baby teeth, which are replaced by permanent teeth around the age of 7 months."
+        },
+        {
+            "fact_number": 2,
+            "fact": "Each day in the US, animal shelters are forced to destroy 30,000 dogs and cats."
+        }
+    ]
+}
+```
+
+The step would succeed and produce the following outputs:
+
+```yaml
+| Name     | Type | Value |
+|----------|------|-------|
+| `status` | `int` | `200` |
+| `fact1` | `string` | `Kittens have baby teeth, which are replaced by permanent teeth around the age of 7 months.` |
+| `fact2` | `string` | `Each day in the US, animal shelters are forced to destroy 30,000 dogs and cats.` |
+```
+
+</TabItem>
+
+<TabItem value="polling" label="Polling">
+
+Building on the basic example, this configuration defines explicit success and
+failure criteria. Any response meeting neither of these criteria will result in
+the step reporting a result of `Running` and being retried.
+
+```yaml
+steps:
+# ...
+- uses: http
+  as: cat-facts
+  config:
+    method: GET
+    url: https://www.catfacts.net/api/
+    successExpression: response.status == 200
+    failureExpression: response.status == 404
+    outputs:
+    - name: status
+      fromExpression: response.status
+    - name: fact1
+      fromExpression: response.body.facts[0]
+    - name: fact2
+      fromExpression: response.body.facts[1]
+```
+
+Our request is considered:
+
+- Successful if the response status is `200`.
+- A failure if the response status is `404`.
+- Running if the response status is anything else. i.e. Any other status code
+  will result in a retry.
+
+</TabItem>
+
+<TabItem value="slack" label="Posting to Slack">
+
+This examples is adapted from
+[Slack's own documentation](https://api.slack.com/tutorials/tracks/posting-messages-with-curl):
+
+```yaml
+steps:
+# ...
+- uses: http
+  config:
+    method: POST
+    url: https://slack.com/api/chat.postMessage
+    headers:
+    - name: Authorization
+      value: Bearer ${{ secrets.slack.token }}
+    - name: Content-Type
+      value: application/json
+    body: |
+      {
+        "channel": ${{ vars.slackChannel }},
+        "blocks": [
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "Hi I am a bot that can post *_fancy_* messages to any public channel."
+            }
+          }
+        ]
+      }
+```
+
+</TabItem>
+
+</Tabs>
+
+### `http` Outputs
+
+The `http` step only produces the outputs described by the `outputs` field of
+its configuration.
+
+[expr-lang]: https://expr-lang.org/
---
+++ internal/directives/http_requester_test.go
@@ -0,0 +1,602 @@
+package directives
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	kargoapi "github.com/akuity/kargo/api/v1alpha1"
+)
+
+func Test_httpRequester_validate(t *testing.T) {
+	testCases := []struct {
+		name             string
+		config           Config
+		expectedProblems []string
+	}{
+		{
+			name:   "url not specified",
+			config: Config{},
+			expectedProblems: []string{
+				"(root): url is required",
+			},
+		},
+		{
+			name: "url is empty string",
+			config: Config{
+				"url": "",
+			},
+			expectedProblems: []string{
+				"url: String length must be greater than or equal to 1",
+			},
+		},
+		{
+			name: "invalid method",
+			config: Config{
+				"method": "invalid",
+			},
+			expectedProblems: []string{
+				"method: Does not match pattern",
+			},
+		},
+		{
+			name: "header name not specified",
+			config: Config{
+				"headers": []Config{{}},
+			},
+			expectedProblems: []string{
+				"headers.0: name is required",
+			},
+		},
+		{
+			name: "header name is empty string",
+			config: Config{
+				"headers": []Config{{
+					"name": "",
+				}},
+			},
+			expectedProblems: []string{
+				"headers.0.name: String length must be greater than or equal to 1",
+			},
+		},
+		{
+			name: "header value not specified",
+			config: Config{
+				"headers": []Config{{}},
+			},
+			expectedProblems: []string{
+				"headers.0: value is required",
+			},
+		},
+		{
+			name: "header value is empty string",
+			config: Config{
+				"headers": []Config{{
+					"value": "",
+				}},
+			},
+			expectedProblems: []string{
+				"headers.0.value: String length must be greater than or equal to 1",
+			},
+		},
+		{
+			name: "query param name not specified",
+			config: Config{
+				"queryParams": []Config{{}},
+			},
+			expectedProblems: []string{
+				"queryParams.0: name is required",
+			},
+		},
+		{
+			name: "query param name is empty string",
+			config: Config{
+				"queryParams": []Config{{
+					"name": "",
+				}},
+			},
+			expectedProblems: []string{
+				"queryParams.0.name: String length must be greater than or equal to 1",
+			},
+		},
+		{
+			name: "query param value not specified",
+			config: Config{
+				"queryParams": []Config{{}},
+			},
+			expectedProblems: []string{
+				"queryParams.0: value is required",
+			},
+		},
+		{
+			name: "query param value is empty string",
+			config: Config{
+				"queryParams": []Config{{
+					"value": "",
+				}},
+			},
+			expectedProblems: []string{
+				"queryParams.0.value: String length must be greater than or equal to 1",
+			},
+		},
+		{
+			name: "output name not specified",
+			config: Config{
+				"outputs": []Config{{}},
+			},
+			expectedProblems: []string{
+				"outputs.0: name is required",
+			},
+		},
+		{
+			name: "output name is empty string",
+			config: Config{
+				"outputs": []Config{{
+					"name": "",
+				}},
+			},
+			expectedProblems: []string{
+				"outputs.0.name: String length must be greater than or equal to 1",
+			},
+		},
+		{
+			name: "output fromExpression not specified",
+			config: Config{
+				"outputs": []Config{{}},
+			},
+			expectedProblems: []string{
+				"outputs.0: fromExpression is required",
+			},
+		},
+		{
+			name: "output fromExpression is empty string",
+			config: Config{
+				"outputs": []Config{{
+					"fromExpression": "",
+				}},
+			},
+			expectedProblems: []string{
+				"outputs.0.fromExpression: String length must be greater than or equal to 1",
+			},
+		},
+	}
+
+	r := newHTTPRequester()
+	runner, ok := r.(*httpRequester)
+	require.True(t, ok)
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			err := runner.validate(testCase.config)
+			if len(testCase.expectedProblems) == 0 {
+				require.NoError(t, err)
+			} else {
+				for _, problem := range testCase.expectedProblems {
+					require.ErrorContains(t, err, problem)
+				}
+			}
+		})
+	}
+}
+
+func Test_httpRequester_runPromotionStep(t *testing.T) {
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		// We'll just respond with a 200 OK for all cases and use different
+		// configurations to test the different code paths.
+		w.WriteHeader(http.StatusOK)
+	}))
+	t.Cleanup(srv.Close)
+
+	testCases := []struct {
+		name       string
+		cfg        HTTPConfig
+		handler    http.HandlerFunc
+		assertions func(*testing.T, PromotionStepResult, error)
+	}{
+		{
+			name: "success and not failed",
+			cfg: HTTPConfig{
+				SuccessExpression: "true",
+				Outputs: []HTTPOutput{{
+					Name:           "theMeaningOfLife",
+					FromExpression: "42",
+				}},
+			},
+			assertions: func(t *testing.T, res PromotionStepResult, err error) {
+				require.NoError(t, err)
+				require.Equal(t, kargoapi.PromotionPhaseSucceeded, res.Status)
+				require.Equal(t, map[string]any{"theMeaningOfLife": 42}, res.Output)
+			},
+		},
+		{
+			name: "failed and not success",
+			cfg: HTTPConfig{
+				FailureExpression: "true",
+			},
+			assertions: func(t *testing.T, res PromotionStepResult, err error) {
+				require.ErrorContains(t, err, "HTTP response met failure criteria")
+				require.Equal(t, kargoapi.PromotionPhaseFailed, res.Status)
+			},
+		},
+		{
+			name: "success AND failed", // Treated like a failure
+			cfg: HTTPConfig{
+				SuccessExpression: "true",
+				FailureExpression: "true",
+			},
+			assertions: func(t *testing.T, res PromotionStepResult, err error) {
+				require.ErrorContains(t, err, "HTTP response met failure criteria")
+				require.Equal(t, kargoapi.PromotionPhaseFailed, res.Status)
+			},
+		},
+		{
+			name: "neither success nor failed",
+			cfg: HTTPConfig{
+				SuccessExpression: "false",
+				FailureExpression: "false",
+			},
+			assertions: func(t *testing.T, res PromotionStepResult, err error) {
+				require.NoError(t, err)
+				require.Equal(t, kargoapi.PromotionPhaseRunning, res.Status)
+			},
+		},
+	}
+
+	h := &httpRequester{}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			testCase.cfg.URL = srv.URL
+			res, err := h.runPromotionStep(context.Background(), nil, testCase.cfg)
+			testCase.assertions(t, res, err)
+		})
+	}
+}
+
+func Test_httpRequester_buildRequest(t *testing.T) {
+	req, err := (&httpRequester{}).buildRequest(HTTPConfig{
+		Method: "GET",
+		URL:    "http://example.com",
+		Headers: []HTTPHeader{{
+			Name:  "Content-Type",
+			Value: "application/json",
+		}},
+		QueryParams: []HTTPQueryParam{{
+			Name:  "param",
+			Value: "some value", // We want to be sure this gets url-encoded
+		}},
+	})
+	require.NoError(t, err)
+	require.Equal(t, "GET", req.Method)
+	require.Equal(t, "http://example.com?param=some+value", req.URL.String())
+	require.Equal(t, "application/json", req.Header.Get("Content-Type"))
+}
+
+func Test_httpRequester_getClient(t *testing.T) {
+	testCases := []struct {
+		name       string
+		cfg        HTTPConfig
+		assertions func(*testing.T, *http.Client)
+	}{
+		{
+			name: "without insecureSkipTLSVerify",
+			assertions: func(t *testing.T, client *http.Client) {
+				require.NotNil(t, client)
+				transport, ok := client.Transport.(*http.Transport)
+				require.True(t, ok)
+				require.Nil(t, transport.TLSClientConfig)
+			},
+		},
+		{
+			name: "with insecureSkipTLSVerify",
+			cfg: HTTPConfig{
+				InsecureSkipTLSVerify: true,
+			},
+			assertions: func(t *testing.T, client *http.Client) {
+				require.NotNil(t, client)
+				transport, ok := client.Transport.(*http.Transport)
+				require.True(t, ok)
+				require.NotNil(t, transport.TLSClientConfig)
+				require.True(t, transport.TLSClientConfig.InsecureSkipVerify)
+			},
+		},
+	}
+	h := &httpRequester{}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			client := h.getClient(testCase.cfg)
+			testCase.assertions(t, client)
+		})
+	}
+}
+
+func Test_httpRequester_buildExprEnv(t *testing.T) {
+	testCases := []struct {
+		name       string
+		resp       *http.Response
+		assertions func(*testing.T, map[string]any, error)
+	}{
+		{
+			name: "without body",
+			resp: &http.Response{
+				StatusCode: http.StatusOK,
+				Header:     http.Header{"Content-Type": []string{"application/json"}},
+				Body:       io.NopCloser(strings.NewReader("")),
+			},
+			assertions: func(t *testing.T, env map[string]any, err error) {
+				require.NoError(t, err)
+				statusAny, ok := env["response"].(map[string]any)["status"]
+				require.True(t, ok)
+				status, ok := statusAny.(int)
+				require.True(t, ok)
+				require.Equal(t, http.StatusOK, status)
+				headerFnAny, ok := env["response"].(map[string]any)["header"]
+				require.True(t, ok)
+				headerFn, ok := headerFnAny.(func(string) string)
+				require.True(t, ok)
+				require.Equal(t, "application/json", headerFn("Content-Type"))
+				headersAny, ok := env["response"].(map[string]any)["headers"]
+				require.True(t, ok)
+				headers, ok := headersAny.(http.Header)
+				require.True(t, ok)
+				require.Equal(t, http.Header{"Content-Type": []string{"application/json"}}, headers)
+				bodyAny, ok := env["response"].(map[string]any)["body"]
+				require.True(t, ok)
+				body, ok := bodyAny.(map[string]any)
+				require.True(t, ok)
+				require.Empty(t, body)
+			},
+		},
+		{
+			name: "with body",
+			resp: &http.Response{
+				StatusCode: 200,
+				Header:     http.Header{"Content-Type": []string{"application/json"}},
+				Body:       io.NopCloser(strings.NewReader(`{"foo": "bar"}`)),
+			},
+			assertions: func(t *testing.T, env map[string]any, err error) {
+				require.NoError(t, err)
+				bodyAny, ok := env["response"].(map[string]any)["body"]
+				require.True(t, ok)
+				body, ok := bodyAny.(map[string]any)
+				require.True(t, ok)
+				require.Equal(t, map[string]any{"foo": "bar"}, body)
+			},
+		},
+	}
+	h := &httpRequester{}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			env, err := h.buildExprEnv(testCase.resp)
+			testCase.assertions(t, env, err)
+		})
+	}
+}
+
+func Test_httpRequester_wasRequestSuccessful(t *testing.T) {
+	testCases := []struct {
+		name       string
+		cfg        HTTPConfig
+		statusCode int
+		assertions func(t *testing.T, success bool, err error)
+	}{
+		{
+			name: "error compiling success expression",
+			cfg:  HTTPConfig{SuccessExpression: "(1 + 2"},
+			assertions: func(t *testing.T, _ bool, err error) {
+				require.ErrorContains(t, err, "error compiling success expression")
+			},
+		},
+		{
+			name: "error evaluating success expression",
+			cfg:  HTTPConfig{SuccessExpression: "invalid()"},
+			assertions: func(t *testing.T, _ bool, err error) {
+				require.ErrorContains(t, err, "error evaluating success expression")
+			},
+		},
+		{
+			name: "success expression evaluates to non-boolean",
+			cfg:  HTTPConfig{SuccessExpression: `"foo"`},
+			assertions: func(t *testing.T, _ bool, err error) {
+				require.ErrorContains(t, err, "success expression did not evaluate to a boolean")
+			},
+		},
+		{
+			name: "success expression evaluates to true",
+			cfg:  HTTPConfig{SuccessExpression: "true"},
+			assertions: func(t *testing.T, success bool, err error) {
+				require.NoError(t, err)
+				require.True(t, success)
+			},
+		},
+		{
+			name: "success expression evaluates to false",
+			cfg:  HTTPConfig{SuccessExpression: "false"},
+			assertions: func(t *testing.T, success bool, err error) {
+				require.NoError(t, err)
+				require.False(t, success)
+			},
+		},
+		{
+			name: "no success expression, but failure expression",
+			cfg:  HTTPConfig{FailureExpression: "true"},
+			assertions: func(t *testing.T, success bool, err error) {
+				require.NoError(t, err)
+				require.False(t, success)
+			},
+		},
+		{
+			name:       "no success or failure expression; good status code",
+			statusCode: 200,
+			assertions: func(t *testing.T, success bool, err error) {
+				require.NoError(t, err)
+				require.True(t, success)
+			},
+		},
+		{
+			name:       "no success or failure expression; bad status code",
+			statusCode: 404,
+			assertions: func(t *testing.T, success bool, err error) {
+				require.NoError(t, err)
+				require.False(t, success)
+			},
+		},
+	}
+	h := &httpRequester{}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			success, err := h.wasRequestSuccessful(testCase.cfg, testCase.statusCode, nil)
+			testCase.assertions(t, success, err)
+		})
+	}
+}
+
+func Test_httpRequester_didRequestFail(t *testing.T) {
+	testCases := []struct {
+		name       string
+		cfg        HTTPConfig
+		statusCode int
+		assertions func(t *testing.T, failed bool, err error)
+	}{
+		{
+			name: "error compiling failure expression",
+			cfg:  HTTPConfig{FailureExpression: "(1 + 2"},
+			assertions: func(t *testing.T, _ bool, err error) {
+				require.ErrorContains(t, err, "error compiling failure expression")
+			},
+		},
+		{
+			name: "error evaluating failure expression",
+			cfg:  HTTPConfig{FailureExpression: "invalid()"},
+			assertions: func(t *testing.T, _ bool, err error) {
+				require.ErrorContains(t, err, "error evaluating failure expression")
+			},
+		},
+		{
+			name: "failure expression evaluates to non-boolean",
+			cfg:  HTTPConfig{FailureExpression: `"foo"`},
+			assertions: func(t *testing.T, _ bool, err error) {
+				require.ErrorContains(t, err, "failure expression did not evaluate to a boolean")
+			},
+		},
+		{
+			name: "failure expression evaluates to true",
+			cfg:  HTTPConfig{FailureExpression: "true"},
+			assertions: func(t *testing.T, failed bool, err error) {
+				require.NoError(t, err)
+				require.True(t, failed)
+			},
+		},
+		{
+			name: "failure expression evaluates to false",
+			cfg:  HTTPConfig{FailureExpression: "false"},
+			assertions: func(t *testing.T, failed bool, err error) {
+				require.NoError(t, err)
+				require.False(t, failed)
+			},
+		},
+		{
+			name: "no failure expression, but success expression",
+			cfg:  HTTPConfig{SuccessExpression: "true"},
+			assertions: func(t *testing.T, failed bool, err error) {
+				require.NoError(t, err)
+				require.False(t, failed)
+			},
+		},
+		{
+			name:       "no success or failure expression; good status code",
+			statusCode: 200,
+			assertions: func(t *testing.T, failed bool, err error) {
+				require.NoError(t, err)
+				require.False(t, failed)
+			},
+		},
+		{
+			name:       "no success or failure expression; bad status code",
+			statusCode: 404,
+			assertions: func(t *testing.T, failed bool, err error) {
+				require.NoError(t, err)
+				require.True(t, failed)
+			},
+		},
+	}
+	h := &httpRequester{}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			failed, err := h.didRequestFail(testCase.cfg, testCase.statusCode, nil)
+			testCase.assertions(t, failed, err)
+		})
+	}
+}
+
+func Test_httpRequester_buildOutputs(t *testing.T) {
+	testCases := []struct {
+		name        string
+		outputExprs []HTTPOutput
+		assertions  func(t *testing.T, outputs map[string]any, err error)
+	}{
+		{
+			name: "no outputs specified",
+			assertions: func(t *testing.T, outputs map[string]any, err error) {
+				require.NoError(t, err)
+				require.Empty(t, outputs)
+			},
+		},
+		{
+			name: "error compiling output expression",
+			outputExprs: []HTTPOutput{{
+				Name:           "fake-output",
+				FromExpression: "(1 + 2",
+			}},
+			assertions: func(t *testing.T, _ map[string]any, err error) {
+				require.ErrorContains(t, err, "error compiling expression")
+			},
+		},
+		{
+			name: "error evaluating output expression",
+			outputExprs: []HTTPOutput{{
+				Name:           "fake-output",
+				FromExpression: "invalid()",
+			}},
+			assertions: func(t *testing.T, _ map[string]any, err error) {
+				require.ErrorContains(t, err, "error evaluating expression")
+			},
+		},
+		{
+			name: "success",
+			outputExprs: []HTTPOutput{
+				{
+					Name:           "string-output",
+					FromExpression: `"foo"`,
+				},
+				{
+					Name:           "int-output",
+					FromExpression: "42",
+				},
+			},
+			assertions: func(t *testing.T, outputs map[string]any, err error) {
+				require.NoError(t, err)
+				require.Equal(
+					t,
+					map[string]any{
+						"string-output": "foo",
+						"int-output":    42,
+					},
+					outputs,
+				)
+			},
+		},
+	}
+	h := &httpRequester{}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			outputs, err := h.buildOutputs(testCase.outputExprs, nil)
+			testCase.assertions(t, outputs, err)
+		})
+	}
+}
