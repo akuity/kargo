771c1b53c9cbd27f8183b25668d896eab2928ec4
Kent Rancourt <kent.rancourt@gmail.com>
2024-11-21 22:03:47 -0500

    do not attempt to infer desired revision when not specified
    Signed-off-by: Kent Rancourt <kent.rancourt@gmail.com>

--- internal/directives/argocd_updater.go
+++ internal/directives/argocd_updater.go
@@ -65,9 +65,7 @@ type argocdUpdater struct {
 	) (argocd.ApplicationSources, error)
 
 	mustPerformUpdateFn func(
-		context.Context,
 		*PromotionStepContext,
-		*ArgoCDUpdateConfig,
 		*ArgoCDAppUpdate,
 		*argocd.Application,
 		argocd.ApplicationSources,
@@ -184,13 +182,7 @@ func (a *argocdUpdater) runPromotionStep(
 			)
 		}
 
-		desiredRevisions, err := a.getDesiredRevisions(
-			ctx,
-			stepCtx,
-			&stepCfg,
-			update,
-			app,
-		)
+		desiredRevisions, err := a.getDesiredRevisions(stepCtx, update, app)
 		if err != nil {
 			return PromotionStepResult{Status: kargoapi.PromotionPhaseErrored}, fmt.Errorf(
 				"error determining desired revisions for Argo CD Application %q in "+
@@ -221,14 +213,7 @@ func (a *argocdUpdater) runPromotionStep(
 		}
 
 		// Check if the update needs to be performed and retrieve its phase.
-		phase, mustUpdate, err := a.mustPerformUpdateFn(
-			ctx,
-			stepCtx,
-			&stepCfg,
-			update,
-			app,
-			desiredSources,
-		)
+		phase, mustUpdate, err := a.mustPerformUpdateFn(stepCtx, update, app, desiredSources)
 
 		// If we have a phase, append it to the results.
 		if phase != "" {
@@ -373,9 +358,7 @@ updateLoop:
 }
 
 func (a *argocdUpdater) mustPerformUpdate(
-	ctx context.Context,
 	stepCtx *PromotionStepContext,
-	stepCfg *ArgoCDUpdateConfig,
 	update *ArgoCDAppUpdate,
 	app *argocd.Application,
 	desiredSources argocd.ApplicationSources,
@@ -453,13 +436,7 @@ func (a *argocdUpdater) mustPerformUpdate(
 	}
 
 	// Check if the desired revisions were applied.
-	desiredRevisions, err := a.getDesiredRevisions(
-		ctx,
-		stepCtx,
-		stepCfg,
-		update,
-		app,
-	)
+	desiredRevisions, err := a.getDesiredRevisions(stepCtx, update, app)
 	if err != nil {
 		return "", true, fmt.Errorf("error determining desired revisions: %w", err)
 	}
