c84e44ab793dc827c7ceb60ff5eb6047e9146ff3
Kent Rancourt <kent.rancourt@gmail.com>
2024-12-05 15:38:13 -0500

    chore: manual backport of refactored retries (#3060) (#3068)
    Signed-off-by: Kent Rancourt <kent.rancourt@gmail.com>

--- api/v1alpha1/promotion_types_test.go
+++ api/v1alpha1/promotion_types_test.go
@@ -2,16 +2,53 @@ package v1alpha1
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
 )
 
-func TestPromotionRetry_GetAttempts(t *testing.T) {
+func TestPromotionRetry_GetTimeout(t *testing.T) {
 	tests := []struct {
 		name     string
 		retry    *PromotionStepRetry
-		fallback int64
-		want     int64
+		fallback *time.Duration
+		want     *time.Duration
+	}{
+		{
+			name:     "retry is nil",
+			retry:    nil,
+			fallback: ptr.To(time.Hour),
+			want:     ptr.To(time.Hour),
+		},
+		{
+			name:     "timeout is not set",
+			retry:    &PromotionStepRetry{},
+			fallback: ptr.To(time.Hour),
+			want:     ptr.To(time.Hour),
+		},
+		{
+			name: "timeout is set",
+			retry: &PromotionStepRetry{
+				Timeout: &metav1.Duration{
+					Duration: 3 * time.Hour,
+				},
+			},
+			want: ptr.To(3 * time.Hour),
+		},
+	}
+	for _, tt := range tests {
+		require.Equal(t, tt.want, tt.retry.GetTimeout(tt.fallback))
+	}
+}
+
+func TestPromotionRetry_GetErrorThreshold(t *testing.T) {
+	tests := []struct {
+		name     string
+		retry    *PromotionStepRetry
+		fallback uint32
+		want     uint32
 	}{
 		{
 			name:     "retry is nil",
@@ -20,20 +57,20 @@ func TestPromotionRetry_GetAttempts(t *testing.T) {
 			want:     1,
 		},
 		{
-			name:     "attempts is not set",
+			name:     "threshold is not set",
 			retry:    &PromotionStepRetry{},
-			fallback: -1,
-			want:     -1,
+			fallback: 1,
+			want:     1,
 		},
 		{
-			name: "attempts is set",
+			name: "threshold is set",
 			retry: &PromotionStepRetry{
-				Attempts: 3,
+				ErrorThreshold: 3,
 			},
 			want: 3,
 		},
 	}
 	for _, tt := range tests {
-		require.Equal(t, tt.want, tt.retry.GetAttempts(tt.fallback))
+		require.Equal(t, tt.want, tt.retry.GetErrorThreshold(tt.fallback))
 	}
 }
