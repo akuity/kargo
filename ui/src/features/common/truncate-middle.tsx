6aabd3020e0e948cee1fbb0dbd517a756350e38b
Mayursinh Sarvaiya <marvinduff97@gmail.com>
2025-03-28 18:55:32 +0530

    fix(ui): freight artifact name overflow issue
    Signed-off-by: Mayursinh Sarvaiya <marvinduff97@gmail.com>

--- ui/src/features/common/truncate-middle.tsx
+++ ui/src/features/common/truncate-middle.tsx
@@ -2,7 +2,7 @@ export const TruncateMiddle = ({ children }: { children: string }) => {
   const start = children.slice(0, children.length - 3);
   const suffix = children.slice(-3).trim();
   return (
-    <div className='min-w-0 max-w-full flex flex-shrink'>
+    <div className='min-w-0 max-w-[76px] flex flex-shrink'>
       <div className='truncate'>{start}</div>
       <div className='flex-shrink-0'>{suffix}</div>
     </div>
