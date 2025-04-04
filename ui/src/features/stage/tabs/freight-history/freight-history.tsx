e2e663270478c9990fd42b90566311fd3ee54a1e
Rafal Pelczar <rafal@akuity.io>
2025-04-04 02:28:19 +0200

    add warehouse selector to freight history list
    Signed-off-by: Rafal Pelczar <rafal@akuity.io>

--- ui/src/features/stage/tabs/freight-history/freight-history.tsx
+++ ui/src/features/stage/tabs/freight-history/freight-history.tsx
@@ -1,7 +1,9 @@
 import { create } from '@bufbuild/protobuf';
 import { useQuery } from '@connectrpc/connect-query';
-import { Card, Descriptions, Flex, Space, Table, Tag } from 'antd';
+import { Card, Descriptions, Flex, Select, Space, Table, Tag } from 'antd';
 import { useMemo } from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import { generatePath, Link } from 'react-router-dom';
 
 import { paths } from '@ui/config/paths';
@@ -32,6 +34,7 @@ export const FreightHistory = ({
   // usually last one is active but we have to consider multi-pipeline case
   currentActiveFreight?: string;
 }) => {
+  const [selectedRequestedFreight, setSelectedRequestedFreight] = useState<FreightRequest>();
   const freightQuery = useQuery(queryFreight, { project: projectName });
 
   const freightMap = useMemo(() => {
@@ -73,81 +76,94 @@ export const FreightHistory = ({
     return freightHistoryPerWarehouse;
   }, [freightHistory]);
 
+  useEffect(() => {
+    if (requestedFreights?.[0]) {
+      setSelectedRequestedFreight(requestedFreights[0]);
+    }
+  }, [requestedFreights]);
+
   if (freightQuery.isFetching) {
     return <LoadingState />;
   }
 
+  const freightUniqueIdentifier = `${selectedRequestedFreight?.origin?.kind}/${selectedRequestedFreight?.origin?.name}`;
+  const freightReferences =
+    (freightHistoryPerWarehouse && freightHistoryPerWarehouse[freightUniqueIdentifier]) || [];
+
   return (
     <Flex vertical gap={16}>
-      {requestedFreights?.map((freight, i) => {
-        const freightUniqueIdentifier = `${freight.origin?.kind}/${freight.origin?.name}`;
+      <Card size='small'>
+        <Descriptions bordered className='max-w-md mb-4' size='small'>
+          <Descriptions.Item label={selectedRequestedFreight?.origin?.kind}>
+            <Select
+              value={freightUniqueIdentifier}
+              className='w-full'
+              onChange={(value) => {
+                const [kind, name] = value.split('/');
+                const newRequestedFreight = requestedFreights.find(
+                  (i) => i.origin?.kind === kind && i.origin?.name === name
+                );
 
-        const freightReferences = freightHistoryPerWarehouse[freightUniqueIdentifier] || [];
+                if (newRequestedFreight) {
+                  setSelectedRequestedFreight(newRequestedFreight);
+                }
+              }}
+              options={requestedFreights?.map((i) => ({
+                label: i.origin?.name,
+                value: `${i?.origin?.kind}/${i?.origin?.name}`
+              }))}
+            />
+          </Descriptions.Item>
+        </Descriptions>
 
-        return (
-          <Card size='small' key={i}>
-            <Descriptions bordered className='max-w-md mb-4' size='small'>
-              <Descriptions.Item label={freight.origin?.kind}>
+        <Table
+          dataSource={freightReferences}
+          size='small'
+          pagination={{ hideOnSinglePage: true }}
+          rowKey={(record, index) => `${record.name}${index}`}
+        >
+          <Table.Column<PlainMessage<FreightReference>>
+            title='ID'
+            width={100}
+            render={(value, record) =>
+              freightMap[record?.name || '']?.metadata?.name?.substring(0, 7)
+            }
+          />
+          <Table.Column<PlainMessage<FreightReference>>
+            title='Alias'
+            width={240}
+            render={(value, record, index) => (
+              <Space>
                 <Link
-                  to={generatePath(paths.warehouse, {
+                  to={generatePath(paths.freight, {
                     name: projectName,
-                    warehouseName: freight?.origin?.name
+                    freightName: record.name
                   })}
                 >
-                  {freight.origin?.name}
+                  {freightMap[record?.name || '']?.alias || record.name}
                 </Link>
-              </Descriptions.Item>
-            </Descriptions>
-
-            <Table
-              dataSource={freightReferences}
-              size='small'
-              pagination={{ hideOnSinglePage: true }}
-              rowKey={(record, index) => `${record.name}${index}`}
-            >
-              <Table.Column<PlainMessage<FreightReference>>
-                title='ID'
-                width={100}
-                render={(value, record) =>
-                  freightMap[record?.name || '']?.metadata?.name?.substring(0, 7)
-                }
+                {index === 0 && <Tag color='success'>Active</Tag>}
+              </Space>
+            )}
+          />
+          <Table.Column<PlainMessage<FreightReference>>
+            title='Contents'
+            render={(value, record) => (
+              <FreightContents
+                horizontal
+                fullContentVisibility
+                highlighted={false}
+                freight={create(FreightSchema, {
+                  metadata: {
+                    name: record.name
+                  },
+                  ...record
+                })}
               />
-              <Table.Column<PlainMessage<FreightReference>>
-                title='Alias'
-                render={(value, record, index) => (
-                  <Space>
-                    <Link
-                      to={generatePath(paths.freight, {
-                        name: projectName,
-                        freightName: record.name
-                      })}
-                    >
-                      {freightMap[record?.name || '']?.alias || record.name}
-                    </Link>
-                    {index === 0 && <Tag color='success'>Active</Tag>}
-                  </Space>
-                )}
-              />
-              <Table.Column<PlainMessage<FreightReference>>
-                title='Contents'
-                render={(value, record) => (
-                  <FreightContents
-                    horizontal
-                    fullContentVisibility
-                    highlighted={false}
-                    freight={create(FreightSchema, {
-                      metadata: {
-                        name: record.name
-                      },
-                      ...record
-                    })}
-                  />
-                )}
-              />
-            </Table>
-          </Card>
-        );
-      })}
+            )}
+          />
+        </Table>
+      </Card>
     </Flex>
   );
 };
