6b49a88514dfffa891a8c0335e2075ee2ee80343
Rafal <rafal@akuity.io>
2025-04-04 12:08:19 +0200

    feat(ui): Improve Stage Details view (#3773)
    Signed-off-by: Rafal Pelczar <rafal@akuity.io>

---
+++ ui/src/features/stage/tabs/freight-history/freight-history.tsx
@@ -0,0 +1,169 @@
+import { create } from '@bufbuild/protobuf';
+import { useQuery } from '@connectrpc/connect-query';
+import { Card, Descriptions, Flex, Select, Space, Table, Tag } from 'antd';
+import { useMemo } from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { generatePath, Link } from 'react-router-dom';
+
+import { paths } from '@ui/config/paths';
+import { queryFreight } from '@ui/gen/api/service/v1alpha1/service-KargoService_connectquery';
+import {
+  Freight,
+  FreightReference,
+  FreightRequest,
+  FreightSchema,
+  StageStatus
+} from '@ui/gen/api/v1alpha1/generated_pb';
+import { PlainMessage } from '@ui/utils/connectrpc-utils';
+
+import { LoadingState } from '../../../common';
+import { FreightContents } from '../../../freight-timeline/freight-contents';
+
+export const FreightHistory = ({
+  projectName,
+  freightHistory,
+  requestedFreights
+}: {
+  requestedFreights: FreightRequest[];
+  projectName: string;
+  // show the freight history thats 1:1 with requested freight
+  freightHistory?: StageStatus['freightHistory'];
+  // freight hash name which is active at the moment
+  // you can get this from lastPromotion in stage status
+  // usually last one is active but we have to consider multi-pipeline case
+  currentActiveFreight?: string;
+}) => {
+  const [selectedRequestedFreight, setSelectedRequestedFreight] = useState<FreightRequest>();
+  const freightQuery = useQuery(queryFreight, { project: projectName });
+
+  const freightMap = useMemo(() => {
+    const freightData = freightQuery.data;
+    // generate metadata.name -> full freight data (because history doesn't have it all) to show in freight history
+    const freightMap: Record<string, Freight> = {};
+
+    for (const freight of freightData?.groups?.['']?.freight || []) {
+      const freightId = freight?.metadata?.name;
+      if (freightId) {
+        freightMap[freightId] = freight;
+      }
+    }
+
+    return freightMap;
+  }, [freightQuery.data]);
+
+  const freightHistoryPerWarehouse = useMemo(() => {
+    // to show the history
+    const freightHistoryPerWarehouse: Record<
+      string /* warehouse eg. Warehouse/w-1 or Warehouse/w-2 */,
+      PlainMessage<FreightReference>[]
+    > = {};
+
+    for (const freightCollection of freightHistory || []) {
+      // key - value
+      // warehouse identifier - freight reference
+      const items = freightCollection?.items || {};
+
+      for (const [warehouseIdentifier, freightReference] of Object.entries(items)) {
+        if (!freightHistoryPerWarehouse[warehouseIdentifier]) {
+          freightHistoryPerWarehouse[warehouseIdentifier] = [];
+        }
+
+        freightHistoryPerWarehouse[warehouseIdentifier].push(freightReference);
+      }
+    }
+
+    return freightHistoryPerWarehouse;
+  }, [freightHistory]);
+
+  useEffect(() => {
+    if (requestedFreights?.[0]) {
+      setSelectedRequestedFreight(requestedFreights[0]);
+    }
+  }, [requestedFreights]);
+
+  if (freightQuery.isFetching) {
+    return <LoadingState />;
+  }
+
+  const freightUniqueIdentifier = `${selectedRequestedFreight?.origin?.kind}/${selectedRequestedFreight?.origin?.name}`;
+  const freightReferences =
+    (freightHistoryPerWarehouse && freightHistoryPerWarehouse[freightUniqueIdentifier]) || [];
+
+  return (
+    <Flex vertical gap={16}>
+      <Card size='small'>
+        <Descriptions bordered className='max-w-md mb-4' size='small'>
+          <Descriptions.Item label={selectedRequestedFreight?.origin?.kind}>
+            <Select
+              value={freightUniqueIdentifier}
+              className='w-full'
+              onChange={(value) => {
+                const [kind, name] = value.split('/');
+                const newRequestedFreight = requestedFreights.find(
+                  (i) => i.origin?.kind === kind && i.origin?.name === name
+                );
+
+                if (newRequestedFreight) {
+                  setSelectedRequestedFreight(newRequestedFreight);
+                }
+              }}
+              options={requestedFreights?.map((i) => ({
+                label: i.origin?.name,
+                value: `${i?.origin?.kind}/${i?.origin?.name}`
+              }))}
+            />
+          </Descriptions.Item>
+        </Descriptions>
+
+        <Table
+          dataSource={freightReferences}
+          size='small'
+          pagination={{ hideOnSinglePage: true }}
+          rowKey={(record, index) => `${record.name}${index}`}
+        >
+          <Table.Column<PlainMessage<FreightReference>>
+            title='ID'
+            width={100}
+            render={(value, record) =>
+              freightMap[record?.name || '']?.metadata?.name?.substring(0, 7)
+            }
+          />
+          <Table.Column<PlainMessage<FreightReference>>
+            title='Alias'
+            width={240}
+            render={(value, record, index) => (
+              <Space>
+                <Link
+                  to={generatePath(paths.freight, {
+                    name: projectName,
+                    freightName: record.name
+                  })}
+                >
+                  {freightMap[record?.name || '']?.alias || record.name}
+                </Link>
+                {index === 0 && <Tag color='success'>Active</Tag>}
+              </Space>
+            )}
+          />
+          <Table.Column<PlainMessage<FreightReference>>
+            title='Contents'
+            render={(value, record) => (
+              <FreightContents
+                horizontal
+                fullContentVisibility
+                highlighted={false}
+                freight={create(FreightSchema, {
+                  metadata: {
+                    name: record.name
+                  },
+                  ...record
+                })}
+              />
+            )}
+          />
+        </Table>
+      </Card>
+    </Flex>
+  );
+};
--- ui/src/features/stage/tabs/settings/stage-edit-form.tsx
+++ ui/src/features/stage/tabs/settings/stage-edit-form.tsx
@@ -1,13 +1,13 @@
 import { useMutation, useQuery } from '@connectrpc/connect-query';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Button, Modal, Space, Typography } from 'antd';
+import { Button, Flex, message, Space, Typography } from 'antd';
 import type { JSONSchema4 } from 'json-schema';
 import { useForm } from 'react-hook-form';
+import { useParams } from 'react-router-dom';
 import { z } from 'zod';
 
 import { YamlEditor } from '@ui/features/common/code-editor/yaml-editor';
 import { FieldContainer } from '@ui/features/common/form/field-container';
-import { ModalComponentProps } from '@ui/features/common/modal/modal-context';
 import {
   getStage,
   updateResource
@@ -17,18 +17,14 @@ import schema from '@ui/gen/schema/stages.kargo.akuity.io_v1alpha1.json';
 import { decodeRawData } from '@ui/utils/decode-raw-data';
 import { zodValidators } from '@ui/utils/validators';
 
-import { getStageYAMLExample } from '../project/pipelines/utils/stage-yaml-example';
-
-type Props = ModalComponentProps & {
-  projectName: string;
-  stageName: string;
-};
+import { getStageYAMLExample } from '../../../project/pipelines/utils/stage-yaml-example';
 
 const formSchema = z.object({
   value: zodValidators.requiredString
 });
 
-export const EditStageModal = ({ visible, hide, projectName, stageName }: Props) => {
+export const StageEditForm = () => {
+  const { name: projectName, stageName } = useParams();
   const { data, isLoading } = useQuery(getStage, {
     project: projectName,
     name: stageName,
@@ -36,7 +32,7 @@ export const EditStageModal = ({ visible, hide, projectName, stageName }: Props)
   });
 
   const { mutateAsync, isPending } = useMutation(updateResource, {
-    onSuccess: () => hide()
+    onSuccess: () => message.success('Stage has been updated')
   });
 
   const { control, handleSubmit } = useForm({
@@ -54,26 +50,7 @@ export const EditStageModal = ({ visible, hide, projectName, stageName }: Props)
   });
 
   return (
-    <Modal
-      destroyOnClose
-      open={visible}
-      title='Edit Stage'
-      closable={false}
-      width={680}
-      footer={
-        <div className='flex items-center justify-between'>
-          <Typography.Link href='https://docs.kargo.io/quickstart/#the-test-stage' target='_blank'>
-            Documentation
-          </Typography.Link>
-          <Space>
-            <Button onClick={hide}>Cancel</Button>
-            <Button type='primary' onClick={onSubmit} loading={isPending}>
-              Update
-            </Button>
-          </Space>
-        </div>
-      }
-    >
+    <>
       <FieldContainer name='value' control={control}>
         {({ field: { value, onChange } }) => (
           <YamlEditor
@@ -81,7 +58,7 @@ export const EditStageModal = ({ visible, hide, projectName, stageName }: Props)
             onChange={(e) => onChange(e || '')}
             height='500px'
             schema={schema as JSONSchema4}
-            placeholder={getStageYAMLExample(projectName)}
+            placeholder={projectName && getStageYAMLExample(projectName)}
             isLoading={isLoading}
             isHideManagedFieldsDisplayed
             label='YAML'
@@ -89,6 +66,17 @@ export const EditStageModal = ({ visible, hide, projectName, stageName }: Props)
           />
         )}
       </FieldContainer>
-    </Modal>
+
+      <Flex justify='space-between'>
+        <Typography.Link href='https://docs.kargo.io/quickstart/#the-test-stage' target='_blank'>
+          Documentation
+        </Typography.Link>
+        <Space>
+          <Button type='primary' onClick={onSubmit} loading={isPending}>
+            Update
+          </Button>
+        </Space>
+      </Flex>
+    </>
   );
 };
