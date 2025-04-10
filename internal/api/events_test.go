e8aed975083c18705900b5919bdfc1cf7c317a33
fuskovic <fhuskovic92@gmail.com>
2025-04-10 19:24:01 -0400

    address codecov missing coverage
    Signed-off-by: fuskovic <fhuskovic92@gmail.com>

---
+++ internal/api/events_test.go
@@ -0,0 +1,57 @@
+package api
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	kargoapi "github.com/akuity/kargo/api/v1alpha1"
+	"github.com/akuity/kargo/internal/server/user"
+)
+
+func TestFormatEventUserActorForOidc(t *testing.T) {
+	for _, test := range []struct {
+		name     string
+		user     user.Info
+		expected string
+	}{
+		{
+			name:     "admin",
+			user:     user.Info{IsAdmin: true},
+			expected: kargoapi.EventActorAdmin,
+		},
+		{
+			name: "email",
+			user: user.Info{
+				Claims: map[string]any{
+					"email": "email@inbox.com",
+				},
+			},
+			expected: kargoapi.EventActorEmailPrefix + "email@inbox.com",
+		},
+		{
+			name: "sub",
+			user: user.Info{
+				Claims: map[string]any{
+					"email": "email@inbox.com",
+				},
+			},
+			expected: kargoapi.EventActorEmailPrefix + "email@inbox.com",
+		},
+		{
+			name: "oidc-username",
+			user: user.Info{
+				Claims: map[string]any{
+					"oidc-provider": "oidc-username",
+				},
+				Username: "oidc-provider",
+			},
+			expected: kargoapi.EventActorOidcUsernamePrefix + "oidc-username",
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			result := FormatEventUserActor(test.user)
+			require.Equal(t, test.expected, result)
+		})
+	}
+}
