095527fc6f8cd46eeb4dfed7c29fa08362550b63
Kent Rancourt <kent.rancourt@gmail.com>
2025-04-14 12:52:22 -0400

    fix lint issues
    Signed-off-by: Kent Rancourt <kent.rancourt@gmail.com>

--- internal/gitprovider/gitlab/gitlab.go
+++ internal/gitprovider/gitlab/gitlab.go
@@ -120,11 +120,11 @@ func (p *provider) CreatePullRequest(
 		opts = &gitprovider.CreatePullRequestOpts{}
 	}
 	glMR, _, err := p.client.CreateMergeRequest(p.projectName, &gitlab.CreateMergeRequestOptions{
-		Title:              &opts.Title,
-		Description:        &opts.Description,
-		Labels:             (*gitlab.LabelOptions)(&opts.Labels),
-		SourceBranch:       &opts.Head,
-		TargetBranch:       &opts.Base,
+		Title:        &opts.Title,
+		Description:  &opts.Description,
+		Labels:       (*gitlab.LabelOptions)(&opts.Labels),
+		SourceBranch: &opts.Head,
+		TargetBranch: &opts.Base,
 	})
 	if err != nil {
 		return nil, err
