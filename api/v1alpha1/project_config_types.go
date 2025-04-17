8766fe25bc664b51cb4495953d35ea5d12f21a76
Hidde Beydals <hidde@hhh.computer>
2025-04-17 23:59:41 +0200

    feat(api): add selectors to PromotionPolicy
    Signed-off-by: Hidde Beydals <hidde@hhh.computer>

--- api/v1alpha1/project_config_types.go
+++ api/v1alpha1/project_config_types.go
@@ -19,15 +19,23 @@ type ProjectConfig struct {
 type ProjectConfigSpec struct {
 	// PromotionPolicies defines policies governing the promotion of Freight to
 	// specific Stages within the Project.
+	//
+	// +kubebuilder:validation:XValidation:message="PromotionPolicy must have exactly one of stage or stageSelector set",rule="has(self.stage) ? !has(self.stageSelector) : has(self.stageSelector)"
 	PromotionPolicies []PromotionPolicy `json:"promotionPolicies,omitempty" protobuf:"bytes,1,rep,name=promotionPolicies"`
 }
 
 // PromotionPolicy defines policies governing the promotion of Freight to a
 // specific Stage.
 type PromotionPolicy struct {
-	// +kubebuilder:validation:MinLength=1
+	// Stage is the name of the Stage to which this policy applies.
+	//
+	// Deprecated: Use StageSelector instead.
+	//
 	// +kubebuilder:validation:Pattern=^[a-z0-9]([-a-z0-9]*[a-z0-9])?(\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*$
-	Stage string `json:"stage" protobuf:"bytes,1,opt,name=stage"`
+	Stage string `json:"stage,omitempty" protobuf:"bytes,1,opt,name=stage"`
+	// StageSelector is a selector that matches the Stage resource to which
+	// this policy applies.
+	StageSelector *PromotionPolicySelector `json:"stageSelector,omitempty" protobuf:"bytes,3,opt,name=stageSelector"`
 	// AutoPromotionEnabled indicates whether new Freight can automatically be
 	// promoted into the Stage referenced by the Stage field. Note: There are may
 	// be other conditions also required for an auto-promotion to occur. This
@@ -36,6 +44,21 @@ type PromotionPolicy struct {
 	// users to define Stages that are automatically updated as soon as new
 	// artifacts are detected.
 	AutoPromotionEnabled bool `json:"autoPromotionEnabled,omitempty" protobuf:"varint,2,opt,name=autoPromotionEnabled"`
+}
+
+type PromotionPolicySelector struct {
+	// Name is the name of the resource to which this policy applies.
+	//
+	// It selects a resource based on its name. It supports exact name matching,
+	// regex matching (with prefix "regex:"), or glob pattern matching (with
+	// prefix "glob:").
+	//
+	// +optional
+	Name string `json:"name,omitempty" protobuf:"bytes,1,opt,name=name"`
+
+	// LabelSelector is a selector that matches the resource to which this policy
+	// applies.
+	*metav1.LabelSelector `json:",inline" protobuf:"bytes,2,opt,name=labelSelector"`
 }
 
 // +kubebuilder:object:root=true
