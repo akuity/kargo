2991f85b5f596212b3971c0374abdc05079e0133
Hidde Beydals <hidde@hhh.computer>
2025-05-01 21:22:41 +0200

    fix(ui): please linter
    Signed-off-by: Hidde Beydals <hidde@hhh.computer>

--- ui/src/features/stage/create-stage.tsx
+++ ui/src/features/stage/create-stage.tsx
@@ -120,6 +120,7 @@ export const CreateStage = ({
           uses: step?.identifier,
           as: step?.as || '',
           if: '',
+          continueOnError: false,
           config: step?.state as JSON, // step.state is type 'object' and it is safe to fake JSON type because it doesn't matter for stageFormToYAML function
           vars: []
         }))
@@ -171,6 +172,7 @@ export const CreateStage = ({
                   uses: step?.identifier,
                   as: step?.as || '',
                   if: '',
+                  continueOnError: false,
                   config: step?.state as JSON, // step.state is type 'object' and it is safe to fake JSON type because it doesn't matter for stageFormToYAML function
                   vars: []
                 }))
