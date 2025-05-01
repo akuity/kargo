c91e1625c570bc3eefef178fec685b636bc64a6f
John Pekcan <johnpekcan@gmail.com>
2025-05-01 21:20:50 +0000

    add targetFreight to meta map
    Signed-off-by: John Pekcan <johnpekcan@gmail.com>

--- internal/promotion/promotion.go
+++ internal/promotion/promotion.go
@@ -185,18 +185,18 @@ func (s *Step) GetErrorThreshold(runner promotion.StepRunner) uint32 {
 func (s *Step) BuildEnv(promoCtx Context, opts ...StepEnvOption) map[string]any {
 	env := map[string]any{
 		"ctx": map[string]any{
-			"project":   promoCtx.Project,
-			"promotion": promoCtx.Promotion,
-			"stage":     promoCtx.Stage,
-			"targetFreight": map[string]any{
-				"name": promoCtx.TargetFreightRef.Name,
-				"origin": map[string]any{
-					"name": promoCtx.TargetFreightRef.Origin.Name,
-				},
-			},
+			"project":       promoCtx.Project,
+			"promotion":     promoCtx.Promotion,
+			"stage":         promoCtx.Stage,
+			"targetFreight": promoCtx.TargetFreightRef.Name,
 			"meta": map[string]any{
 				"promotion": map[string]any{
 					"actor": promoCtx.Actor,
+				},
+				"targetFreight": map[string]any{
+					"origin": map[string]any{
+						"name": promoCtx.TargetFreightRef.Origin.Name,
+					},
 				},
 			},
 		},
