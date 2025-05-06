2fb42e1c70a21080976007a78ba96939d45147d2
fuskovic <fhuskovic92@gmail.com>
2025-05-06 18:28:21 -0400

    add handler test
    Signed-off-by: fuskovic <fhuskovic92@gmail.com>

--- internal/webhook/external/handlers/refresh_warehouse_test.go
+++ internal/webhook/external/handlers/refresh_warehouse_test.go
@@ -2,33 +2,117 @@ package handlers
 
 import (
 	"bytes"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
 	"net/http"
+	"net/http/httptest"
+	"os"
 	"testing"
 
+	kargoapi "github.com/akuity/kargo/api/v1alpha1"
+	"github.com/akuity/kargo/internal/logging"
 	"github.com/akuity/kargo/internal/webhook/external/providers"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
 
-func TestRefreshWarehouse(t *testing.T) {
-	// logger := logging.NewLogger(logging.InfoLevel)
-	// kubeClient := fake.NewClientBuilder().Build()
-	// httpClient := cleanhttp.DefaultClient()
+func TestRefreshWarehouseWebhook(t *testing.T) {
+	kubeClient := fake.NewClientBuilder().Build()
+	require.NoError(t,
+		kargoapi.AddToScheme(kubeClient.Scheme()),
+	)
+	handler := NewRefreshWarehouseWebhook(
+		providers.Github,
+		logging.NewLogger(logging.InfoLevel),
+		kubeClient,
+	)
+	serverURL := "http://doesntmatter.com"
 
 	for _, test := range []struct {
 		name         string
-		providerName providers.Name
-		req          func() *http.Request
+		setup        func() *http.Request
+		expectedCode int
 	}{
 		{
-			name: "success",
-			req: func() *http.Request {
-				return nil
+			name: "OK",
+			setup: func() *http.Request {
+				secret := uuid.New().String()
+				os.Setenv("GH_WEBHOOK_SECRET", secret)
+				req := httptest.NewRequest(
+					http.MethodPost,
+					serverURL,
+					mockRequestPayload,
+				)
+				req.Header.Set("X-Hub-Signature-256", sign(t, secret))
+				req.Header.Set("X-GitHub-Event", "push")
+				return req
 			},
+			expectedCode: http.StatusOK,
+		},
+		{
+			name: "internal server error",
+			setup: func() *http.Request {
+				os.Clearenv()
+				return httptest.NewRequest(
+					http.MethodPost,
+					serverURL,
+					mockRequestPayload,
+				)
+			},
+			expectedCode: http.StatusInternalServerError,
+		},
+		{
+			name: "unauthorized",
+			setup: func() *http.Request {
+				os.Setenv("GH_WEBHOOK_SECRET", uuid.New().String())
+				return httptest.NewRequest(
+					http.MethodPost,
+					serverURL,
+					mockRequestPayload,
+				)
+			},
+			expectedCode: http.StatusUnauthorized,
+		},
+		{
+			name: "bad request",
+			setup: func() *http.Request {
+				secret := uuid.New().String()
+				os.Setenv("GH_WEBHOOK_SECRET", secret)
+				req := httptest.NewRequest(
+					http.MethodPost,
+					serverURL,
+					mockRequestPayload,
+				)
+				req.Header.Set("X-Hub-Signature-256", sign(t, secret))
+				req.Header.Set("X-GitHub-Event", "ping")
+				return req
+			},
+			expectedCode: http.StatusBadRequest,
 		},
 	} {
 		t.Run(test.name, func(t *testing.T) {
-
+			req := test.setup()
+			w := httptest.NewRecorder()
+			handler(w, req)
+			require.Equal(t,
+				test.expectedCode,
+				w.Code,
+			)
 		})
 	}
+}
+
+func sign(t *testing.T, secret string) string {
+	t.Helper()
+
+	mac := hmac.New(sha256.New, []byte(secret))
+	mac.Write(mockRequestPayload.Bytes())
+	return fmt.Sprintf("sha256=%s",
+		hex.EncodeToString(mac.Sum(nil)),
+	)
 }
 
 var mockRequestPayload = bytes.NewBuffer([]byte(`
@@ -37,14 +121,14 @@ var mockRequestPayload = bytes.NewBuffer([]byte(`
 	"before": "1fe030abc48d0d0ee7b3d650d6e9449775990318",
 	"after": "f12cd167152d80c0a2e28cb45e827c6311bba910",
 	"repository": {
-	  "html_url": "https://github.com/akuityio/ee-server-poc",
+	  "html_url": "https://github.com/username/repo"
 	},
 	"pusher": {
-	  "name": "akuityio",
-	  "email": "fhuskovic92@gmail.com"
+	  "name": "username",
+	  "email": "email@inbox.com"
 	},
 	"head_commit": {
-	  "id": "f12cd167152d80c0a2e28cb45e827c6311bba910",
+	  "id": "f12cd167152d80c0a2e28cb45e827c6311bba910"
 	}
   }	
 `))
