ecf37482d300b46afc82ae002258e4cbc5e4c3cf
fuskovic <fhuskovic92@gmail.com>
2025-05-08 13:42:46 -0400

    make error message more consistent
    Signed-off-by: fuskovic <fhuskovic92@gmail.com>

--- cmd/controlplane/external_webhooks.go
+++ cmd/controlplane/external_webhooks.go
@@ -78,7 +78,7 @@ func (o *externalWebhooksServerOptions) run(ctx context.Context) error {
 	}
 
 	if err = kargoapi.AddToScheme(cluster.GetClient().Scheme()); err != nil {
-		return fmt.Errorf("failed to add custom kargo type to runtime scheme: %w", err)
+		return fmt.Errorf("error adding Kargo API to Kargo manager scheme: %w", err)
 	}
 
 	err = cluster.GetFieldIndexer().IndexField(
