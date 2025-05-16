5a4e32c3ecbe6f5da85cb400886b9e314424dc9e
Mayursinh Sarvaiya <marvinduff97@gmail.com>
2025-05-16 11:51:31 +0530

    fix(ui): map promotion policies from projectconfig
    Signed-off-by: Mayursinh Sarvaiya <marvinduff97@gmail.com>

--- ui/src/features/project/pipelines/use-stage-auto-promotion-map.ts
+++ ui/src/features/project/pipelines/use-stage-auto-promotion-map.ts
@@ -1,14 +1,28 @@
 import { useMemo } from 'react';
 
-import { Project } from '@ui/gen/api/v1alpha1/generated_pb';
+import { Project, ProjectConfig } from '@ui/gen/api/v1alpha1/generated_pb';
 
-export const useStageAutoPromotionMap = (project: Project): Record<string, boolean> =>
+export const useStageAutoPromotionMap = (
+  project: Project,
+  projectConfig: ProjectConfig
+): Record<string, boolean> =>
   useMemo(() => {
     const map: Record<string, boolean> = {};
 
+    // deprecated
     for (const policy of project?.spec?.promotionPolicies || []) {
       if (policy.stage) {
         map[policy.stage] = policy.autoPromotionEnabled;
+      }
+    }
+
+    for (const policy of projectConfig?.spec?.promotionPolicies || []) {
+      if (policy.stage) {
+        map[policy.stage] = policy.autoPromotionEnabled;
+      }
+
+      if (policy.stageSelector?.name) {
+        map[policy.stageSelector?.name || ''] = policy.autoPromotionEnabled;
       }
     }
 
