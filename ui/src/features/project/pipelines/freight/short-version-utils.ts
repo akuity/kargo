f6ebac9cbef5ed3b15a41378d631533d31e01dca
Mayursinh Sarvaiya <marvinduff97@gmail.com>
2025-05-19 19:54:31 +0530

    fix(ui): valid short version (#4119)
    Signed-off-by: Mayursinh Sarvaiya <marvinduff97@gmail.com>

--- ui/src/features/project/pipelines/freight/short-version-utils.ts
+++ ui/src/features/project/pipelines/freight/short-version-utils.ts
@@ -1,9 +1,9 @@
 export const shortVersion = (version: string = '') => {
-  if (version.length <= 10) {
+  if (version.length <= 12) {
     return version;
   }
 
-  const prefix = version.slice(0, 7);
+  const prefix = version.slice(0, 6);
   const suffix = version.slice(-6);
 
   return `${prefix}...${suffix}`;
