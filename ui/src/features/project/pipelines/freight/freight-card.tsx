7ca9fe516af1d5ef7bd7c86dca92e4bad64d2d13
Mayursinh Sarvaiya <marvinduff97@gmail.com>
2025-05-23 17:01:33 +0530

    fix(ui): git provider deeplink and cred form schema (#4155)
    Signed-off-by: Mayursinh Sarvaiya <marvinduff97@gmail.com>

--- ui/src/features/project/pipelines/freight/freight-card.tsx
+++ ui/src/features/project/pipelines/freight/freight-card.tsx
@@ -107,126 +107,158 @@ export const FreightCard = (props: FreightCardProps) => {
     );
   } else {
     CardContent = (
-      <div
-        className={classNames('relative px-2', {
-          'pl-4': props.inUse
-        })}
-      >
-        <Flex align='center' justify='space-between' gap={4} className='-mr-2'>
-          <Typography.Text className='text-xs text-nowrap' type='secondary'>
-            <FontAwesomeIcon className='mr-1' icon={faWarehouse} size='xs' />
-            {props.freight?.origin?.name}
-          </Typography.Text>
-          <Dropdown
-            menu={{
-              items: [
-                {
-                  key: 'manually-approve',
-                  label: 'Manually Approve',
-                  icon: <FontAwesomeIcon icon={faCheck} />,
-                  onClick: (e) => {
-                    e.domEvent.stopPropagation();
-                    actionContext?.actManuallyApprove(props.freight);
-                  }
-                },
-                {
-                  key: 'delete-freight',
-                  label: 'Delete Freight',
-                  icon: <FontAwesomeIcon icon={faTrash} />,
-                  onClick: (e) => {
-                    e.domEvent.stopPropagation();
-                    deleteFreightModal.show((modalProps) => (
-                      <DeleteFreightModal
-                        freight={props.freight}
-                        onDelete={modalProps.hide}
-                        {...modalProps}
-                      />
-                    ));
+      <>
+        <div
+          className={classNames('relative px-2', {
+            'pl-4': props.inUse
+          })}
+        >
+          <Flex align='center' justify='space-between' gap={4} className='-mr-2'>
+            <Typography.Text className='text-xs text-nowrap' type='secondary'>
+              <FontAwesomeIcon className='mr-1' icon={faWarehouse} size='xs' />
+              {props.freight?.origin?.name}
+            </Typography.Text>
+            <Dropdown
+              menu={{
+                items: [
+                  {
+                    key: 'manually-approve',
+                    label: 'Manually Approve',
+                    icon: <FontAwesomeIcon icon={faCheck} />,
+                    onClick: (e) => {
+                      e.domEvent.stopPropagation();
+                      actionContext?.actManuallyApprove(props.freight);
+                    }
                   },
-                  disabled: props.inUse
-                }
-              ]
-            }}
-          >
-            <Button
-              icon={<FontAwesomeIcon icon={faEllipsis} />}
-              size='small'
-              type='text'
-              onClick={(e) => e.stopPropagation()}
-            />
-          </Dropdown>
-        </Flex>
-        <Divider className='mt-0 mb-1' />
-
-        {props.inUse && !props?.preferredFilter?.showColors && (
-          <div className='absolute top-1 bottom-1 left-1 w-1.5 rounded bg-lime-200' />
-        )}
-        {props?.preferredFilter?.showColors && (
-          <div className='flex flex-col gap-0.5 justify-center absolute left-1 top-1 bottom-1'>
-            {props.stagesInFreight.map((stage) => (
-              <div
-                key={stage?.metadata?.uid}
-                title={stage?.metadata?.name}
-                className='w-1.5 rounded flex-1'
-                style={{
-                  background: `linear-gradient(60deg,rgb(255 255 255/0%),rgb(200 200 200/30%)), ${props.stageColorMap[stage?.metadata?.name || '']}`
-                }}
+                  {
+                    key: 'delete-freight',
+                    label: 'Delete Freight',
+                    icon: <FontAwesomeIcon icon={faTrash} />,
+                    onClick: (e) => {
+                      e.domEvent.stopPropagation();
+                      deleteFreightModal.show((modalProps) => (
+                        <DeleteFreightModal
+                          freight={props.freight}
+                          onDelete={modalProps.hide}
+                          {...modalProps}
+                        />
+                      ));
+                    },
+                    disabled: props.inUse
+                  }
+                ]
+              }}
+            >
+              <Button
+                icon={<FontAwesomeIcon icon={faEllipsis} />}
+                size='small'
+                type='text'
+                onClick={(e) => e.stopPropagation()}
               />
-            ))}
+            </Dropdown>
+          </Flex>
+          <Divider className='mt-0 mb-1' />
+
+          {props.inUse && !props?.preferredFilter?.showColors && (
+            <div className='absolute top-1 bottom-1 left-1 w-1.5 rounded bg-lime-200' />
+          )}
+          {props?.preferredFilter?.showColors && (
+            <div className='flex flex-col gap-0.5 justify-center absolute left-1 top-1 bottom-1'>
+              {props.stagesInFreight.map((stage) => (
+                <div
+                  key={stage?.metadata?.uid}
+                  title={stage?.metadata?.name}
+                  className='w-1.5 rounded flex-1'
+                  style={{
+                    background: `linear-gradient(60deg,rgb(255 255 255/0%),rgb(200 200 200/30%)), ${props.stageColorMap[stage?.metadata?.name || '']}`
+                  }}
+                />
+              ))}
+            </div>
+          )}
+
+          {props?.preferredFilter?.showAlias && (
+            <div className='text-[10px] text-nowrap mb-2'>{freightAlias}</div>
+          )}
+
+          <div className='flex flex-col gap-1 justify-center items-center'>
+            {props.freight?.commits
+              ?.slice(0, 2)
+              .map((commit) => <FreightArtifact key={commit?.repoURL} artifact={commit} />)}
+
+            {props.freight?.charts
+              ?.slice(0, 2)
+              .map((chart) => <FreightArtifact key={chart?.repoURL} artifact={chart} />)}
+
+            {props.freight?.images
+              ?.slice(0, 2)
+              .map((image) => <FreightArtifact key={image?.repoURL} artifact={image} />)}
+
+            {noOfGitCommits + noOfHelmReleases + noOfContainerImages > 6 && (
+              <Typography.Text type='secondary' className='text-[10px]'>
+                +
+                {noOfGitCommits +
+                  noOfHelmReleases +
+                  noOfContainerImages -
+                  (props.freight?.charts?.slice(0, 2)?.length +
+                    props.freight?.charts?.slice(0, 2)?.length +
+                    props.freight?.images?.slice(0, 2)?.length)}{' '}
+                more
+              </Typography.Text>
+            )}
+          </div>
+
+          <div className='flex flex-col mx-auto w-full gap-0.5 items-center justify-center text-nowrap my-1'>
+            {(noOfGitCommits ? 1 : 0) + (noOfHelmReleases ? 1 : 0) + (noOfContainerImages ? 1 : 0) >
+              2 && (
+              <>
+                <FreightCard.ArtifactCount icon={faGithub} count={noOfGitCommits} />
+
+                <FreightCard.ArtifactCount icon={faAnchor} count={noOfHelmReleases} />
+
+                <FreightCard.ArtifactCount icon={faDocker} count={noOfContainerImages} />
+              </>
+            )}
+            <Typography.Text
+              className='text-xs text-nowrap'
+              type='secondary'
+              title={creation.abs?.toString()}
+            >
+              {creation.relative}
+            </Typography.Text>
+          </div>
+        </div>
+
+        {soakTimeFormatted && !props.promote && (
+          <div className='px-1 pb-1'>
+            <Button
+              disabled
+              onClick={(e) => e.stopPropagation()}
+              size='small'
+              className='text-[10px] text-center w-[230px] flex font-semibold'
+            >
+              <span className='mr-auto'>Soak: {soakTimeFormatted}</span>
+              <Progress strokeWidth={12} type='circle' size={14} percent={soakTimePercentage} />
+            </Button>
           </div>
         )}
 
-        {props?.preferredFilter?.showAlias && (
-          <div className='text-[10px] text-nowrap mb-2'>{freightAlias}</div>
+        {props.promote && (
+          <div className='px-1 pb-1'>
+            <Button
+              className='w-full'
+              type='primary'
+              size='small'
+              onClick={(e) => {
+                e.stopPropagation();
+                props.onReviewAndPromote?.();
+              }}
+            >
+              Select
+            </Button>
+          </div>
         )}
-
-        <div className='flex flex-col gap-1 justify-center items-center'>
-          {props.freight?.commits
-            ?.slice(0, 2)
-            .map((commit) => <FreightArtifact key={commit?.repoURL} artifact={commit} />)}
-
-          {props.freight?.charts
-            ?.slice(0, 2)
-            .map((chart) => <FreightArtifact key={chart?.repoURL} artifact={chart} />)}
-
-          {props.freight?.images
-            ?.slice(0, 2)
-            .map((image) => <FreightArtifact key={image?.repoURL} artifact={image} />)}
-
-          {noOfGitCommits + noOfHelmReleases + noOfContainerImages > 6 && (
-            <Typography.Text type='secondary' className='text-[10px]'>
-              +
-              {noOfGitCommits +
-                noOfHelmReleases +
-                noOfContainerImages -
-                (props.freight?.charts?.slice(0, 2)?.length +
-                  props.freight?.charts?.slice(0, 2)?.length +
-                  props.freight?.images?.slice(0, 2)?.length)}{' '}
-              more
-            </Typography.Text>
-          )}
-        </div>
-
-        <div className='flex flex-col mx-auto w-full gap-0.5 items-center justify-center text-nowrap my-1'>
-          {(noOfGitCommits ? 1 : 0) + (noOfHelmReleases ? 1 : 0) + (noOfContainerImages ? 1 : 0) >
-            2 && (
-            <>
-              <FreightCard.ArtifactCount icon={faGithub} count={noOfGitCommits} />
-
-              <FreightCard.ArtifactCount icon={faAnchor} count={noOfHelmReleases} />
-
-              <FreightCard.ArtifactCount icon={faDocker} count={noOfContainerImages} />
-            </>
-          )}
-          <Typography.Text
-            className='text-xs text-nowrap'
-            type='secondary'
-            title={creation.abs?.toString()}
-          >
-            {creation.relative}
-          </Typography.Text>
-        </div>
-      </div>
+      </>
     );
   }
 
@@ -256,36 +288,6 @@ export const FreightCard = (props: FreightCardProps) => {
       }}
     >
       {CardContent}
-
-      {soakTimeFormatted && !props.promote && (
-        <div className='px-1 pb-1'>
-          <Button
-            disabled
-            onClick={(e) => e.stopPropagation()}
-            size='small'
-            className='text-[10px] text-center w-[230px] flex font-semibold'
-          >
-            <span className='mr-auto'>Soak: {soakTimeFormatted}</span>
-            <Progress strokeWidth={12} type='circle' size={14} percent={soakTimePercentage} />
-          </Button>
-        </div>
-      )}
-
-      {props.promote && (
-        <div className='px-1 pb-1'>
-          <Button
-            className='w-full'
-            type='primary'
-            size='small'
-            onClick={(e) => {
-              e.stopPropagation();
-              props.onReviewAndPromote?.();
-            }}
-          >
-            Select
-          </Button>
-        </div>
-      )}
     </div>
   );
 };
