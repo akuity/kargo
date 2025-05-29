9db17bbc82bfb37a735f799724dc8e968b1fc2ae
fuskovic <fhuskovic92@gmail.com>
2025-05-29 11:04:48 -0400

    s/provider/kind/g
    Signed-off-by: fuskovic <fhuskovic92@gmail.com>

--- internal/webhook/external/path.go
+++ internal/webhook/external/path.go
@@ -8,11 +8,11 @@ import (
 
 // GenerateWebhookPath generates a unique path for a webhook based on the
 // provided webhook receiver name, project name, kind, and token.
-// The path is formatted as "/webhook/{provider}/{hash}" where the hash is
+// The path is formatted as "/webhook/{kind}/{hash}" where the hash is
 // a SHA-256 hash of the concatenated webhook receiver name, project name, and token.
-func GenerateWebhookPath(name, project, provider, token string) string {
+func GenerateWebhookPath(name, project, kind, token string) string {
 	input := []byte(name + project + token)
 	h := sha256.New()
 	h.Write(input)
-	return fmt.Sprintf("/webhook/%s/%x", strings.ToLower(provider), h.Sum(nil))
+	return fmt.Sprintf("/webhook/%s/%x", strings.ToLower(kind), h.Sum(nil))
 }
