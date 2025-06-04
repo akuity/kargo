10e0271db3299e5d31cb6f18fa74aa0c1b57fa60
nitishfy <justnitish06@gmail.com>
2025-06-04 14:14:51 +0530

    feat: add ghcr webhook support
    Signed-off-by: nitishfy <justnitish06@gmail.com>

--- internal/webhook/external/github.go
+++ internal/webhook/external/github.go
@@ -59,7 +59,7 @@ func githubHandler(
 		// different actions (e.g. refresh Promotion on PR merge)
 		eventType := r.Header.Get("X-GitHub-Event")
 		switch eventType {
-		case "ping", "push":
+		case "ping", "push", "package":
 		default:
 			xhttp.WriteErrorJSON(
 				w,
@@ -134,6 +134,49 @@ func githubHandler(
 			logger = logger.WithValues("repoWebURL", repoWebURL)
 			ctx = logging.ContextWithLogger(ctx, logger)
 			result, err := refreshWarehouses(ctx, c, namespace, repoWebURL)
+			if err != nil {
+				xhttp.WriteErrorJSON(w,
+					xhttp.Error(err, http.StatusInternalServerError),
+				)
+				return
+			}
+
+			logger.Debug("execution complete",
+				"successes", result.successes,
+				"failures", result.failures,
+			)
+
+			if result.failures > 0 {
+				xhttp.WriteResponseJSON(w,
+					http.StatusInternalServerError,
+					map[string]string{
+						"error": fmt.Sprintf("failed to refresh %d of %d warehouses",
+							result.failures,
+							result.successes+result.failures,
+						),
+					},
+				)
+				return
+			}
+
+			xhttp.WriteResponseJSON(w,
+				http.StatusOK,
+				map[string]string{
+					"msg": fmt.Sprintf("refreshed %d warehouse(s)",
+						result.successes,
+					),
+				},
+			)
+		case *gh.PackageEvent:
+			name := e.GetPackage().Name
+			ns := e.GetPackage().Namespace
+			// construct the package URL that will be used by the warehouse
+			// based on name and namespace field from the package payload
+			// https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-container-registry#pushing-container-images
+			packageURL := fmt.Sprintf("ghcr.io/%v/%v", *ns, *name)
+			logger = logger.WithValues("packageURL", packageURL)
+			ctx = logging.ContextWithLogger(ctx, logger)
+			result, err := refreshWarehouses(ctx, c, namespace, packageURL)
 			if err != nil {
 				xhttp.WriteErrorJSON(w,
 					xhttp.Error(err, http.StatusInternalServerError),
--- internal/webhook/external/github_test.go
+++ internal/webhook/external/github_test.go
@@ -87,7 +87,7 @@ func TestGithubHandler(t *testing.T) {
 					Build()
 			},
 			req: func() *http.Request {
-				b := newBody()
+				b := newGitHubEventBody("push")
 				req := httptest.NewRequest(http.MethodPost, url, b)
 				req.Header.Set("Content-Type", "application/json")
 				req.Header.Set("X-Hub-Signature-256", sign(t, testSecret, b.Bytes()))
@@ -159,7 +159,7 @@ func TestGithubHandler(t *testing.T) {
 					Build()
 			},
 			req: func() *http.Request {
-				b := newBody()
+				b := newGitHubEventBody("push")
 				req := httptest.NewRequest(http.MethodPost, url, b)
 				req.Header.Set("Content-Type", "application/json")
 				req.Header.Set("X-Hub-Signature-256", sign(t, "fakesecret", b.Bytes()))
@@ -231,7 +231,7 @@ func TestGithubHandler(t *testing.T) {
 					Build()
 			},
 			req: func() *http.Request {
-				b := newBody()
+				b := newGitHubEventBody("deployment")
 				req := httptest.NewRequest(http.MethodPost, url, b)
 				req.Header.Set("Content-Type", "application/json")
 				req.Header.Set("X-Hub-Signature-256", sign(t, testSecret, b.Bytes()))
@@ -376,7 +376,7 @@ func TestGithubHandler(t *testing.T) {
 					Build()
 			},
 			req: func() *http.Request {
-				b := newBody()
+				b := newGitHubEventBody("push")
 				req := httptest.NewRequest(http.MethodPost, url, b)
 				req.Header.Set("X-GitHub-Event", "push")
 				return req
@@ -446,7 +446,7 @@ func TestGithubHandler(t *testing.T) {
 					Build()
 			},
 			req: func() *http.Request {
-				b := newBody()
+				b := newGitHubEventBody("push")
 				req := httptest.NewRequest(http.MethodPost, url, b)
 				req.Header.Set("X-Hub-Signature-256", sign(t, "invalid-sig", b.Bytes()))
 				req.Header.Set("X-GitHub-Event", "push")
@@ -604,7 +604,7 @@ func TestGithubHandler(t *testing.T) {
 					Build()
 			},
 			req: func() *http.Request {
-				b := newBody()
+				b := newGitHubEventBody("push")
 				req := httptest.NewRequest(http.MethodPost, url, b)
 				req.Header.Set("Content-Type", "application/json")
 				req.Header.Set("X-Hub-Signature-256", sign(t, "mysupersecrettoken", b.Bytes()))
@@ -637,7 +637,7 @@ func TestGithubHandler(t *testing.T) {
 					Build()
 			},
 			req: func() *http.Request {
-				b := newBody()
+				b := newGitHubEventBody("ping")
 				req := httptest.NewRequest(http.MethodPost, url, b)
 				req.Header.Set("Content-Type", "application/json")
 				req.Header.Set("X-Hub-Signature-256", sign(t, "mysupersecrettoken", b.Bytes()))
@@ -646,6 +646,93 @@ func TestGithubHandler(t *testing.T) {
 			},
 			secret: "fakesecret",
 			msg:    "{\"msg\":\"ping event received, webhook is configured correctly for https://github.com/username/repo\"}\n", // nolint: lll
+			code:   http.StatusOK,
+		},
+		{
+			name: "success - package event",
+			kClient: func() client.Client {
+				scheme := runtime.NewScheme()
+				require.NoError(t, corev1.AddToScheme(scheme))
+				require.NoError(t, kargoapi.AddToScheme(scheme))
+				return fake.NewClientBuilder().
+					WithScheme(scheme).
+					WithObjects(
+						&corev1.Secret{
+							ObjectMeta: metav1.ObjectMeta{
+								Name:      "fakesecret",
+								Namespace: "fakenamespace",
+							},
+							Data: map[string][]byte{
+								"token": []byte("mysupersecrettoken"),
+							},
+						},
+						&kargoapi.ProjectConfig{
+							ObjectMeta: metav1.ObjectMeta{
+								Namespace: "fakenamespace",
+								Name:      "fakename",
+							},
+							Spec: kargoapi.ProjectConfigSpec{
+								WebhookReceivers: []kargoapi.WebhookReceiverConfig{
+									{
+										GitHub: &kargoapi.GitHubWebhookReceiver{
+											SecretRef: corev1.LocalObjectReference{
+												Name: "fakesecret",
+											},
+										},
+									},
+								},
+							},
+							Status: kargoapi.ProjectConfigStatus{
+								WebhookReceivers: []kargoapi.WebhookReceiver{
+									{
+										Path: GenerateWebhookPath(
+											"fake-webhook-receiver-name",
+											"fakename",
+											kargoapi.WebhookReceiverTypeGitHub,
+											"mysupersecrettoken",
+										),
+									},
+								},
+							},
+						},
+						&kargoapi.Warehouse{
+							ObjectMeta: metav1.ObjectMeta{
+								Namespace: "fakenamespace",
+								Name:      "fakename",
+							},
+							Spec: kargoapi.WarehouseSpec{
+								Subscriptions: []kargoapi.RepoSubscription{
+									{
+										Git: &kargoapi.GitSubscription{
+											RepoURL: "ghcr.io/username/repo",
+										},
+									},
+								},
+							},
+						},
+					).
+					WithIndex(
+						&kargoapi.Warehouse{},
+						indexer.WarehousesBySubscribedURLsField,
+						indexer.WarehousesBySubscribedURLs,
+					).
+					WithIndex(
+						&kargoapi.ProjectConfig{},
+						indexer.ProjectConfigsByWebhookReceiverPathsField,
+						indexer.ProjectConfigsByWebhookReceiverPaths,
+					).
+					Build()
+			},
+			req: func() *http.Request {
+				b := newGitHubEventBody("package")
+				req := httptest.NewRequest(http.MethodPost, url, b)
+				req.Header.Set("Content-Type", "application/json")
+				req.Header.Set("X-Hub-Signature-256", sign(t, "mysupersecrettoken", b.Bytes()))
+				req.Header.Set("X-GitHub-Event", "package")
+				return req
+			},
+			secret: "fakesecret",
+			msg:    "{\"msg\":\"refreshed 1 warehouse(s)\"}\n",
 			code:   http.StatusOK,
 		},
 	} {
@@ -674,22 +761,100 @@ func sign(t *testing.T, s string, b []byte) string {
 	)
 }
 
-func newBody() *bytes.Buffer {
-	return bytes.NewBuffer([]byte(`
+func newGitHubEventBody(eventType string) *bytes.Buffer {
+	switch eventType {
+	case "push", "ping", "deployment":
+		return bytes.NewBuffer([]byte(`
 {
 	"ref": "refs/heads/main",
 	"before": "1fe030abc48d0d0ee7b3d650d6e9449775990318",
 	"after": "f12cd167152d80c0a2e28cb45e827c6311bba910",
 	"repository": {
-	  "html_url": "https://github.com/username/repo"
+		"html_url": "https://github.com/username/repo"
 	},
 	"pusher": {
-	  "name": "username",
-	  "email": "email@inbox.com"
+		"name": "username",
+		"email": "email@inbox.com"
 	},
 	"head_commit": {
-	  "id": "f12cd167152d80c0a2e28cb45e827c6311bba910"
+		"id": "f12cd167152d80c0a2e28cb45e827c6311bba910"
 	}
-  }	
+}
 `))
+	case "package":
+		return bytes.NewBuffer([]byte(`
+{
+  "action": "published",
+  "package": {
+    "id": 7656404,
+    "name": "repo",
+    "namespace": "username",
+    "description": "",
+    "ecosystem": "CONTAINER",
+    "package_type": "CONTAINER",
+    "html_url": "https://github.com/username/packages/7656404",
+    "created_at": "2025-02-10T10:47:09Z",
+    "updated_at": "2025-06-04T07:37:38Z",
+    "owner": {
+      "login": "username",
+      "id": 86051118,
+      "node_id": "MDQ6VXNlcjg2MDUxMTE4",
+      "avatar_url": "https://avatars.githubusercontent.com/u/86051118?v=4",
+      "gravatar_id": "",
+      "url": "https://api.github.com/users/username",
+      "html_url": "https://github.com/username",
+      "followers_url": "https://api.github.com/users/username/followers",
+      "following_url": "https://api.github.com/users/username/following{/other_user}",
+      "gists_url": "https://api.github.com/users/username/gists{/gist_id}",
+      "starred_url": "https://api.github.com/users/username/starred{/owner}{/repo}",
+      "subscriptions_url": "https://api.github.com/users/username/subscriptions",
+      "organizations_url": "https://api.github.com/users/username/orgs",
+      "repos_url": "https://api.github.com/users/username/repos",
+      "events_url": "https://api.github.com/users/username/events{/privacy}",
+      "received_events_url": "https://api.github.com/users/username/received_events",
+      "type": "User",
+      "user_view_type": "public",
+      "site_admin": false
+    }
+  },
+  "package_version": {
+    "id": 430232,
+    "version": "sha256:ceebe538c5f4c3b32dba99b9c3b1d7401d6815db8d7240e8854c87b570a991e9",
+    "name": "sha256:ceebe538c5f4c3b32dba99b9c3b1d7401d6815db8d7240e8854c87b570a991e9",
+    "package_url": "ghcr.io/username/repo:v0.1.6",
+    "body": {
+      "repository": {
+        "repository": {
+          "name": "repo",
+          "owner_login": "username"
+        }
+      }
+    }
+  },
+  "sender": {
+    "login": "username",
+    "id": 86051118,
+    "node_id": "MDQ6VXNlcjg2MDUxMTE4",
+    "avatar_url": "https://avatars.githubusercontent.com/u/86051118?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/username",
+    "html_url": "https://github.com/username",
+    "followers_url": "https://api.github.com/users/username/followers",
+    "following_url": "https://api.github.com/users/username/following{/other_user}",
+    "gists_url": "https://api.github.com/users/username/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/username/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/username/subscriptions",
+    "organizations_url": "https://api.github.com/users/username/orgs",
+    "repos_url": "https://api.github.com/users/username/repos",
+    "events_url": "https://api.github.com/users/username/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/username/received_events",
+    "type": "User",
+    "user_view_type": "public",
+    "site_admin": false
+  }
+}
+`))
+	default:
+		return bytes.NewBuffer([]byte(`{}`))
+	}
 }
