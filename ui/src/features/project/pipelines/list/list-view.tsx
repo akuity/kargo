5426c8f33cf593b64747ac170150c9081dcb4561
Mayursinh Sarvaiya <marvinduff97@gmail.com>
2025-06-24 17:50:20 +0530

    feat(ui): stage list view (#4371)
    Signed-off-by: Mayursinh Sarvaiya <marvinduff97@gmail.com>
    Signed-off-by: Rafal Pelczar <rafal@akuity.io>
    Co-authored-by: Jakub Maliszewski <jakub.maliszewski@akuity.io>
    Co-authored-by: Rafal Pelczar <rafal@akuity.io>
    (cherry picked from commit c6ac159405def7043bad1ff8b684092f708c3a53)

---
+++ ui/src/features/project/pipelines/list/list-view.tsx
@@ -0,0 +1,80 @@
+import { Card, Table } from 'antd';
+import classNames from 'classnames';
+import { useMemo, useState } from 'react';
+
+import { IAction, useActionContext } from '@ui/features/project/pipelines/context/action-context';
+import { useEventsWatcher } from '@ui/features/project/pipelines/graph/use-events-watcher';
+import { Freight, Stage, Warehouse } from '@ui/gen/api/v1alpha1/generated_pb';
+
+import { useFreightTimelineControllerContext } from '../context/freight-timeline-controller-context';
+
+import { AppliedFilters } from './applied-filters';
+import { actionColumn } from './columns/action-column';
+import { healthColumn } from './columns/health-column';
+import { lastPromotionColumn } from './columns/last-promotion-column';
+import { phaseColumn } from './columns/phase-column';
+import { stageColumn } from './columns/stage-column';
+import { versionColumn } from './columns/version-column';
+import { Filter, FilterContext } from './context/filter-context';
+
+type PipelineListViewProps = {
+  stages: Stage[];
+  warehouses: Warehouse[];
+  freights: Freight[];
+  className?: string;
+  project: string;
+};
+
+export const PipelineListView = (props: PipelineListViewProps) => {
+  const actionContext = useActionContext();
+  const freightTimelineControllerContext = useFreightTimelineControllerContext();
+
+  useEventsWatcher(props.project);
+
+  const [filters, setFilters] = useState<Filter>({
+    stage: ''
+  });
+
+  const filteredStages = useMemo(() => {
+    const filterWarehouses = freightTimelineControllerContext?.preferredFilter?.warehouses || [];
+
+    if (!freightTimelineControllerContext?.preferredFilter?.warehouses?.length) {
+      return props.stages;
+    }
+
+    return props.stages.filter((stage) => {
+      return stage.spec?.requestedFreight?.find((f) =>
+        filterWarehouses.includes(f?.origin?.name || '')
+      );
+    });
+  }, [freightTimelineControllerContext?.preferredFilter?.warehouses, props.stages]);
+
+  return (
+    <FilterContext.Provider
+      value={{
+        filters,
+        onFilter: setFilters
+      }}
+    >
+      <Card className={classNames(props.className, 'm-2')} size='small'>
+        <AppliedFilters className='px-2 pb-4' />
+        <Table
+          pagination={{ hideOnSinglePage: true }}
+          dataSource={filteredStages}
+          rowKey={(stage) => `${stage?.metadata?.name}-${stage?.status?.observedGeneration}`}
+          size='small'
+          columns={[
+            stageColumn(filters),
+            phaseColumn(),
+            healthColumn(),
+            versionColumn(),
+            lastPromotionColumn(),
+            actionColumn({
+              onPromote: (stage) => actionContext?.actPromote(IAction.PROMOTE, stage)
+            })
+          ]}
+        />
+      </Card>
+    </FilterContext.Provider>
+  );
+};
