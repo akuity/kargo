1833e91ab57c32c3dee5781d0cd04fb899982b9c
Hidde Beydals <hidde@hhh.computer>
2025-06-27 14:19:33 +0200

    feat: set AutoPromotionEnabled in StageStatus
    Signed-off-by: Hidde Beydals <hidde@hhh.computer>

--- internal/controller/stages/regular_stages.go
+++ internal/controller/stages/regular_stages.go
@@ -1630,8 +1630,10 @@ func (r *RegularStageReconciler) autoPromoteFreight(
 
 	// Confirm that auto-promotion is allowed for the Stage.
 	if autoPromotionAllowed, err := r.autoPromotionAllowed(ctx, stage.ObjectMeta); err != nil || !autoPromotionAllowed {
+		newStatus.AutoPromotionEnabled = false
 		return newStatus, err
 	}
+	newStatus.AutoPromotionEnabled = true
 
 	// Retrieve promotable Freight for the Stage.
 	promotableFreight, err := r.getPromotableFreight(ctx, stage)
