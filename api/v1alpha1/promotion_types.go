e8650a050aa8d6987ce802b42dcb8e3d0bd29b5b
Hidde Beydals <hidde@hhh.computer>
2025-06-30 13:21:13 +0200

    chore: run codegen
    Signed-off-by: Hidde Beydals <hidde@hhh.computer>

--- api/v1alpha1/promotion_types.go
+++ api/v1alpha1/promotion_types.go
@@ -322,7 +322,7 @@ type PromotionStatus struct {
 	// the Promotion has completed.
 	HealthChecks []HealthCheckStep `json:"healthChecks,omitempty" protobuf:"bytes,8,rep,name=healthChecks"`
 	// StartedAt is the time when the promotion started.
-	StartedAt *metav1.Time `json:"startedAt,omitempty"`
+	StartedAt *metav1.Time `json:"startedAt,omitempty" protobuf:"bytes,12,opt,name=startedAt"`
 	// FinishedAt is the time when the promotion was completed.
 	FinishedAt *metav1.Time `json:"finishedAt,omitempty" protobuf:"bytes,6,opt,name=finishedAt"`
 	// CurrentStep is the index of the current promotion step being executed. This
