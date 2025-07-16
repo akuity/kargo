6aaf4929254132ed3d2f81af1096788959a47423
nitishfy <justnitish06@gmail.com>
2025-07-16 16:00:25 +0530

    refactor again
    Signed-off-by: nitishfy <justnitish06@gmail.com>

--- internal/controller/warehouses/images.go
+++ internal/controller/warehouses/images.go
@@ -129,16 +129,12 @@ func imageDiscoveryLogFields(sub kargoapi.ImageSubscription) []any {
 		"platformConstrained", sub.Platform != "",
 	}
 	switch sub.ImageSelectionStrategy {
-	case kargoapi.ImageSelectionStrategySemVer:
-		if sub.Constraint != "" {
-			f = append(f, "constraint", sub.Constraint)
-		} else {
-			f = append(f, "semverConstraint", sub.SemverConstraint) //nolint:staticcheck
+	case kargoapi.ImageSelectionStrategySemVer, kargoapi.ImageSelectionStrategyDigest:
+		constraint := sub.Constraint
+		if constraint == "" {
+			constraint = sub.SemverConstraint // nolint: staticcheck
 		}
-	case kargoapi.ImageSelectionStrategyDigest:
-		if sub.Constraint != "" {
-			f = append(f, "constraint", sub.Constraint)
-		}
+		f = append(f, "constraint", sub.Constraint)
 	case kargoapi.ImageSelectionStrategyLexical, kargoapi.ImageSelectionStrategyNewestBuild:
 		f = append(f, "tagConstrained", sub.AllowTags != "" || len(sub.IgnoreTags) > 0)
 	}
