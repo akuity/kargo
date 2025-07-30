18be92a08fda9b6f97b0ac307d074b945f66ab67
Mayursinh Sarvaiya <marvinduff97@gmail.com>
2025-07-30 17:38:11 +0530

    chore: equal size of label
    Signed-off-by: Mayursinh Sarvaiya <marvinduff97@gmail.com>

--- ui/src/features/project/pipelines/warehouse/repo-subscriptions.tsx
+++ ui/src/features/project/pipelines/warehouse/repo-subscriptions.tsx
@@ -8,6 +8,10 @@ type Props = {
   projectName?: string;
 };
 
+const DescriptionsLabelStyle: React.CSSProperties = {
+  width: '40%'
+};
+
 export const RepoSubscriptions = ({ subscriptions }: Props) => {
   if (!subscriptions) {
     return null;
@@ -25,7 +29,7 @@ export const RepoSubscriptions = ({ subscriptions }: Props) => {
               column={1}
               style={{ width: '40%', minWidth: 500 }}
             >
-              <Descriptions.Item label='repo URL'>
+              <Descriptions.Item label='repo URL' styles={{ label: DescriptionsLabelStyle }}>
                 <Typography.Link
                   href={`${subscription.chart?.repoURL}/${subscription?.chart?.name}`}
                   target='_blank'
@@ -36,13 +40,18 @@ export const RepoSubscriptions = ({ subscriptions }: Props) => {
               </Descriptions.Item>
 
               {!!subscription?.chart?.discoveryLimit && (
-                <Descriptions.Item label='discovery limit'>
+                <Descriptions.Item
+                  label='discovery limit'
+                  styles={{ label: DescriptionsLabelStyle }}
+                >
                   {subscription?.chart?.discoveryLimit}
                 </Descriptions.Item>
               )}
 
               {subscription?.chart?.name && (
-                <Descriptions.Item label='name'>{subscription?.chart?.name}</Descriptions.Item>
+                <Descriptions.Item label='name' styles={{ label: DescriptionsLabelStyle }}>
+                  {subscription?.chart?.name}
+                </Descriptions.Item>
               )}
             </Descriptions>
           )}
@@ -55,30 +64,38 @@ export const RepoSubscriptions = ({ subscriptions }: Props) => {
               column={1}
               style={{ width: '40%', minWidth: 500 }}
             >
-              <Descriptions.Item label='repo URL'>
+              <Descriptions.Item label='repo URL' styles={{ label: DescriptionsLabelStyle }}>
                 <Typography.Link href={subscription.git?.repoURL} target='_blank' rel='noreferrer'>
                   {subscription.git?.repoURL}
                 </Typography.Link>
               </Descriptions.Item>
 
               {!!subscription?.git?.discoveryLimit && (
-                <Descriptions.Item label='discovery limit'>
+                <Descriptions.Item
+                  label='discovery limit'
+                  styles={{ label: DescriptionsLabelStyle }}
+                >
                   {subscription?.git?.discoveryLimit}
                 </Descriptions.Item>
               )}
 
               {subscription?.git?.branch && (
-                <Descriptions.Item label='branch'>{subscription?.git?.branch}</Descriptions.Item>
+                <Descriptions.Item label='branch' styles={{ label: DescriptionsLabelStyle }}>
+                  {subscription?.git?.branch}
+                </Descriptions.Item>
               )}
 
               {!!subscription?.git?.semverConstraint && (
-                <Descriptions.Item label='constraint'>
+                <Descriptions.Item label='constraint' styles={{ label: DescriptionsLabelStyle }}>
                   {subscription?.git?.semverConstraint}
                 </Descriptions.Item>
               )}
 
               {subscription?.git?.commitSelectionStrategy && (
-                <Descriptions.Item label='commit selection strategy'>
+                <Descriptions.Item
+                  label='commit selection strategy'
+                  styles={{ label: DescriptionsLabelStyle }}
+                >
                   {subscription?.git?.commitSelectionStrategy}
                 </Descriptions.Item>
               )}
@@ -93,7 +110,7 @@ export const RepoSubscriptions = ({ subscriptions }: Props) => {
               column={1}
               style={{ width: '40%', minWidth: 500 }}
             >
-              <Descriptions.Item label='repo URL'>
+              <Descriptions.Item label='repo URL' styles={{ label: DescriptionsLabelStyle }}>
                 <Typography.Link
                   href={urlForImage(subscription.image?.repoURL)}
                   target='_blank'
@@ -104,19 +121,25 @@ export const RepoSubscriptions = ({ subscriptions }: Props) => {
               </Descriptions.Item>
 
               {!!subscription?.image?.discoveryLimit && (
-                <Descriptions.Item label='discovery limit'>
+                <Descriptions.Item
+                  label='discovery limit'
+                  styles={{ label: DescriptionsLabelStyle }}
+                >
                   {subscription?.image?.discoveryLimit}
                 </Descriptions.Item>
               )}
 
               {(!!subscription?.image?.constraint || !!subscription?.image?.semverConstraint) && (
-                <Descriptions.Item label='constraint'>
+                <Descriptions.Item label='constraint' styles={{ label: DescriptionsLabelStyle }}>
                   {subscription?.image?.constraint || subscription?.image?.semverConstraint}
                 </Descriptions.Item>
               )}
 
               {subscription?.image?.imageSelectionStrategy && (
-                <Descriptions.Item label='image selection strategy'>
+                <Descriptions.Item
+                  label='image selection strategy'
+                  styles={{ label: DescriptionsLabelStyle }}
+                >
                   {subscription?.image?.imageSelectionStrategy}
                 </Descriptions.Item>
               )}
