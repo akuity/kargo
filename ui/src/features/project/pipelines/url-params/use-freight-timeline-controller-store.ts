3be601c9a36ffa87618fb7fbf82e80415389d31b
Mayursinh Sarvaiya <marvinduff97@gmail.com>
2025-07-30 18:59:14 +0530

    feat(ui): pipeline filters in URL query (#4716)
    Signed-off-by: Mayursinh Sarvaiya <marvinduff97@gmail.com>

---
+++ ui/src/features/project/pipelines/url-params/use-freight-timeline-controller-store.ts
@@ -0,0 +1,125 @@
+import { useCallback, useMemo } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
+import { FreightTimelineControllerContextType } from '@ui/features/project/pipelines/context/freight-timeline-controller-context';
+import { timerangeTypes } from '@ui/features/project/pipelines/freight/filter-timerange-utils';
+
+export const useFreightTimelineControllerStore = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const filters = useMemo(() => {
+    const filters: FreightTimelineControllerContextType['preferredFilter'] = {
+      showAlias: true,
+      sources: [],
+      timerange: 'all-time',
+      showColors: true,
+      warehouses: [],
+      hideUnusedFreights: false,
+      stackedNodesParents: [],
+      hideSubscriptions: {},
+      images: false
+    };
+
+    const showAliasParam = searchParams.get('showAlias');
+    if (showAliasParam && showAliasParam !== '') {
+      filters.showAlias = showAliasParam === 'true';
+    }
+
+    const sourcesParam = searchParams.getAll('sources');
+    if (sourcesParam && sourcesParam.length > 0) {
+      filters.sources = sourcesParam;
+    }
+
+    const timerangeParam = searchParams.get('timerange');
+    if (timerangeParam && timerangeParam !== '') {
+      filters.timerange = timerangeParam as timerangeTypes;
+    }
+
+    const showColorsParam = searchParams.get('showColors');
+    if (showColorsParam && showColorsParam !== '') {
+      filters.showColors = showColorsParam === 'true';
+    }
+
+    const warehousesParam = searchParams.getAll('warehouses');
+    if (warehousesParam && warehousesParam.length > 0) {
+      filters.warehouses = warehousesParam;
+    }
+
+    const hideUnusedFreightsParam = searchParams.get('hideUnusedFreights');
+    if (hideUnusedFreightsParam && hideUnusedFreightsParam !== '') {
+      filters.hideUnusedFreights = hideUnusedFreightsParam === 'true';
+    }
+
+    const stackedNodesParentsParam = searchParams.getAll('stackedNodesParents');
+    if (stackedNodesParentsParam && stackedNodesParentsParam.length > 0) {
+      filters.stackedNodesParents = stackedNodesParentsParam;
+    }
+
+    const imagesParam = searchParams.get('images');
+    if (imagesParam && imagesParam !== '') {
+      filters.images = imagesParam === 'true';
+    }
+
+    const hideSubscriptionsParam = searchParams.getAll('hideSubscriptions');
+    if (hideSubscriptionsParam && hideSubscriptionsParam.length > 0) {
+      for (const hideSubscriptionOf of hideSubscriptionsParam) {
+        filters.hideSubscriptions[hideSubscriptionOf] = true;
+      }
+    }
+
+    return filters;
+  }, [searchParams]);
+
+  return [
+    filters,
+    useCallback(
+      (nextPartial: Partial<FreightTimelineControllerContextType['preferredFilter']>) => {
+        const currentSearchParams = new URLSearchParams(searchParams);
+
+        currentSearchParams.set('showColors', `${nextPartial.showColors}`);
+
+        currentSearchParams.set('showAlias', `${nextPartial.showAlias}`);
+
+        currentSearchParams.delete('sources');
+        if (nextPartial.sources && nextPartial.sources.length > 0) {
+          for (const source of nextPartial.sources) {
+            currentSearchParams.append('sources', source);
+          }
+        }
+
+        currentSearchParams.set('timerange', `${nextPartial.timerange}`);
+
+        currentSearchParams.delete('warehouses');
+        if (nextPartial.warehouses && nextPartial.warehouses.length > 0) {
+          for (const warehouse of nextPartial.warehouses) {
+            currentSearchParams.append('warehouses', warehouse);
+          }
+        }
+
+        currentSearchParams.set('hideUnusedFreights', `${nextPartial.hideUnusedFreights}`);
+
+        currentSearchParams.delete('stackedNodesParents');
+        if (nextPartial.stackedNodesParents && nextPartial.stackedNodesParents.length > 0) {
+          for (const stackedNodesParent of nextPartial.stackedNodesParents) {
+            currentSearchParams.append('stackedNodesParents', stackedNodesParent);
+          }
+        }
+
+        currentSearchParams.set('images', `${nextPartial.images}`);
+
+        const hideSubscriptionEntries = Object.entries(nextPartial.hideSubscriptions || {});
+        currentSearchParams.delete('hideSubscriptions');
+        if (hideSubscriptionEntries.length > 0) {
+          for (const [name, hideSubscription] of hideSubscriptionEntries) {
+            if (hideSubscription) {
+              currentSearchParams.append('hideSubscriptions', name);
+            }
+          }
+        }
+
+        setSearchParams(currentSearchParams);
+      },
+      [searchParams, setSearchParams]
+    )
+  ] as const;
+};
