a6253fe733f4d813249f48df2fa2e36ef77d4ad3
Kent Rancourt <kent.rancourt@gmail.com>
2025-08-01 17:44:52 -0400

    small improvement to refresh logic -- sort and de-dedupe qualifiers (git refs or image/chart tags)
    Signed-off-by: Kent Rancourt <kent.rancourt@gmail.com>

--- internal/webhook/external/refresh.go
+++ internal/webhook/external/refresh.go
@@ -44,6 +44,15 @@ func refreshWarehouses(
 		repoURLs = repoURLs[1:]
 	}
 
+	// De-dupe qualifiers
+	slices.Sort(qualifiers)
+	qualifiers = slices.Compact(qualifiers)
+	// If there had been any empty strings in the slice, after sorting and
+	// compacting, at most the zero element will be empty. If it is, remove it.
+	if qualifiers[0] == "" {
+		qualifiers = qualifiers[1:]
+	}
+
 	// The distinct set of all Warehouses that should be refreshed
 	toRefresh := map[client.ObjectKey]*kargoapi.Warehouse{}
 
