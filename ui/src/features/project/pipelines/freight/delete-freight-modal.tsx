c0675932d9ae2fb0e2289774edd83362446dec7e
bhavyabhut <pbhavy4@gmail.com>
2025-08-07 10:16:16 +0530

    chore: remove invalid query
    Signed-off-by: bhavyabhut <pbhavy4@gmail.com>

--- ui/src/features/project/pipelines/freight/delete-freight-modal.tsx
+++ ui/src/features/project/pipelines/freight/delete-freight-modal.tsx
@@ -1,15 +1,10 @@
-import { useMutation, createConnectQueryKey } from '@connectrpc/connect-query';
-import { useQueryClient } from '@tanstack/react-query';
+import { useMutation } from '@connectrpc/connect-query';
 import { Alert, Modal, message } from 'antd';
 import { useParams } from 'react-router-dom';
 
-import { transportWithAuth } from '@ui/config/transport';
 import { ModalComponentProps } from '@ui/features/common/modal/modal-context';
 import { getAlias } from '@ui/features/common/utils';
-import {
-  deleteFreight,
-  queryFreight
-} from '@ui/gen/api/service/v1alpha1/service-KargoService_connectquery';
+import { deleteFreight } from '@ui/gen/api/service/v1alpha1/service-KargoService_connectquery';
 import { Freight } from '@ui/gen/api/v1alpha1/generated_pb';
 
 export const DeleteFreightModal = ({
@@ -19,22 +14,10 @@ export const DeleteFreightModal = ({
   freight
 }: ModalComponentProps & { onDelete: () => void; freight: Freight }) => {
   const { name: project } = useParams();
-  const queryClient = useQueryClient();
 
   const { mutate: deleteAction, isPending } = useMutation(deleteFreight, {
     onSuccess: () => {
       message.success('Freight successfully deleted');
-
-      queryClient.invalidateQueries({
-        queryKey: createConnectQueryKey({
-          cardinality: 'finite',
-          schema: queryFreight,
-          input: {
-            project
-          },
-          transport: transportWithAuth
-        })
-      });
       onDelete();
     }
   });
