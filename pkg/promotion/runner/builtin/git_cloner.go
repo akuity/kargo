e2a4cbe275ecf19ffa40be4ea5d9414336740412
Kyounghoon Jang <matkimchi_@naver.com>
2025-09-02 14:14:50 +0000

    feat(git-clone): add support for submodules
    Signed-off-by: Kyounghoon Jang <matkimchi_@naver.com>

--- pkg/promotion/runner/builtin/git_cloner.go
+++ pkg/promotion/runner/builtin/git_cloner.go
@@ -186,12 +186,13 @@ func (g *gitCloner) run(
 					"error adding work tree %s to repo %s: %w",
 					checkout.Path, cfg.RepoURL, err,
 				)
-		} 
-		if cfg.RecurseSubmodules{
-			if err := worktree.UpdateSubmodules(); err != nil { 
-                return promotion.StepResult{Status: kargoapi.PromotionStepStatusErrored},
-                    fmt.Errorf("error updating submodules for worktree at %s: %w", path, err)
-            }
+		}
+		if cfg.RecurseSubmodules {
+			err = worktree.UpdateSubmodules()
+			if err != nil {
+				return promotion.StepResult{Status: kargoapi.PromotionStepStatusErrored},
+					fmt.Errorf("error updating submodules for worktree at %s: %w", path, err)
+			}
 		}
 		key := checkout.Path
 		if checkout.As != "" {
