716c4eeacf4405c1a9d48272168d3202300beaa6
akuitybot <105087302+akuitybot@users.noreply.github.com>
2025-09-09 18:05:38 -0700

    chore(backport release-1.7): fix(step)!: tighten validation `desiredRevision` (#5013)
    Signed-off-by: Hidde Beydals <hidde@hhh.computer>
    Signed-off-by: Kent Rancourt <kent.rancourt@gmail.com>
    Co-authored-by: Hidde Beydals <hiddeco@users.noreply.github.com>
    Co-authored-by: Kent Rancourt <kent.rancourt@gmail.com>

--- internal/promotion/runner/builtin/argocd_updater_test.go
+++ internal/promotion/runner/builtin/argocd_updater_test.go
@@ -285,7 +285,7 @@ func Test_argoCDUpdater_convert(t *testing.T) {
 					"namespace": "argocd",
 					"sources": []promotion.Config{{
 						"repoURL":              "fake-git-url",
-						"desiredRevision":      "fake-commit",
+						"desiredRevision":      "0532b614f4b84aa2af74a0c7d62e3d9d8ba446b5",
 						"updateTargetRevision": true,
 						"helm": promotion.Config{
 							"images": []promotion.Config{
