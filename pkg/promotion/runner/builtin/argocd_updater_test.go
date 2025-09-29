6ea156804f67284e20888e944a53d4b706ebff6c
nitishfy <justnitish06@gmail.com>
2025-09-29 22:19:35 +0530

    fix lint
    Signed-off-by: nitishfy <justnitish06@gmail.com>

--- pkg/promotion/runner/builtin/argocd_updater_test.go
+++ pkg/promotion/runner/builtin/argocd_updater_test.go
@@ -1373,7 +1373,7 @@ func Test_argoCDUpdater_getAuthorizedApplication(t *testing.T) {
 					client.WithWatch,
 					client.ObjectKey,
 					client.Object,
-				...client.GetOption,
+					...client.GetOption,
 				) error {
 					return errors.New("something went wrong")
 				},
