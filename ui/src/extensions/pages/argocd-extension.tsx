e0ff9bd40c95834c757ddc012d9a62997422ac35
Rafal Pelczar <rafal@akuity.io>
2025-10-29 22:14:48 +0100

    feat(ui): move ArgoCD link into header; remove link from details; handle ext
    Signed-off-by: Rafal Pelczar <rafal@akuity.io>

--- ui/src/extensions/pages/argocd-extension.tsx
+++ ui/src/extensions/pages/argocd-extension.tsx
@@ -1,18 +1,20 @@
 import { Breadcrumb } from 'antd';
-import React from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 
+import { LoadingState } from '@ui/features/common';
 import { BaseHeader } from '@ui/features/common/layout/base-header';
 import { useProjectBreadcrumbs } from '@ui/features/project/project-utils';
 
 import { useExtensionsContext } from '../extensions-context';
+import { useIsAnyExtensionLoaded } from '../utils';
 
 export const ArgoCDExtension = () => {
   const { appName } = useParams();
   const projectBreadcrumbs = useProjectBreadcrumbs();
   const { argoCDExtension } = useExtensionsContext();
+  const isAnyExenstionLoaded = useIsAnyExtensionLoaded();
 
-  if (!argoCDExtension) {
+  if (!isAnyExenstionLoaded) {
     return <Navigate to='/' replace />;
   }
 
@@ -23,9 +25,9 @@ export const ArgoCDExtension = () => {
           separator='>'
           items={[...projectBreadcrumbs, { title: 'ArgoCD' }, { title: appName }]}
         />
-        {argoCDExtension.tabBarExtraContent && <argoCDExtension.tabBarExtraContent />}
+        {argoCDExtension?.tabBarExtraContent && <argoCDExtension.tabBarExtraContent />}
       </BaseHeader>
-      <argoCDExtension.component />
+      {argoCDExtension ? <argoCDExtension.component /> : <LoadingState />}
     </>
   );
 };
