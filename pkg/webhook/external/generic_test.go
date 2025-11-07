d3056cb426d03f1b1c0dc788f17769853bdd7e6c
fuskovic <fhuskovic92@gmail.com>
2025-11-07 20:29:15 +0530

    remove redundant test

--- pkg/webhook/external/generic_test.go
+++ pkg/webhook/external/generic_test.go
@@ -18,7 +18,6 @@ import (
 
 	kargoapi "github.com/akuity/kargo/api/v1alpha1"
 	"github.com/akuity/kargo/pkg/indexer"
-	"github.com/akuity/kargo/pkg/urls"
 )
 
 func TestGenericHandler(t *testing.T) {
@@ -435,75 +434,6 @@ func TestGenericHandler(t *testing.T) {
 				config: tc.config,
 			}).getHandler(requestBody).ServeHTTP(w, tc.req())
 			tc.assertions(t, w)
-		})
-	}
-}
-
-func Test_newListOptionsForIndexSelector(t *testing.T) {
-	// This is indirectly tested via Test_buildListOptionsForTarget,
-	// so we just need a basic smoke test here.
-	testRepoURL := urls.NormalizeGit("https://github.com/example/repo.git")
-	testScheme := runtime.NewScheme()
-	require.NoError(t, kargoapi.AddToScheme(testScheme))
-
-	tests := []struct {
-		name      string
-		kClient   client.Client
-		selector  kargoapi.IndexSelector
-		env       map[string]any
-		expectErr bool
-	}{
-		{
-			name: "Equal selector satisfied",
-			kClient: fake.NewClientBuilder().WithScheme(testScheme).WithObjects(
-				&kargoapi.Warehouse{
-					ObjectMeta: metav1.ObjectMeta{
-						Namespace: "some-namespace",
-						Name:      "some-warehouse",
-					},
-					Spec: kargoapi.WarehouseSpec{
-						Subscriptions: []kargoapi.RepoSubscription{{
-							Git: &kargoapi.GitSubscription{RepoURL: testRepoURL},
-						}},
-					},
-				},
-			).WithIndex(
-				&kargoapi.Warehouse{},
-				indexer.WarehousesBySubscribedURLsField,
-				indexer.WarehousesBySubscribedURLs,
-			).Build(),
-			selector: kargoapi.IndexSelector{
-				MatchExpressions: []kargoapi.IndexSelectorRequirement{
-					{
-						Key:      indexer.WarehousesBySubscribedURLsField,
-						Operator: kargoapi.IndexSelectorRequirementOperatorEqual,
-						Value:    "${{ request.body.repository.clone_url }}",
-					},
-				},
-			},
-			env: map[string]any{
-				"request": map[string]any{
-					"body": map[string]any{
-						"repository": map[string]any{
-							"clone_url": testRepoURL,
-						},
-					},
-				},
-			},
-			expectErr: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			listOpts, err := newListOptionsForIndexSelector(tt.selector, tt.env)
-			if tt.expectErr {
-				require.Error(t, err)
-				return
-			}
-			whList := new(kargoapi.WarehouseList)
-			err = tt.kClient.List(t.Context(), whList, listOpts...)
-			require.NoError(t, err)
-			require.Len(t, whList.Items, 1)
 		})
 	}
 }
