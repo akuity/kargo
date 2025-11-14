2e1a7c8c040221a62fbe5e3f54336e1a38bd4b5f
tal-hason <thaon@redhat.com>
2025-11-14 12:51:19 +0000

    fix ui carshses from freight-2
    Signed-off-by: thason <thason@redhat.com>

--- ui/src/utils/url.ts
+++ ui/src/utils/url.ts
@@ -13,31 +13,41 @@ export const urlForImage = (image: string | undefined | null): string => {
 
   const parts = image.split('/');
 
-  if (parts[0] === 'docker.io') {
+  if (parts.length > 0 && parts[0] === 'docker.io') {
     parts.shift();
   }
 
-  if (parts[0] === 'library') {
+  if (parts.length > 0 && parts[0] === 'library') {
     parts.shift();
+  }
+
+  if (parts.length === 0) {
+    return '';
   }
 
   image = parts.join('/');
 
   if (parts.length === 1) {
     return `${protocol}${dockerHub}/${officialPath}/${image}`;
-  } else if (
-    parts[0] && !(parts[0].includes('.') || parts[0].includes(':')) &&
+  }
+
+  if (!parts[0] || typeof parts[0] !== 'string') {
+    return `${protocol}${image}`;
+  }
+
+  if (
+    !(parts[0].includes('.') || parts[0].includes(':')) &&
     parts[0] !== 'localhost' &&
     parts[0].toLowerCase() === parts[0]
   ) {
     return `${protocol}${dockerHub}/${userPath}/${image}`;
   }
 
-  if (parts[0] && parts[0] === 'public.ecr.aws') {
+  if (parts[0] === 'public.ecr.aws') {
     return `${protocol}gallery.ecr.aws/${parts.slice(1).join('/')}`;
   }
 
-  if (parts[0] && parts[0].endsWith('amazonaws.com')) {
+  if (parts[0].endsWith('amazonaws.com')) {
     const domainParts = parts[0].split('.');
     const region = domainParts[3];
     const id = domainParts[0];
