db9e610594c7a0323a55ea6006f8f3b32067d83e
Mayursinh Sarvaiya <marvinduff97@gmail.com>
2025-11-21 14:24:13 +0530

    feat(ui): generic subscriber support
    Signed-off-by: Mayursinh Sarvaiya <marvinduff97@gmail.com>

--- ui/src/features/project/pipelines/freight/freight-table.tsx
+++ ui/src/features/project/pipelines/freight/freight-table.tsx
@@ -4,6 +4,7 @@ import { faAnchor } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Table } from 'antd';
 import classNames from 'classnames';
+import { useMemo } from 'react';
 
 import { ArtifactMetadata } from '@ui/features/freight/artifact-metadata';
 import { flattenFreightOrigin } from '@ui/features/freight/flatten-freight-origin-utils';
@@ -15,56 +16,99 @@ type FreightTableProps = {
 };
 
 export const FreightTable = (props: FreightTableProps) => {
+  const freightSource = useMemo(() => {
+    if (!props.freight) {
+      return {
+        builtInFreightSource: [],
+        genericFreightSource: []
+      };
+    }
+
+    const freightSources = flattenFreightOrigin(props.freight);
+
+    const builtInFreightSource = freightSources.filter((f) => f.type !== 'other');
+    const genericFreightSource = freightSources.filter((f) => f.type === 'other');
+
+    return {
+      builtInFreightSource,
+      genericFreightSource
+    };
+  }, [props.freight]);
+
   return (
-    <Table
-      className={classNames(props.className)}
-      pagination={{ pageSize: 5 }}
-      dataSource={flattenFreightOrigin(props.freight)}
-      columns={[
-        {
-          title: 'Source',
-          width: '5%',
-          render: (_, { type }) => {
-            let icon: IconProp = faGitAlt;
+    <>
+      {freightSource.builtInFreightSource?.length > 0 && (
+        <Table
+          className={classNames(props.className)}
+          pagination={{ pageSize: 5 }}
+          dataSource={freightSource.builtInFreightSource}
+          columns={[
+            {
+              title: 'Source',
+              width: '5%',
+              render: (_, { type }) => {
+                let icon: IconProp = faGitAlt;
 
-            switch (type) {
-              case 'helm':
-                icon = faAnchor;
-                break;
-              case 'image':
-                icon = faDocker;
-                break;
-            }
+                switch (type) {
+                  case 'helm':
+                    icon = faAnchor;
+                    break;
+                  case 'image':
+                    icon = faDocker;
+                    break;
+                }
 
-            return <FontAwesomeIcon icon={icon} />;
-          }
-        },
-        {
-          title: 'Repo',
-          dataIndex: 'repoURL',
-          width: '30%'
-        },
-        {
-          title: 'Version',
-          render: (_, record) => {
-            switch (record.type) {
-              case 'git':
-                return record.id;
-              case 'helm':
-                return record.version;
-              case 'image':
-                return record.tag;
+                return <FontAwesomeIcon icon={icon} />;
+              }
+            },
+            {
+              title: 'Repo',
+              dataIndex: 'repoURL',
+              width: '30%'
+            },
+            {
+              title: 'Version',
+              render: (_, record) => {
+                switch (record.type) {
+                  case 'git':
+                    return record.id;
+                  case 'helm':
+                    return record.version;
+                  case 'image':
+                    return record.tag;
+                }
+              }
+            },
+            {
+              title: 'Metadata',
+              width: '600px',
+              render: (_, record) => {
+                return <ArtifactMetadata {...record} />;
+              }
             }
-          }
-        },
-        {
-          title: 'Metadata',
-          width: '600px',
-          render: (_, record) => {
-            return <ArtifactMetadata {...record} />;
-          }
-        }
-      ]}
-    />
+          ]}
+        />
+      )}
+
+      {freightSource.genericFreightSource.length > 0 && (
+        <>
+          <div className='font-semibold mt-4 mb-2 text-xs'>GENERIC ARTIFACTS</div>
+          <Table
+            pagination={{ pageSize: 5 }}
+            dataSource={freightSource.genericFreightSource}
+            columns={[
+              {
+                title: 'Name',
+                dataIndex: 'subscriptionName'
+              },
+              {
+                title: 'Version',
+                dataIndex: 'version'
+              }
+            ]}
+          />
+        </>
+      )}
+    </>
   );
 };
