e6e9ab34e6dd09d0674380cffa1f8cbe0c0fb341
Kent Rancourt <kent.rancourt@gmail.com>
2025-12-04 11:49:29 -0500

    add new defaulting and validation methods to subscriber interface
    Signed-off-by: Kent Rancourt <kent.rancourt@gmail.com>

--- pkg/subscription/subscriber.go
+++ pkg/subscription/subscriber.go
@@ -3,6 +3,8 @@ package subscription
 import (
 	"context"
 
+	"k8s.io/apimachinery/pkg/util/validation/field"
+
 	kargoapi "github.com/akuity/kargo/api/v1alpha1"
 )
 
@@ -10,6 +12,18 @@ import (
 // relevant subscription details from a kargoapi.RepoSubscription and use them
 // to discover corresponding artifacts.
 type Subscriber interface {
+	// ApplySubscriptionDefaults applies default values to the provided
+	// kargoapi.RepoSubscription.
+	ApplySubscriptionDefaults(
+		ctx context.Context,
+		sub *kargoapi.RepoSubscription,
+	) error
+	// ValidateSubscription validates the provided kargoapi.RepoSubscription.
+	ValidateSubscription(
+		ctx context.Context,
+		f *field.Path,
+		sub kargoapi.RepoSubscription,
+	) field.ErrorList
 	// DiscoverArtifacts discovers artifacts according to the parameters of the
 	// provided kargoapi.RepoSubscription. Implementations may return a value of
 	// any type, but in practice, callers can only make sense of:
