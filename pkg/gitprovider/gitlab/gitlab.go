7c25b18eccccc62a1a4a48659fcc5d3030e10071
Zadkiel AHARONIAN <hello@zadkiel.fr>
2025-12-05 13:56:07 +0100

    fix(gitprovider): Normalize Git URL before parsing in GitLab provider
    Signed-off-by: GitHub <noreply@github.com>

--- pkg/gitprovider/gitlab/gitlab.go
+++ pkg/gitprovider/gitlab/gitlab.go
@@ -18,7 +18,7 @@ const ProviderName = "gitlab"
 
 var registration = gitprovider.Registration{
 	Predicate: func(repoURL string) bool {
-		u, err := url.Parse(repoURL)
+		u, err := url.Parse(urls.NormalizeGit(repoURL))
 		if err != nil {
 			return false
 		}
