cbe3d9848e9e00d9b5926d5acf54b748c8b6b938
Rafal Pelczar <rafal@akuity.io>
2026-01-13 23:16:47 +0100

    fix lint
    Signed-off-by: Rafal Pelczar <rafal@akuity.io>

--- ui/src/features/settings/cluster-secret/create-system-secret-modal.tsx
+++ ui/src/features/settings/cluster-secret/create-system-secret-modal.tsx
@@ -6,8 +6,8 @@ import { z } from 'zod';
 
 import { FieldContainer } from '@ui/features/common/form/field-container';
 import { ModalComponentProps } from '@ui/features/common/modal/modal-context';
-import { dnsRegex } from '@ui/features/common/utils';
 import { SecretEditor } from '@ui/features/common/settings/secrets/secret-editor';
+import { dnsRegex } from '@ui/features/common/utils';
 import {
   createGenericCredentials,
   updateGenericCredentials
