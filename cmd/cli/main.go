7e541eabdd57d174bdc8a4ba869d223de9ac2114
Kent Rancourt <kent.rancourt@gmail.com>
2026-01-26 17:38:12 -0500

    allow auth using api token in env var
    Signed-off-by: Kent Rancourt <kent.rancourt@gmail.com>

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -10,13 +10,22 @@ import (
 
 func main() {
 	ctx := context.Background()
-	cfg, err := config.LoadCLIConfig()
-	if err != nil {
-		if !config.IsConfigNotFoundErr(err) {
-			_, _ = fmt.Fprintln(os.Stderr, fmt.Errorf("load config: %w", err))
-			os.Exit(1)
+	// Get config from env vars first.
+	cfg := config.NewEnvVarCLIConfig()
+	// If env vars provided insufficient connection details, try loading from
+	// config file.
+	if cfg.APIAddress == "" || cfg.BearerToken == "" {
+		var err error
+		if cfg, err = config.LoadCLIConfig(); err != nil {
+			if !config.IsConfigNotFoundErr(err) {
+				_, _ = fmt.Fprintln(os.Stderr, fmt.Errorf("load config: %w", err))
+				os.Exit(1)
+			}
+			// If we get to here, we tried loading config from a file, but it wasn't
+			// found. Fall back to defaults (which will be empty). Individual commands
+			// will fail if they use a client that requires connection details.
+			cfg = config.NewDefaultCLIConfig()
 		}
-		cfg = config.NewDefaultCLIConfig()
 	}
 	cmd := NewRootCommand(cfg)
 	if err := cmd.ExecuteContext(ctx); err != nil {
