a94c84459f3460132f1867a68e006660d44f5bac
Mayursinh Sarvaiya <marvinduff97@gmail.com>
2026-03-06 10:17:46 +0530

    chore: update existing components
    Signed-off-by: Mayursinh Sarvaiya <marvinduff97@gmail.com>

--- ui/src/pages/downloads/components.tsx
+++ ui/src/pages/downloads/components.tsx
@@ -1,32 +1,34 @@
 import { faCircleDown } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Button, Card, Flex, Typography } from 'antd';
 
 import { Platform, Release } from './types';
 
 export const DownloadLink = ({ url, children }: { url: string; children: React.ReactNode }) => (
-  <a
+  <Button
+    type='primary'
     href={url}
     target='_blank'
     rel='noreferrer'
-    className='bg-blue-500 inline-flex items-center text-white px-2 py-1 rounded text-sm font-medium no-underline'
+    icon={<FontAwesomeIcon icon={faCircleDown} />}
+    size='small'
   >
-    <FontAwesomeIcon icon={faCircleDown} className='mr-2' />
     {children}
-  </a>
+  </Button>
 );
 
 export const DownloadItem = ({ title, icon, links, release }: Platform & { release?: Release }) => (
-  <div className='p-4 rounded bg-gray-100 flex flex-col' style={{ width: '250px' }}>
-    <div className='flex items-center mb-4 text-gray-800 font-medium text-lg mx-auto'>
-      <FontAwesomeIcon icon={icon} className='mr-2' />
+  <Card style={{ width: 250 }}>
+    <Typography.Title level={5} style={{ textAlign: 'center', marginBottom: 16 }}>
+      <FontAwesomeIcon icon={icon} style={{ marginRight: 8 }} />
       {title}
-    </div>
-    <div className='flex items-center gap-2 justify-center'>
+    </Typography.Title>
+    <Flex wrap gap='small' justify='center'>
       {links.map((link) => (
         <DownloadLink key={link.title} url={link.getUrl(release)}>
           {link.title}
         </DownloadLink>
       ))}
-    </div>
-  </div>
+    </Flex>
+  </Card>
 );
