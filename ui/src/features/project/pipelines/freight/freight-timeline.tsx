4c295527cce88a3835104cbf2923942ce1a3b075
Henry Catalini Smith <henry@catalinismith.se>
2026-03-13 10:29:45 +0100

    fix(ui): add smooth scrolling to freight list
    Signed-off-by: Henry Catalini Smith <henry@catalinismith.se>

--- ui/src/features/project/pipelines/freight/freight-timeline.tsx
+++ ui/src/features/project/pipelines/freight/freight-timeline.tsx
@@ -199,7 +199,10 @@ export const FreightTimeline = (props: { freights: Freight[]; project: string })
             }
           }}
         >
-          <div className='flex gap-1 relative right-0' ref={freightListStyleRef}>
+          <div
+            className='flex gap-1 relative right-0 transition-[right] duration-300 ease-out'
+            ref={freightListStyleRef}
+          >
             {filteredFreights.map((freight) => {
               const freightSoakTime = soakTime?.[freight?.metadata?.name || ''];
 
