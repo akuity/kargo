b804232d0b9dba1e551f19cd5800e310d5bc02cc
Mayursinh Sarvaiya <marvinduff97@gmail.com>
2026-03-25 12:32:08 +0530

    chore: refactor
    Signed-off-by: Mayursinh Sarvaiya <marvinduff97@gmail.com>

--- ui/src/features/stage/promotion-steps.tsx
+++ ui/src/features/stage/promotion-steps.tsx
@@ -1,11 +1,11 @@
-import { Alert, Collapse, ConfigProvider } from 'antd';
+import { Alert, Collapse } from 'antd';
 import { useMemo } from 'react';
 
 import { Promotion } from '@ui/gen/api/v1alpha1/generated_pb';
 
 import {
   getPromotionDirectiveStepStatus,
-  PromotionDirectiveStepStatus
+  isFailedStep
 } from '../common/promotion-directive-step-status/utils';
 import {
   getPromotionStatusPhase,
@@ -31,44 +31,35 @@ export const PromotionSteps = (props: PromotionStepsProps) => {
   const shouldShowMessage =
     isPromotionPhaseTerminal(phase) &&
     phase !== PromotionStatusPhase.SUCCEEDED &&
+    phase !== PromotionStatusPhase.ERRORED && // because its already handled at individual step level
     !!props.promotion?.status?.message;
 
   const steps = props.promotion?.spec?.steps ?? [];
 
-  const failedStepIndex = shouldShowMessage
-    ? steps.findIndex(
-        (_, i) =>
-          getPromotionDirectiveStepStatus(i, props.promotion.status) ===
-          PromotionDirectiveStepStatus.FAILED
-      )
-    : -1;
+  const errorItem = {
+    key: 'error',
+    label: (
+      <Alert className='rounded-none' message={props.promotion.status?.message} type='error' />
+    ),
+    showArrow: false,
+    collapsible: 'disabled' as const,
+    style: { border: 'none' },
+    styles: { header: { padding: 0 } }
+  };
 
   const items = steps.flatMap((step, i) => {
     const result = getPromotionDirectiveStepStatus(i, props.promotion.status);
-    const item = Step({ step, result, output: outputsByStepAlias?.[step?.as || ''] });
+    const item = Step({ step, result, output: outputsByStepAlias[step.as || ''] });
 
-    if (shouldShowMessage && i === failedStepIndex) {
-      return [
-        item,
-        {
-          key: `error-${i}`,
-          label: (
-            <ConfigProvider
-              theme={{ token: { colorErrorBorder: 'transparent', borderRadiusLG: 0 } }}
-            >
-              <Alert message={props.promotion?.status?.message} type='error' showIcon />
-            </ConfigProvider>
-          ),
-          showArrow: false,
-          collapsible: 'disabled' as const,
-          style: { border: 'none' },
-          styles: { header: { padding: 0, background: 'transparent' } }
-        }
-      ];
-    }
-
-    return [item];
+    return isFailedStep(i, props.promotion.status) ? [item, errorItem] : [item];
   });
 
-  return <Collapse expandIconPosition='end' bordered={false} items={items} />;
+  return (
+    <>
+      <Collapse expandIconPosition='end' bordered={false} items={items} />
+      {shouldShowMessage && (
+        <Alert message={props.promotion.status?.message} type='error' className='mt-4' />
+      )}
+    </>
+  );
 };
