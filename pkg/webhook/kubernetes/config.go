7793441edc10899563952a3dacf8607070121e14
Kent Rancourt <kent.rancourt@gmail.com>
2026-03-25 18:04:52 -0400

    implement admission controls to enforce replica resource immutability by all user except the management controller
    Signed-off-by: Kent Rancourt <kent.rancourt@gmail.com>

--- pkg/webhook/kubernetes/config.go
+++ pkg/webhook/kubernetes/config.go
@@ -7,10 +7,17 @@ import (
 )
 
 type Config struct {
+	// KargoNamespace is the namespace in which Kargo is installed.
+	KargoNamespace string `envconfig:"KARGO_NAMESPACE" required:"true"`
 	// RawControlplaneUserRegex is a regular expression to match the username in
 	// admission request to distinguish if the request is coming from controlplane.
 	RawControlplaneUserRegex string         `envconfig:"CONTROLPLANE_USER_REGEX"`
 	ControlplaneUserRegex    *regexp.Regexp `ignored:"true"`
+	// ManagementControllerUsername is the exact username (typically a service
+	// account name) of the management controller. This is used where only the
+	// management controller (not the API server or other controlplane
+	// components) should be permitted to act.
+	ManagementControllerUsername string `envconfig:"MANAGEMENT_CONTROLLER_USERNAME"`
 }
 
 func ConfigFromEnv() Config {
