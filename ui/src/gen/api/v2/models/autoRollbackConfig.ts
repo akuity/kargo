605d10bd671d3ee888710a1e5f94ef8dd3e94e46
Jesse Suen <jesse@akuity.io>
2026-06-22 16:22:09 -0700

    fix: replace unbounded CEL rules on AutoRollbackConfig with enum item validation
    Signed-off-by: Jesse Suen <jesse@akuity.io>

--- ui/src/gen/api/v2/models/autoRollbackConfig.ts
+++ ui/src/gen/api/v2/models/autoRollbackConfig.ts
@@ -15,7 +15,9 @@ due to transient issues with the deployment itself (network, credential
 expirations, etc...). Defaults to [].
 
 +optional
-+listType=set */
++listType=set
++kubebuilder:validation:MaxItems=2
++kubebuilder:validation:Enum=Failed;Errored */
   onPromotion?: string[];
   /** OnVerification is the list of terminal verification phases that should
 trigger an automated rollback. Only Failed and Error are accepted (note:
@@ -23,6 +25,8 @@ trigger an automated rollback. Only Failed and Error are accepted (note:
 defaults to [Failed].
 
 +optional
-+listType=set */
++listType=set
++kubebuilder:validation:MaxItems=2
++kubebuilder:validation:Enum=Failed;Error */
   onVerification?: string[];
 }
