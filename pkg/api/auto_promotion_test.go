5fb5b795409b33f09aa6617e4f2ded13bf90a85c
fuskovic <fhuskovic92@gmail.com>
2026-07-03 15:33:05 +0530

    consolidate auto_promotion_hold_test.go into auto_promotion_test.go
    Signed-off-by: fuskovic <fhuskovic92@gmail.com>

--- pkg/api/auto_promotion_test.go
+++ pkg/api/auto_promotion_test.go
@@ -488,3 +488,33 @@ func TestSelectAutoPromotionCandidates(t *testing.T) {
 		})
 	}
 }
+
+func TestSetAutoPromotionHoldAnnotation(t *testing.T) {
+	origin := kargoapi.FreightOrigin{Kind: kargoapi.FreightOriginKindWarehouse, Name: "fake-warehouse"}
+	promo := &kargoapi.Promotion{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "p",
+			Annotations: map[string]string{
+				kargoapi.AnnotationKeyAutoPromotionResume: "Warehouse/old",
+			},
+		},
+	}
+	SetAutoPromotionHoldAnnotation(promo, origin)
+	require.Equal(t, "Warehouse/fake-warehouse", promo.Annotations[kargoapi.AnnotationKeyAutoPromotionHold])
+	require.NotContains(t, promo.Annotations, kargoapi.AnnotationKeyAutoPromotionResume)
+}
+
+func TestSetAutoPromotionResumeAnnotation(t *testing.T) {
+	origin := kargoapi.FreightOrigin{Kind: kargoapi.FreightOriginKindWarehouse, Name: "fake-warehouse"}
+	promo := &kargoapi.Promotion{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "p",
+			Annotations: map[string]string{
+				kargoapi.AnnotationKeyAutoPromotionHold: "Warehouse/old",
+			},
+		},
+	}
+	SetAutoPromotionResumeAnnotation(promo, origin)
+	require.Equal(t, "Warehouse/fake-warehouse", promo.Annotations[kargoapi.AnnotationKeyAutoPromotionResume])
+	require.NotContains(t, promo.Annotations, kargoapi.AnnotationKeyAutoPromotionHold)
+}
