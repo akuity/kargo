3808e3b91bba88386487964e57898705ce8f1658
Daniil Fedotov <daniil.fedotov@akuity.io>
2026-08-21 15:54:55 -0400

    WIP: base structure for sharing test environments

--- hack/test/e2e/suites/shared/shared_test.go
+++ hack/test/e2e/suites/shared/shared_test.go
@@ -1,6 +1,6 @@
-//go:build e2e
+//go:build e2e && shared
 //nolint:forcetypeassert
-package argocd_helm_chart
+package shared_test
 
 // This test implements the Argo CD driven, Helm chart-only example from
 // https://github.com/akuity/kargo-examples (01-argocd-driven/02-helm-driven/01-chart-only).
@@ -11,17 +11,18 @@ package argocd_helm_chart
 
 import (
 	"testing"
+
 	"github.com/akuity/kargo/hack/test/e2e/framework/utils"
+	_ "github.com/akuity/kargo/hack/test/e2e/suites/kargo_fixtures"
+	_ "github.com/akuity/kargo/hack/test/e2e/suites/argocd_helm_chart"
 )
 
-// This file provides necessary setup for a test package to run environment setup for e2e test.
-// Because golang doesn't allow import of test code, this code needs to be added to each test package.
 func TestMain(m *testing.M) {
 	utils.InitEnv(m)
 }
 
-func TestArgocdHelmChart(t *testing.T) {
-	// Actual test code lives in test_code.go
-	// This is a trick to allow shared run between multiple packages
-	utils.TestEnv.Test(t, feature())
+func TestAll(t *testing.T) {
+	for _, feature := range utils.TestFeatures {
+		utils.TestEnv.Test(t, feature)
+	}
 }
